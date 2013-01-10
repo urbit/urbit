b7976ec3196e88109fff6ea8908dcfbcc6261bc0
Orson Peters <orsonpeters@gmail.com>
2013-01-10 22:42:57 +0100

    done for today

--- fe.h
+++ fe.h
@@ -13,25 +13,6 @@ t[0]+2^26 t[1]+2^51 t[2]+2^77 t[3]+2^102 t[4]+...+2^230 t[9].
 Bounds on each t[i] vary depending on context.
 */
 
-#define fe_frombytes crypto_sign_ed25519_ref10_fe_frombytes
-#define fe_tobytes crypto_sign_ed25519_ref10_fe_tobytes
-#define fe_copy crypto_sign_ed25519_ref10_fe_copy
-#define fe_isnonzero crypto_sign_ed25519_ref10_fe_isnonzero
-#define fe_isnegative crypto_sign_ed25519_ref10_fe_isnegative
-#define fe_0 crypto_sign_ed25519_ref10_fe_0
-#define fe_1 crypto_sign_ed25519_ref10_fe_1
-#define fe_cswap crypto_sign_ed25519_ref10_fe_cswap
-#define fe_cmov crypto_sign_ed25519_ref10_fe_cmov
-#define fe_add crypto_sign_ed25519_ref10_fe_add
-#define fe_sub crypto_sign_ed25519_ref10_fe_sub
-#define fe_neg crypto_sign_ed25519_ref10_fe_neg
-#define fe_mul crypto_sign_ed25519_ref10_fe_mul
-#define fe_sq crypto_sign_ed25519_ref10_fe_sq
-#define fe_sq2 crypto_sign_ed25519_ref10_fe_sq2
-#define fe_mul121666 crypto_sign_ed25519_ref10_fe_mul121666
-#define fe_invert crypto_sign_ed25519_ref10_fe_invert
-#define fe_pow22523 crypto_sign_ed25519_ref10_fe_pow22523
-
 extern void fe_frombytes(fe,const unsigned char *);
 extern void fe_tobytes(unsigned char *,const fe);
 
--- ge.h
+++ ge.h
@@ -50,27 +50,6 @@ typedef struct {
   fe T2d;
 } ge_cached;
 
-#define ge_frombytes_negate_vartime crypto_sign_ed25519_ref10_ge_frombytes_negate_vartime
-#define ge_tobytes crypto_sign_ed25519_ref10_ge_tobytes
-#define ge_p3_tobytes crypto_sign_ed25519_ref10_ge_p3_tobytes
-
-#define ge_p2_0 crypto_sign_ed25519_ref10_ge_p2_0
-#define ge_p3_0 crypto_sign_ed25519_ref10_ge_p3_0
-#define ge_precomp_0 crypto_sign_ed25519_ref10_ge_precomp_0
-#define ge_p3_to_p2 crypto_sign_ed25519_ref10_ge_p3_to_p2
-#define ge_p3_to_cached crypto_sign_ed25519_ref10_ge_p3_to_cached
-#define ge_p1p1_to_p2 crypto_sign_ed25519_ref10_ge_p1p1_to_p2
-#define ge_p1p1_to_p3 crypto_sign_ed25519_ref10_ge_p1p1_to_p3
-#define ge_p2_dbl crypto_sign_ed25519_ref10_ge_p2_dbl
-#define ge_p3_dbl crypto_sign_ed25519_ref10_ge_p3_dbl
-
-#define ge_madd crypto_sign_ed25519_ref10_ge_madd
-#define ge_msub crypto_sign_ed25519_ref10_ge_msub
-#define ge_add crypto_sign_ed25519_ref10_ge_add
-#define ge_sub crypto_sign_ed25519_ref10_ge_sub
-#define ge_scalarmult_base crypto_sign_ed25519_ref10_ge_scalarmult_base
-#define ge_double_scalarmult_vartime crypto_sign_ed25519_ref10_ge_double_scalarmult_vartime
-
 extern void ge_tobytes(unsigned char *,const ge_p2 *);
 extern void ge_p3_tobytes(unsigned char *,const ge_p3 *);
 extern int ge_frombytes_negate_vartime(ge_p3 *,const unsigned char *);
