20c7bc9e6c2c697bd0ffea46c4235d79c642044e
C. Guy Yarvin <cgyarvin@gmail.com>
2014-02-18 08:42:47 -0800

    Minor bull fix.

--- gen164/6/ut_peek.c
+++ gen164/6/ut_peek.c
@@ -37,6 +37,13 @@
       case c3__atom: {
         return c3__void;
       }
+      case c3__bull: {
+        if ( (u2_no == u2_as_trel(sut, 0, &p_sut, &q_sut)) ) {
+          return u2_bl_bail(wir_r, c3__fail);
+        } else {
+          return _peek_in(wir_r, van, q_sut, way, axe, gil);
+        }
+      }
       case c3__cell: {
         if ( (u2_no == u2_as_trel(sut, 0, &p_sut, &q_sut)) ) {
           return u2_bl_bail(wir_r, c3__fail);
