632bab1c01eea483cb8abcdc8c721ea9fa8c546e
~hatteb-mitlyd <anthony@tlon.io>
2014-04-11 17:02:10 -0700

    sort-of fix jets for Ed25519
    puck:ed is correct for at least the single test-point i tried.
    
    sign:ed is still wrong.

--- gen164/5/ed.c
+++ gen164/5/ed.c
@@ -7,16 +7,20 @@
 
 /* declarations
 */
-  extern u2_ho_jet j2_mcj(Pt5, ed, sign)[];
+  extern u2_ho_jet j2_mdj(Pt5, coed, ed, sign)[];
+  extern u2_ho_jet j2_mdj(Pt5, coed, ed, puck)[];
+
+  u2_ho_driver
+  j2_mcd(Pt5, coed, ed)[] = {
+    { j2_sd(Pt5, coed, ed, sign), j2_mdj(Pt5, coed, ed, sign), 0, 0, u2_none },
+    { j2_sd(Pt5, coed, ed, puck), j2_mdj(Pt5, coed, ed, puck), 0, 0, u2_none },
+    {}
+  };
 
 /* structures
 */
   u2_ho_driver
-  j2_mbd(Pt5, ed)[] = {
-    { j2_sc(Pt5, ed, sign), j2_mcj(Pt5, ed, sign), 0, 0, u2_none },
+  j2_mbd(Pt5, coed)[] = {
+    { j2_sc(Pt5, coed, ed), 0, j2_mcd(Pt5, coed, ed), 0, 0, u2_none },
     {}
   };
-
-  u2_ho_driver
-  j2_db(Pt5, ed) =
-    { j2_sb(Pt5, ed), 0, j2_mbd(Pt5, ed), 0, u2_none };
