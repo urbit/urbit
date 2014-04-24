00d8f436ecd52b82c3d1e53afd60c9c80e0e71f6
~hatteb-mitlyd <anthony@tlon.io>
2014-04-23 19:34:32 -0700

    use a non-noun-based hashtable to jam

--- gen164/5/jam.c
+++ gen164/5/jam.c
@@ -4,30 +4,30 @@
 */
 #include "all.h"
 #include "../pit.h"
-
+#include "f/nash.h"
 /* functions
 */
+  struct u2_nash* T_m;
   static u2_noun
-  _jam_in(u2_wire, u2_atom, u2_atom, u2_noun, u2_noun);
+  _jam_in(u2_wire, u2_atom, u2_atom, u2_noun);
 
   static u2_noun                                                  //  produce
   _jam_in_pair(u2_wire wir_r,
                u2_atom h_a,                                       //  retain
                u2_atom t_a,                                       //  retain
                u2_atom b,                                         //  retain
-               u2_noun m,                                         //  retain
                u2_noun l)                                         //  retain
   {
     u2_noun w = u2_bc(wir_r, u2_bc(wir_r, _2, _1), u2_rx(wir_r, l));
     u2_noun x = j2_mbc(Pt1, add)(wir_r, _2, b);
-    u2_noun d = _jam_in(wir_r, h_a, x, m, w);
+    u2_noun d = _jam_in(wir_r, h_a, x, w);
     u2_noun p_d, q_d, r_d;
     u2_noun r;
 
     u2_as_trel(d, &p_d, &q_d, &r_d);
     {
       u2_noun y = j2_mbc(Pt1, add)(wir_r, x, p_d);
-      u2_noun e = _jam_in(wir_r, t_a, y, r_d, q_d);
+      u2_noun e = _jam_in(wir_r, t_a, y, q_d);
       u2_noun p_e, q_e, r_e;
 
       u2_as_trel(e, &p_e, &q_e, &r_e);
@@ -37,7 +37,7 @@
         r = u2_bt
           (wir_r, j2_mbc(Pt1, add)(wir_r, _2, z),
                   u2_rx(wir_r, q_e),
-                  u2_rx(wir_r, r_e));
+                  0);
 
         u2_rz(wir_r, z);
       }
@@ -54,7 +54,6 @@
   static u2_noun                                                  //  produce
   _jam_in_flat(u2_wire wir_r,
                u2_atom a,                                         //  retain
-               u2_noun m,                                         //  retain
                u2_noun l)                                         //  retain
   {
     u2_noun d = j2_mby(Pt5, mat)(wir_r, a);
@@ -65,7 +64,7 @@
                                  x,
                                  j2_mbc(Pt3, lsh)(wir_r, _0, _1, u2_t(d))),
                            u2_rx(wir_r, l)),
-              u2_rx(wir_r, m));
+              0);
 
     u2_rz(wir_r, d);
 
@@ -75,7 +74,6 @@
   static u2_noun                                                  //  produce
   _jam_in_ptr(u2_wire wir_r,
               u2_atom u_c,                                        //  retain
-              u2_noun m,                                          //  retain
               u2_noun l)                                          //  retain
   {
     u2_noun d = j2_mby(Pt5, mat)(wir_r, u_c);
@@ -85,7 +83,7 @@
       (wir_r, u2_rx(wir_r, y),
               u2_bc(wir_r, u2_bc(wir_r, y, j2_mbc(Pt3, mix)(wir_r, _3, x)),
                            u2_rx(wir_r, l)),
-              u2_rx(wir_r, m));
+              0);
 
     u2_rz(wir_r, d);
     u2_rz(wir_r, x);
@@ -97,32 +95,27 @@
   _jam_in(u2_wire wir_r,
           u2_noun a,                                              //  retain
           u2_atom b,                                              //  retain
-          u2_noun m,                                              //  retain
           u2_noun l)                                              //  retain
   {
-    u2_noun c = j2_mcc(Pt4, by, get)(wir_r, m, a);
+    u2_noun c = u2_na_get(T_m, a);
     u2_noun x;
 
     if ( u2_nul == c ) {
-      m = j2_mcc(Pt4, by, put)(wir_r, m, a, b);
+      u2_na_put(T_m, a, b);
 
       if ( u2_yes == u2_stud(a) ) {
-        x = _jam_in_flat(wir_r, a, m, l);
+        x = _jam_in_flat(wir_r, a, l);
       } else {
-        x = _jam_in_pair(wir_r, u2_h(a), u2_t(a), b, m, l);
+        x = _jam_in_pair(wir_r, u2_h(a), u2_t(a), b, l);
       }
-      u2_rz(wir_r, m);
     }
     else {
-      u2_noun u_c = u2_t(c);
-
-      if ( u2_yes == u2_stud(a) && u2_met(0, a) <= u2_met(0, u_c) ) {
-        x = _jam_in_flat(wir_r, a, m, l);
+      if ( u2_yes == u2_stud(a) && u2_met(0, a) <= u2_met(0, c) ) {
+        x = _jam_in_flat(wir_r, a, l);
       }
       else {
-        x = _jam_in_ptr(wir_r, u_c, m, l);
+        x = _jam_in_ptr(wir_r, c, l);
       }
-      u2_rz(wir_r, c);
     }
     return x;
   }
@@ -131,13 +124,15 @@
   j2_mby(Pt5, jam)(u2_wire wir_r,
                    u2_atom a)                                     //  retain
   {
-    u2_noun x = _jam_in(wir_r, a, _0, u2_nul, u2_nul);
+    T_m = u2_na_make();
+    u2_noun x = _jam_in(wir_r, a, _0, u2_nul);
     u2_noun q = j2_mbc(Pt2, flop)(wir_r, u2_h(u2_t(x)));
     u2_noun r = j2_mbc(Pt3, can)(wir_r, _0, q);
 
     u2_rz(wir_r, x);
     u2_rz(wir_r, q);
-
+    u2_na_take(T_m);
+    T_m = NULL;
     return r;
   }
   u2_noun                                                         //  transfer
@@ -157,6 +152,6 @@
 */
   u2_ho_jet
   j2_mbj(Pt5, jam)[] = {
-    { ".2", c3__hevy, j2_mb(Pt5, jam), Tier3, u2_none, u2_none },
+    { ".2", c3__hevy, j2_mb(Pt5, jam), Tier5, u2_none, u2_none },
     { }
   };
