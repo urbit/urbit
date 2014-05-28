7ccf1e5a8f4eced76b6360c02bc03fedac67fe3d
~hatteb-mitlyd <anthony@tlon.io>
2014-05-28 15:23:35 -0700

    Don't assert in moog

--- f/nock.c
+++ f/nock.c
@@ -901,7 +901,6 @@ _nock_moog(u2_noun bus,
       if ( u2h(hoe) == c3__exit ) {
         res = u2nc(2, u2k(u2t(hoe)));
 
-        c3_assert(0);
         u2z(hoe);
       }
       else if ( u2h(hoe) == c3__need ) {
