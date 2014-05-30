04396afdcf3e423a5c9e26df431ed799fe4a274a
C. Guy Yarvin <cgyarvin@gmail.com>
2014-05-30 11:43:48 -0700

    GHETTO.

--- v/raft.c
+++ v/raft.c
@@ -1535,7 +1535,9 @@ _raft_lame(u2_reck* rec_u, u2_noun ovo, u2_noun why, u2_noun tan)
 static void
 _raft_punk(u2_reck* rec_u, u2_noun ovo)
 {
-  //  c3_c* txt_c = u2_cr_string(u2h(u2t(ovo)));
+#ifdef GHETTO
+  c3_c* txt_c = u2_cr_string(u2h(u2t(ovo)));
+#endif
   c3_w sec_w;
   //  static c3_w num_w;
   u2_noun gon;
@@ -1555,14 +1557,19 @@ _raft_punk(u2_reck* rec_u, u2_noun ovo)
     u2_Host.beh_u.run_w = 0;
   }
 
-  //  struct timeval b4, f2, d0;
-  //  gettimeofday(&b4, 0);
+#ifdef GHETTO
+  struct timeval b4, f2, d0;
+  gettimeofday(&b4, 0);
+#endif
 
   gon = u2_lo_soft(rec_u, sec_w, u2_reck_poke, u2k(ovo));
 
-  //  gettimeofday(&f2, 0);
-  //  timersub(&f2, &b4, &d0);
-  //  uL(fprintf(uH, "%%punk %ld.%d\n", d0.tv_sec, d0.tv_usec));
+#ifdef GHETTO
+  gettimeofday(&f2, 0);
+  timersub(&f2, &b4, &d0);
+  uL(fprintf(uH, "%%punk %s %ld.%d\n", txt_c, d0.tv_sec, d0.tv_usec));
+  free(txt_c);
+#endif
 
   if ( u2_blip != u2h(gon) ) {
     u2_noun why = u2k(u2h(gon));
