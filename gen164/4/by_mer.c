07c242d31c704cbbb9084b69e0a18f90499b42e1
pcmonk <pcmonk@asu.edu>
2014-06-11 11:18:43 -0700

    Added map merge (mer:by)

--- gen164/4/by_mer.c
+++ gen164/4/by_mer.c
@@ -1,4 +1,4 @@
-/* j/4/in_mer.c
+/* j/4/by_mer.c
 **
 ** This file is in the public domain.
 */
@@ -8,7 +8,7 @@
 /* functions
 */
   u2_weak                                                         //  transfer
-  j2_mcc(Pt4, in, mer)(u2_wire wir_r,
+  j2_mcc(Pt4, by, mer)(u2_wire wir_r,
                        u2_noun a,                                 //  retain
                        u2_noun b)                                 //  retain
   {
@@ -19,8 +19,8 @@
       return u2_rx(wir_r, a);
     }
     else {
-      u2_noun l_a, n_a, r_a, lr_a;
-      u2_noun l_b, n_b, r_b, lr_b;
+      u2_noun l_a, n_a, r_a, lr_a, p_n_a, q_n_a;
+      u2_noun l_b, n_b, r_b, lr_b, p_n_b, q_n_b;
       u2_noun c;
 
       if ( (u2_no == u2_as_cell(a, &n_a, &lr_a)) ) {
@@ -29,11 +29,19 @@
       else if ( (u2_no == u2_as_cell(b, &n_b, &lr_b)) ) {
         return u2_bl_bail(wir_r, c3__exit);
       }
+      else if ( (u2_no == u2_as_cell(n_a, &p_n_a, &q_n_a)) ) {
+        return u2_bl_bail(wir_r, c3__exit);
+      }
+      else if ( (u2_no == u2_as_cell(n_b, &p_n_b, &q_n_b)) ) {
+        return u2_bl_bail(wir_r, c3__exit);
+      }
       else {
-        if ( u2_yes == j2_mbc(Pt3, vor)(wir_r, n_b, n_a) ) {
-          c = a;    a = b;       b = c;
-          c = n_a;  n_a = n_b;   n_b = c;
-          c = lr_a; lr_a = lr_b; lr_b = c;
+        if ( u2_yes == j2_mbc(Pt3, vor)(wir_r, p_n_b, p_n_a) ) {
+          c = a;         a = b;         b = c;
+          c = n_a;     n_a = n_b;     n_b = c;
+          c = lr_a;   lr_a = lr_b;   lr_b = c;
+          c = p_n_a; p_n_a = p_n_b; p_n_b = c;
+          c = q_n_a; q_n_a = q_n_b; q_n_b = c;
         }
         if ( u2_no == u2_as_cell(lr_a, &l_a, &r_a) ) {
           return u2_bl_bail(wir_r, c3__exit);
@@ -41,16 +49,16 @@
         else if ( u2_no == u2_as_cell(lr_b, &l_b, &r_b) ) {
           return u2_bl_bail(wir_r, c3__exit);
         }
-        else if ( u2_yes == u2_sing(n_a, n_b) ) {
-          return u2_rt(wir_r, u2_rx(wir_r, n_a),
-                              j2_mcc(Pt4, in, mer)(wir_r, l_a, l_b),
-                              j2_mcc(Pt4, in, mer)(wir_r, r_a, r_b));
+        else if ( u2_yes == u2_sing(p_n_a, p_n_b) ) {
+          return u2_rt(wir_r, u2_rx(wir_r, n_b),
+                              j2_mcc(Pt4, by, mer)(wir_r, l_a, l_b),
+                              j2_mcc(Pt4, by, mer)(wir_r, r_a, r_b));
         }
-        else if ( u2_yes == j2_mbc(Pt3, hor)(wir_r, n_b, n_a) ) {
-          return j2_mcc(Pt4, in, mer)(wir_r,
+        else if ( u2_yes == j2_mbc(Pt3, hor)(wir_r, p_n_b, p_n_a) ) {
+          return j2_mcc(Pt4, by, mer)(wir_r,
                                       u2_rt(wir_r,
                                             n_a,
-                                            j2_mcc(Pt4, in, mer)(wir_r,
+                                            j2_mcc(Pt4, by, mer)(wir_r,
                                                                 l_a,
                                                                 u2_rt(wir_r,
                                                                       n_b,
@@ -60,11 +68,11 @@
                                       r_b);
         }
         else {
-          return j2_mcc(Pt4, in, mer)(wir_r,
+          return j2_mcc(Pt4, by, mer)(wir_r,
                                       u2_rt(wir_r,
                                             n_a,
                                             l_a,
-                                            j2_mcc(Pt4, in, mer)(wir_r,
+                                            j2_mcc(Pt4, by, mer)(wir_r,
                                                                 r_a,
                                                                 u2_rt(wir_r,
                                                                       n_b,
@@ -76,7 +84,7 @@
     }
   }
   u2_weak                                                         //  transfer
-  j2_mc(Pt4, in, mer)(u2_wire wir_r,
+  j2_mc(Pt4, by, mer)(u2_wire wir_r,
                       u2_noun cor)                                //  retain
   {
     u2_noun a, b;
@@ -84,15 +92,15 @@
     if ( u2_no == u2_mean(cor, u2_cv_sam, &b, u2_cv_con_sam, &a, 0) ) {
       return u2_bl_bail(wir_r, c3__exit);
     } else {
-      return j2_mcc(Pt4, in, mer)(wir_r, a, b);
+      return j2_mcc(Pt4, by, mer)(wir_r, a, b);
     }
   }
 
 /* structures
 */
   u2_ho_jet
-  j2_mcj(Pt4, in, mer)[] = {
-    { ".2", c3__lite, j2_mc(Pt4, in, mer), Tier4, u2_none, u2_none },
+  j2_mcj(Pt4, by, mer)[] = {
+    { ".2", c3__lite, j2_mc(Pt4, by, mer), Tier4, u2_none, u2_none },
     { }
   };
 
