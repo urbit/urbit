810f83bc6f79727cee8759cb9a275e6bf4002a5a
Luke Patmore <lpatmore@gmx.se>
2014-06-10 17:51:43 -0700

    Added wtkt macro expansion

--- gen164/6/ap.c
+++ gen164/6/ap.c
@@ -122,6 +122,17 @@
                 _open_in_wtbr(wir_r, tp_gen));
     }
   }
+  _open_do_pqr(wtkt)   //  ?^
+  {
+    //        [%wtkt *]   [%wtcl [%wtts [%axil %atom %$] p.gen] r.gen q.gen]
+    return u2_bq
+      (wir_r, c3__wtcl,
+              u2_bt(wir_r, c3__wtts,
+		           u2_bt(wir_r, c3__axil, c3__atom, u2_blip),
+	                   u2_rx(wir_r, p_gen)),
+              u2_rx(wir_r, r_gen),
+              u2_rx(wir_r, q_gen));
+  }
   _open_do_pq(wtgl)   //  ?<
   {
     return u2_bq
@@ -208,7 +219,6 @@
 ***/
   _open_do_pq(clhp) //  :-
   {
-    printf("wongwong!\n");
     return u2_bc
       (wir_r, u2_rx(wir_r, p_gen),
               u2_rx(wir_r, q_gen));
@@ -529,42 +539,34 @@
         _open_p   (brdt);
         _open_pq  (brcb);
         _open_pq  (brkt);
-
 	_open_pq  (clcb);
 	_open_pq  (clhp);
         _open_pqrs(clkt);
         _open_pqr (clls);
         _open_p   (cltr);
         _open_p   (clsg);
-
         _open_pq  (cncl);
         _open_pq  (cndt);
         _open_pqrs(cnkt);
         _open_pq  (cnhp);
         _open_pqr (cnls);
         _open_pqr (cnsg);
-
 	_open_pq  (sgts);
 	_open_pq  (sgbr);
 	_open_pq  (sggl);
 	_open_pq  (sgbc);
 	_open_pqrs(sgcn);
 	_open_pq  (sgls);
-
         _open_pq  (smcl);
-        // _open_pq  (smsg);
-
         _open_pq  (tsgl);
         _open_pq  (tshp);
         _open_pq  (tsls);
-
         _open_pqr (wtdt);
         _open_pq  (wtgl);
         _open_p   (wtzp);
         _open_p   (wtbr);
+        _open_pqr (wtkt); //testing
         _open_p   (wtpm);
-	// _open_pqr (wtls);
-        // _open_pqr (wtsg);
         _open_pq  (wthp);
         _open_pq  (wtgr);
 
