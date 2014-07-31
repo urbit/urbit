5c3223ba1f4bba7cb78e37c8ae3b2ebb81a5d142
C. Guy Yarvin <cgyarvin@gmail.com>
2014-07-31 13:23:02 -0700

    More memory pls.

--- f/wire.c
+++ f/wire.c
@@ -20,9 +20,11 @@ u2_wr_init(c3_m   hip_m,
 {
   u2_ray wir_r;
 
+#if 0
   fprintf(stderr, "initial: hat_r %d, mat_r %d\r\n", 
       hat_r >> LoomPageWords, 
       mat_r >> LoomPageWords);
+#endif
 
   wir_r = u2_rl_init(hip_m, hat_r, mat_r);
   u2_rail_hat_r(wir_r) += (c3_wiseof(u2_loom_wire) - c3_wiseof(u2_loom_rail));
