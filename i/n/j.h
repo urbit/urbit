5332ae57a3ca4f2104e16b80377b42a3373d6aa4
C. Guy Yarvin <cgyarvin@gmail.com>
2014-11-17 14:12:11 -0800

    Revert jet code to un-break.

--- i/n/j.h
+++ i/n/j.h
@@ -9,22 +9,14 @@
   ++  bash  ,@uvH                                         ::  ctx identity hash
   ++  bosh  ,@uvH                                         ::  local battery hash
   ++  batt  ,*                                            ::  battery
-  ++  calf                                                ::  
+  ++  calx                                                ::  cached by battery
     $:  jax=,@ud                                          ::  jet index
         pax=,@ud                                          ::  parent axis or 0
         hap=(map ,@ud ,@ud)                               ::  axis/jet
+        huc=(map term nock)                               ::  name/tool
     ==                                                    ::
-  ++  calx  (trel calf ,* (map term nock))                ::  cached by battery
-  ++  chum  $?  lef=term                                  ::  jet name
-                [std=term kel=@]                          ::  kelvin version
-                [ven=term pro=term kel=@]                 ::  vendor and product
-                [ven=term pro=term ver=@ kel=@]           ::  all of the above
-            ==                                            ::
-  ++  club  (pair corp (map term nock))                   ::  battery pattern
-  ++  clue  ,[p=chum q=nock r=(list (pair term nock))]    ::  battery definition
-  ++  clog  (pair cope (map batt club))                   ::  identity record
+  ++  clog  (pair cope (map batt (map term nock)))        ::  identity record
   ++  cope  (trel bane axis (each bash noun))             ::  core pattern
-  ++  corp  (each batt core)                              ::  parent or static
   ++  dash                                                ::  jet system
     $:  sys=(map batt bash)                               ::  battery/identity
         haw=(map bash clog)                               ::  identity/core
@@ -68,35 +60,39 @@
 
   /** Data structures.
   ***
-  *** All of these are process structures on the heap or data segment.
+  *** All of these are transient structures allocated with malloc.
   **/
     /* u3j_harm: jet arm.
     */
       typedef struct _u3j_harm {
-        c3_c*               fcs_c;            //  `.axe` or name
-        u3_noun           (*fun_f)(u3_noun);  //  compute or 0 / semitransfer
-        c3_o                ice;              //  perfect (don't test)
-        c3_o                tot;              //  total (never punts)
-        c3_o                liv;              //  live (enabled)
+        c3_c*               fcs_c;             //  `.axe` or name
+        u3_noun           (*fun_f)(u3_noun);   //  compute or 0 / semitransfer
+        // c3_o           (*val_f)(u3_noun);   //  validate or 0 / retain
+        c3_o                ice;               //  perfect (don't test)
+        c3_o                tot;               //  total (never punts)
+        c3_o                liv;               //  live (enabled)
+        c3_l                axe_l;             //  computed/discovered axis
+        struct _u3j_core* cop_u;             //  containing core
       } u3j_harm;
 
     /* u3j_core: driver definition.
     */
       typedef struct _u3j_core {
-        c3_c*             cos_c;              //  control string
-        struct _u3j_harm* arm_u;              //  blank-terminated static list
-        struct _u3j_core* dev_u;              //  blank-terminated static list
-        struct _u3j_core* par_u;              //  initialized parent pointer 
-        c3_l              jax_l;              //  initialized jet index
+        c3_c*               cos_c;      //  control string
+        struct _u3j_harm* arm_u;      //  blank-terminated static list
+        struct _u3j_core* dev_u;      //  blank-terminated static list
+        struct _u3j_core* par_u;      //  dynamic parent pointer 
+        c3_l                axe_l;      //  axis to parent
+        c3_l                jax_l;      //  index in global dashboard
       } u3j_core;
 
     /* u3e_dash, u3_Dash, u3D: jet dashboard singleton
     */
       typedef struct _u3e_dash {
-        u3j_core* dev_u;                      //  null-terminated static list
-        c3_l      len_l;                      //  dynamic array length
-        c3_l      all_l;                      //  allocated length
-        u3j_core* ray_u;                      //  dynamic array by axis
+        u3j_core* dev_u;              //  null-terminated static list
+        c3_l        len_l;              //  dynamic array length
+        c3_l        all_l;              //  allocated length
+        u3j_core* ray_u;              //  dynamic array by axis
       } u3j_dash;
 
   /** Globals.
@@ -113,6 +109,11 @@
       */
         void
         u3j_boot(void);
+
+      /* u3j_clear(): clear jet table to re-register.
+      */
+        void
+        u3j_clear(void);
 
       /* u3j_hook():
       **
@@ -144,17 +145,19 @@
       */
         u3_weak
         u3j_kick(u3_noun cor,
-                 u3_noun axe);
+                   u3_noun axe);
 
       /* u3j_kink(): kick either by jet or by nock.
       */
         u3_noun
-        u3j_kink(u3_noun cor, u3_noun axe);
+        u3j_kink(u3_noun cor,
+                   u3_noun axe);
  
       /* u3j_mine(): register core for jets.
       */
         void
-        u3j_mine(u3_noun clu, u3_noun cor);
+        u3j_mine(u3_noun clu,
+                   u3_noun cor);
 
       /* u3j_ream(): refresh after restoring from checkpoint.
       */
