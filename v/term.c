045f862a84002942558da5da0e6dc1204e7fe0cc
C. Guy Yarvin <cgyarvin@gmail.com>
2015-02-09 12:40:44 -0800

    Full test pathway.

--- v/term.c
+++ v/term.c
@@ -677,7 +677,7 @@ _term_io_flow(u3_utty* uty_u)
   u3_noun tid = u3dc("scot", c3__ud, uty_u->tid_l);
   u3_noun pax = u3nq(u3_blip, c3__term, tid, u3_nul);
 
-  u3v_plan(pax, u3nq(c3__flow, c3__seat, c3__talk, u3_nul));
+  u3v_plan(pax, u3nq(c3__flow, c3__seat, c3__galk, u3_nul));
 }
 
 /* _term_io_belt(): send belt.
