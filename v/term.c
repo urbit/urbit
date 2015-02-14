5d3f63cc55510ac3c28c2d4a9596d657ba682cd8
C. Guy Yarvin <cgyarvin@gmail.com>
2015-02-13 16:37:40 -0800

    Various fixes and improvements.

--- v/term.c
+++ v/term.c
@@ -306,7 +306,7 @@ _term_listen_cb(uv_stream_t *wax_u, int sas_i)
       u3_noun pax = u3nq(u3_blip, c3__term, tid, u3_nul);
       u3v_plan(u3k(pax), u3nc(c3__blew, u3nc(80, 25)));
       u3v_plan(u3k(pax), u3nc(c3__hail, u3_nul));
-      u3v_plan(u3k(pax), u3nq(c3__flow, c3__seat, c3__galk, u3_nul));
+      u3v_plan(u3k(pax), u3nq(c3__flow, c3__seat, c3__talk, u3_nul));
       u3z(pax);
     }
 
@@ -677,7 +677,7 @@ _term_io_flow(u3_utty* uty_u)
   u3_noun tid = u3dc("scot", c3__ud, uty_u->tid_l);
   u3_noun pax = u3nq(u3_blip, c3__term, tid, u3_nul);
 
-  u3v_plan(pax, u3nq(c3__flow, c3__seat, c3__galk, u3_nul));
+  u3v_plan(pax, u3nq(c3__flow, c3__seat, c3__talk, u3_nul));
 }
 
 /* _term_io_belt(): send belt.
