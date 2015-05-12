8a3d03a7bd7e9c663463bc2d97436b0311588b96
Henry Ault <henry@tlon.io>
2015-05-12 16:35:59 -0700

    working do

--- urb/zod/base/pub/do/src/main.js
+++ urb/zod/base/pub/do/src/main.js
@@ -10,11 +10,11 @@ td = React.DOM.td
 input = React.DOM.input
 
 Droplet = React.createClass({
-  deleteDroplet: function() {
+  dropletAction:function(id, action){
     urb.send({
-      appl: "do",
-      data: {action: 'delete',
-            id: this.props.id}})
+      appl:"do",
+      data: {action: action,
+            id: id}})
   },
 
   rebootDroplet: function() {
@@ -25,12 +25,20 @@ Droplet = React.createClass({
   },
 
   render: function() {
-    $this = this
+    var $this = this    //local var, else it always points at second
+    var acts = ["reboot","power_cycle","shutdown","power_off","power_on","password_reset",
+                "enable_ipv6","enable_private_networking","snapshot","upgrade"]
+    var buttons = [];
+    var buttons = acts.map(function(act){
+      console.log($this.props.id)
+      return b({onClick:function(){
+        $this.dropletAction($this.props.id, act)
+      }}, act)
+    })     
     kay = Object.keys(this.props)
     kay = kay.filter(function(b){return b!="children"}) //  XX individually adress props
   return div({},
-    b({id:this.props.id}, "delete"),
-    b({id:this.props.id,onClick:this.rebootDroplet}, "reboot"),
+    buttons,
     table({},
       tr({},kay.map(function(k){return th({},k)})),
       tr({},kay.map(function(k){return td({},JSON.stringify($this.props[k]))}))),
@@ -41,6 +49,7 @@ Droplet = React.createClass({
 Page = recl({
   handleClick: function(){
     if(window.authcode.length !== ''){
+      console.log(window.authcode);
       urb.send({
         appl: "do",
         data: window.authcode,
@@ -81,13 +90,13 @@ Page = recl({
   },
 
   render: function(){
-    href = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=d8f46b95af38c1ab3d78ad34c2157a6959c23eb0eb5d8e393f650f08e6a75c6f&redirect_uri=http%3A%2F%2Flocalhost%3A8443%2Fmain%2Fpub%2Fdo%2Ffab&response_type=code&redirect_uri=http://localhost:8443/main/pub/do/fab&scope=read+write"
-    return (div({}, [
+    href = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=d8f46b95af38c1ab3d78ad34c2157a6959c23eb0eb5d8e393f650f08e6a75c6f&redirect_uri=http%3A%2F%2Flocalhost%3A8443%2Fhome%2Fpub%2Fdo%2Ffab&response_type=code&scope=read+write"
+      return (div({}, [
         div({},
           a({href:href},[
-            "get authcode",  
-            b({onClick:this.handleClick}, "Send Authcode")
-          ])
+            "get authcode"
+          ]),
+          b({onClick:this.handleClick}, "Send Authcode")
         ),
         div({}, [
           input({id:"appsecret"}, 
