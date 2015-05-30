5c543bf2c8c46cc3b85cc2fd9db075a2e4a57799
Henry Ault <henry@tlon.io>
2015-05-29 19:45:56 -0700

    complete unified interface, actions & imgs left

--- urb/zod/base/pub/cloud/src/main.js
+++ urb/zod/base/pub/cloud/src/main.js
@@ -9,25 +9,130 @@ tr = React.DOM.tr
 td = React.DOM.td
 input = React.DOM.input
 
+function HashToJSON() {            
+    var pairs = window.location.hash.slice(1).split('&');
+    var result = {};
+    pairs.forEach(function(pair) {
+        pair = pair.split('=');
+        result[pair[0]] = decodeURIComponent(pair[1] || '');
+    });
+
+    return JSON.parse(JSON.stringify(result));
+}
+
+
+DOControls = React.createClass({
+  createDroplet: function(){
+    urb.send({appl: "cloud",
+              data: {
+                    action:'create-do',
+                    name:$('#name').val(),
+                    region:$('#region').val(),
+                    size:$('#size').val(),
+                    image:$('#image').val(),
+                    ssh:[], // $('#ssh').val()]
+                    backups:null,//$('#backups').val(),
+                    ipv6:null,//$('#ipv6').val(),
+                    priv_networking:null,//$('#priv-networking').val(),
+                    user_data:null//$('#user-data').val()
+                    },
+              mark: "json"})
+  },
+
+  render: function(){
+      href = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=d8f46b95af38c1ab3d78ad34c2157a6959c23eb0eb5d8e393f650f08e6a75c6f&redirect_uri=http%3A%2F%2Flocalhost%3A8443%2Fhome%2Fpub%2Fcloud%2Ffab&response_type=code&scope=read+write"
+    return (
+      div({}, [
+        div({},
+          a({href:href},"get authcode"),
+          b({onClick:this.props.handleClick('do')}, "Send Authcode")
+        ),
+        div({}, [
+          input({id:"appsecret"}, 
+          b({onClick:this.props.sendSecret('do','#appsecret')}, "Send Secret"))
+        ]),
+
+        div({}, [
+          b({onClick:this.createDroplet}, "Create Droplet"),
+          input({id:"name",placeholder:"Name of droplet"}), 
+          input({id:"region",placeholder:"Region"}),
+          input({id:"size",placeholder:"Size (str ending in mb"}),
+          input({id:"image",placeholder:"Image"}),
+          input({id:"ssh",placeholder:"ssh keys (optional)"}),
+          input({id:"backups",placeholder:"backups (optional)"}),
+          input({id:"ipv6",placeholder:"ipv6 (boolean, optional)"}),
+          input({id:"user-data",placeholder:" user-data string (optional)"}),
+          input({id:"priv-networking",placeholder:"Private Networking (boolean, optional)"})
+        ])
+      ])
+    )
+  }
+})
+
+GCEControls = React.createClass({
+  createDroplet: function(){
+  urb.send({
+    appl: 'cloud',
+    data: {action:'create-gce',
+          project:$('#project').val(),
+          zone:$('#zone').val(),
+          name:$('#gname').val(),
+          machine_type:$('#machine_type').val()
+          },
+    mark: 'json'})
+  },
+
+  createDisk: function(){
+      urb.send({
+        appl: 'cloud',
+        data: {action:'create-gce-disk',
+               snap:$('#gsnap').val(),
+               number:$('#number').val(),
+               name:$('#gcpName').val()},
+        mark: 'json'})
+  },
+
+  render: function(){
+    ghref = "https://accounts.google.com/o/oauth2/auth?response_type=token&scope=https://www.googleapis.com/auth/compute&redirect_uri=http://localhost:8443/home/pub/cloud/fab&client_id=719712694742-6htfj2t9s1j2jid92rc4dfq9psrr9qpo.apps.googleusercontent.com"
+    return(
+      div({}, [
+        div({}, [
+          b({onClick:this.createDisk}, 'Create Disk From Image'),
+          input({id:'gcpName',placeholder:'Name for GCE Disk and Instance'}),
+          input({id:'number',placeholder:'Number of instances'}),
+          input({id:'gsnap',placeholder:'Snapshot'})
+        ]),
+        div({}, [
+          a({href:ghref},"Get Google Authcode"),
+          b({onClick:this.props.handleClick('gce')}, "Send Google Authcode")
+        ]),
+        div({}, [
+          input({id:"gappsecret"}, 
+          b({onClick:this.props.sendSecret('gce','#gappsecret')}, "Send Google Secret"))
+        ]),
+        div({}, [
+          b({onClick:this.createDroplet}, "Create Droplet"),
+          input({id:"project",placeholder:"project"}),
+          input({id:"zone",placeholder:"zone"}),
+          input({id:"gname",placeholder:"Name of droplet"}), 
+          input({id:"machine_type",placeholder:"Machine Type"}),
+          //input({id:"image",placeholder:"Image"}),
+      ])
+    ]))
+  }
+})
+
 Droplet = React.createClass({
   dropletAction:function(id, action){
     urb.send({
       appl:"cloud",
-      data: {action: action,
-            id: id}})
-  },
-
-  rebootDroplet: function() {
-    urb.send({
-      appl: "cloud",
-      data: {action: 'reboot',
-            id: this.props.id}})
+      data: {action:action,
+            id:id}})
   },
 
   render: function() {
     var $this = this    //local var, else it always points at second
-    var acts = ["reboot","power_cycle","shutdown","power_off","power_on","password_reset",
-                "enable_ipv6","enable_private_networking","snapshot","upgrade"]
+    var acts = ["start","stop","reboot","delete"]
     var buttons = [];
     var buttons = acts.map(function(act){
       console.log($this.props.id)
@@ -50,10 +155,11 @@ Page = recl({
   handleClick: function(platform){
   return  function(){
       console.log(platform);
+      console.log(window.authcode.platform)
       if(window.authcode.length !== ''){
         urb.send({
           appl: "cloud",
-          data: {authcode:window.authcode,
+          data: {authcode:authcode[platform],
                 platform:platform},
           mark: "cloud-auth"})
       } else { console.log("nocode") }
@@ -79,68 +185,18 @@ Page = recl({
               mark: "json"})
   },
 
-  createDroplet: function(){
-    urb.send({appl: "cloud",
-              data: {
-                    action:'create',
-                    name:$('#name').val(),
-                    region:$('#region').val(),
-                    size:$('#size').val(),
-                    image:$('#image').val(),
-                    ssh:[], // $('#ssh').val()]
-                    backups:null,//$('#backups').val(),
-                    ipv6:null,//$('#ipv6').val(),
-                    priv_networking:null,//$('#priv-networking').val(),
-                    user_data:null//$('#user-data').val()
-                    },
-              mark: "json"})
-  },
 
   render: function(){
-    href = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=d8f46b95af38c1ab3d78ad34c2157a6959c23eb0eb5d8e393f650f08e6a75c6f&redirect_uri=http%3A%2F%2Flocalhost%3A8443%2Fhome%2Fpub%2Fcloud%2Ffab&response_type=code&scope=read+write"
-    ghref = "https://accounts.google.com/o/oauth2/auth?response_type=token&scope=https://www.googleapis.com/auth/compute&state=someinfo&redirect_uri=http://localhost:8443/home/pub/cloud/fab&client_id=720541965785-jr3c6ijo8abonu9qj77qre1itsdra52r.apps.googleusercontent.com"
-      return (div({}, [
-        div({},
-          a({href:href},[
-            "get authcode"
-          ]),
-          b({onClick:this.handleClick('do')}, "Send Authcode")
-        ),
-        div({}, [
-          input({id:"appsecret"}, 
-          b({onClick:this.sendSecret('do','#appsecret')}, "Send Secret"))
-        ]),
-        b({onClick:this.getList}, "Get List"),
-        div({},
-          a({href:ghref},[
-            "Get Google Authcode"
-          ]),
-          b({onClick:this.handleClick('gce','#gappsecret')}, "Send Google Authcode")
-        ),
-        div({}, [
-          input({id:"gappsecret"}, 
-          b({onClick:this.sendSecret('gce')}, "Send Google Secret"))
-        ]),
-        div({}, [
-          b({onClick:this.createDroplet}, "Create Droplet"),
-          input({id:"name",placeholder:"Name of droplet"}), 
-          input({id:"region",placeholder:"Region"}),
-          input({id:"size",placeholder:"Size (str ending in mb"}),
-          input({id:"image",placeholder:"Image"}),
-          input({id:"ssh",placeholder:"ssh keys (optional)"}),
-          input({id:"backups",placeholder:"backups (optional)"}),
-          input({id:"ipv6",placeholder:"ipv6 (boolean, optional)"}),
-          input({id:"user-data",placeholder:" user-data string (optional)"}),
-          input({id:"priv-networking",placeholder:"Private Networking (boolean, optional)"})
-        ]),
-        div({},
-          this.props.droplets.map(Droplet)
-        )
-      ])
-    )
+      return (div({},
+        DOControls({handleClick:this.handleClick,sendSecret:this.sendSecret}), 
+        GCEControls({handleClick:this.handleClick,sendSecret:this.sendSecret}),
+        this.props.droplets.map(Droplet)
+      ))
   }
 })
 
+var hash = HashToJSON()     //pull out hash of query string for gce authcode
+authcode.gce = hash.access_token
 
 mounted = React.render(Page({droplets:[]}), $("#container")[0])
 urb.bind("/", function(err,d) {
