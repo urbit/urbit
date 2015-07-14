055233afd1369fc0281ad8e6943568ddb6ca9315
Anton Dyudin <ohAitch@gmail.com>
2015-07-13 18:20:09 -0700

    overly zealous error reporting

--- urb/zod/lib/urb.js
+++ urb/zod/lib/urb.js
@@ -33,7 +33,7 @@ window.urb.req = function(method,url,params,json,cb) {
         if(res.data.reload)
           res.reload = res.data.reload
       } catch(e) {
-        if(urb.wall !== false) document.write(this.responseText) // XX
+        // if(urb.wall !== false) document.write(this.responseText) // XX
         err = {
           message:"Failed to parse JSON",
           raw:this.responseText
