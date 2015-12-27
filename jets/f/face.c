8d1e7e11a07f0015b939d24a5d6464b6861a2a62
C. Guy Yarvin <cgyarvin@gmail.com>
2015-12-27 00:43:40 -0500

    New powers for $face.

--- jets/f/face.c
+++ jets/f/face.c
@@ -7,24 +7,24 @@
 /* functions
 */
   u3_noun
-  u3qf_face(u3_noun cog,
+  u3qf_face(u3_noun sag,
             u3_noun tip)
   {
     if ( c3__void == tip ) {
       return c3__void;
     }
     else return u3nt(c3__face,
-                     u3k(cog),
+                     u3k(sag),
                      u3k(tip));
   }
   u3_noun
   u3wf_face(u3_noun cor)
   {
-    u3_noun cog, tip;
+    u3_noun sag, tip;
 
-    if ( c3n == u3r_mean(cor, u3x_sam_2, &cog, u3x_sam_3, &tip, 0) ) {
+    if ( c3n == u3r_mean(cor, u3x_sam_2, &sag, u3x_sam_3, &tip, 0) ) {
       return u3m_bail(c3__fail);
     } else {
-      return u3qf_face(cog, tip);
+      return u3qf_face(sag, tip);
     }
   }
