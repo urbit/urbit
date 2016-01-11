b86fb544da14508472049d570b6647c15505495f
C. Guy Yarvin <cgyarvin@gmail.com>
2016-01-10 19:10:47 -0800

    Double solid; about to incrementally refrog.

--- jets/f/fork.c
+++ jets/f/fork.c
@@ -35,6 +35,34 @@
   }
 
   u3_noun
+  u3qf_forq(u3_noun hoz,
+            u3_noun bur)
+  {
+    if ( c3y == u3r_sing(hoz, bur) ) {
+      return u3k(hoz);
+    }
+    else if ( c3__void == bur ) {
+      return u3k(hoz);
+    }
+    else if ( c3__void == hoz ) {
+      return u3k(bur);
+    }
+    else return u3nq
+      (c3__frog, u3k(hoz), u3k(bur), u3_nul);
+  }
+  u3_noun
+  u3wf_forq(u3_noun cor)
+  {
+    u3_noun hoz, bur;
+
+    if ( c3n == u3r_mean(cor, u3x_sam_2, &hoz, u3x_sam_3, &bur, 0) ) {
+      return u3m_bail(c3__fail);
+    } else {
+      return u3qf_forq(hoz, bur);
+    }
+  }
+
+  u3_noun
   u3qf_frog(u3_noun yed)
   {
     u3_noun lez = u3_nul;
--- jets/f/ut_find.c
+++ jets/f/ut_find.c
@@ -3,7 +3,52 @@
 */
 #include "all.h"
 
+  static u3_noun
+  _find_outer(u3_noun van,
+              u3_noun sut,
+              u3_noun dep,
+              u3_noun way,
+              u3_noun cug,
+              u3_noun gil)
+  {
+  }
 
+{
+  u3_noun p_sut, q_sut, r_sut;
+
+  if ( c3n == u3du(sut) ) switch ( sut ) {
+    default: return u3m_bail(c3__fail);
+
+    case c3__noun:
+    {
+    }
+    case c3__void:
+    {
+    }
+  }
+  else switch ( u3h(sut) ) {
+    default: return u3m_bail(c3__fail);
+
+    case c3__atom: u3x_cell(u3t(sut), &p_sut, &q_sut);
+    {
+    }
+    case c3__cell: u3x_cell(u3t(sut), &p_sut, &q_sut);
+    {
+    }
+    case c3__core: u3x_cell(u3t(sut), &p_sut, &q_sut);
+    {
+    }
+    case c3__face: u3x_cell(u3t(sut), &p_sut, &q_sut);
+    {
+    }
+    case c3__fork: u3x_cell(u3t(sut), &p_sut, &q_sut);
+    {
+    }
+    case c3__hold: p_sut = u3t(sut);
+    {
+    }
+  }
+}
 
   static u3_noun
   _find_in(u3_noun van,
