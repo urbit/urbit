fda8ff38a49d9232c2627b36c87ac853e16469bb
Raymond Pasco <ray@the.ug>
2016-01-28 08:28:13 -0500

    support U3_LIB define

--- vere/main.c
+++ vere/main.c
@@ -206,6 +206,10 @@ _main_getopt(c3_i argc, c3_c** argv)
       u3_Host.ops_u.pil_c = strdup("urbit.pill");
     } else if ( stat("urb/urbit.pill", &s) == 0 ) {
       u3_Host.ops_u.pil_c = strdup("urb/urbit.pill");
+#ifdef U3_LIB
+    } else if ( stat(U3_LIB"/urbit.pill", &s) == 0 ) {
+      u3_Host.ops_u.pil_c = strdup(U3_LIB"/urbit.pill");
+#endif
     } else {
       fprintf(stderr, "Could not find urbit.pill\n");
       return c3n;
