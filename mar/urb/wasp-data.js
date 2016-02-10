c95bd7af46e55a17ea358e941b5938bd26c0b359
Anton Dyudin <ohAitch@gmail.com>
2016-02-09 21:13:05 -0800

    wasp-data debug mode urb.verb

--- mar/urb/wasp-data.js
+++ mar/urb/wasp-data.js
@@ -1,3 +1,8 @@
+// debugging 
+urb.verb = false
+urb.sources = {}
+urb.deps.map(function(a){urb.sources[a] = "dep"})
+
 urb.waspElem = function(ele){
   url = ele.src || ele.href
   if(!url || (new URL(url)).host != document.location.host)
@@ -9,8 +14,12 @@ urb.waspUrl = function(url){
   xhr.open("HEAD", url)
   xhr.send()
   xhr.onload = urb.waspLoadedXHR
+  xhr.channel = url
 }
-urb.waspLoadedXHR = function(){urb.wasp(urb.getXHRWasp(this))}
+urb.waspLoadedXHR = function(){
+  urb.sources[urb.getXHRWasp(this)] = this.channel
+  urb.wasp(urb.getXHRWasp(this))
+}
 urb.getXHRWasp = function(xhr){
   var dep = xhr.getResponseHeader("etag")
   if(dep) return JSON.parse(dep.substr(2))
@@ -25,6 +34,8 @@ urb.ondataupdate = urb.onupdate  // overridable
 
 var _onupdate = urb.onupdate
 urb.onupdate = function(dep){
+  if(urb.verb)
+    console.log("update", urb.datadeps[dep] ? "data" : "full", dep, urb.sources[dep])
   if(urb.datadeps[dep]) urb.ondataupdate(dep)
   else _onupdate(dep)
 }
