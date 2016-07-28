607ef00903a7d44f5eae2a856079f3fb5f7323c5
Raymond Pasco <ray@the.ug>
2016-07-28 12:26:04 -0700

    De-C++ify murmur3 for loops

--- outside/murmur3/MurmurHash3.c
+++ outside/murmur3/MurmurHash3.c
@@ -96,6 +96,7 @@ void MurmurHash3_x86_32 ( const void * key, int len,
 {
   const uint8_t * data = (const uint8_t*)key;
   const int nblocks = len / 4;
+  int i;
 
   uint32_t h1 = seed;
 
@@ -107,7 +108,7 @@ void MurmurHash3_x86_32 ( const void * key, int len,
 
   const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
 
-  for(int i = -nblocks; i; i++)
+  for(i = -nblocks; i; i++)
   {
     uint32_t k1 = getblock32(blocks,i);
 
@@ -152,6 +153,7 @@ void MurmurHash3_x86_128 ( const void * key, const int len,
 {
   const uint8_t * data = (const uint8_t*)key;
   const int nblocks = len / 16;
+  int i;
 
   uint32_t h1 = seed;
   uint32_t h2 = seed;
@@ -168,7 +170,7 @@ void MurmurHash3_x86_128 ( const void * key, const int len,
 
   const uint32_t * blocks = (const uint32_t *)(data + nblocks*16);
 
-  for(int i = -nblocks; i; i++)
+  for(i = -nblocks; i; i++)
   {
     uint32_t k1 = getblock32(blocks,i*4+0);
     uint32_t k2 = getblock32(blocks,i*4+1);
@@ -257,6 +259,7 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
 {
   const uint8_t * data = (const uint8_t*)key;
   const int nblocks = len / 16;
+  int i;
 
   uint64_t h1 = seed;
   uint64_t h2 = seed;
@@ -269,7 +272,7 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
 
   const uint64_t * blocks = (const uint64_t *)(data);
 
-  for(int i = 0; i < nblocks; i++)
+  for(i = 0; i < nblocks; i++)
   {
     uint64_t k1 = getblock64(blocks,i*2+0);
     uint64_t k2 = getblock64(blocks,i*2+1);
