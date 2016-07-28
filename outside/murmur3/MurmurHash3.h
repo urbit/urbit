36781ab5c6b719dcd1b5d1f51b1d34e3aef2a017
Raymond Pasco <ray@the.ug>
2016-07-28 12:32:35 -0700

    De-C++ify murmur3 comments

--- outside/murmur3/MurmurHash3.h
+++ outside/murmur3/MurmurHash3.h
@@ -1,14 +1,14 @@
-//-----------------------------------------------------------------------------
-// MurmurHash3 was written by Austin Appleby, and is placed in the public
-// domain. The author hereby disclaims copyright to this source code.
+/*
+   MurmurHash3 was written by Austin Appleby, and is placed in the public
+   domain. The author hereby disclaims copyright to this source code.
+*/
 
 #ifndef _MURMURHASH3_H_
 #define _MURMURHASH3_H_
 
-//-----------------------------------------------------------------------------
-// Platform-specific functions and macros
+/* Platform-specific functions and macros */
 
-// Microsoft Visual Studio
+/* Microsoft Visual Studio */
 
 #if defined(_MSC_VER) && (_MSC_VER < 1600)
 
@@ -16,15 +16,15 @@ typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef unsigned __int64 uint64_t;
 
-// Other compilers
+/* Other compilers */
 
-#else	// defined(_MSC_VER)
+#else	/* defined(_MSC_VER) */
 
 #include <stdint.h>
 
-#endif // !defined(_MSC_VER)
+#endif /* !defined(_MSC_VER) */
 
-//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
 
 void MurmurHash3_x86_32  ( const void * key, int len, uint32_t seed, void * out );
 
@@ -32,6 +32,6 @@ void MurmurHash3_x86_128 ( const void * key, int len, uint32_t seed, void * out 
 
 void MurmurHash3_x64_128 ( const void * key, int len, uint32_t seed, void * out );
 
-//-----------------------------------------------------------------------------
+/*---------------------------------------------------------------------------*/
 
-#endif // _MURMURHASH3_H_
+#endif /* _MURMURHASH3_H_ */
