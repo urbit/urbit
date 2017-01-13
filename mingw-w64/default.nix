bdfcbaa54b0983355714951d24863c9ab9b4841a
David Grayson <davidegrayson@gmail.com>
2017-01-12 20:14:38 -0800

    Added mingw-w64 headers.

--- mingw-w64/default.nix
+++ mingw-w64/default.nix
@@ -1,6 +1,23 @@
 { nixpkgs, arch }:
 let
   binutils = import ./binutils { inherit nixpkgs arch; };
+
+  mingw_w64 = rec {
+    version = "4.0.6";
+    src = nixpkgs.fetchurl {
+      url = "mirror://sourceforge/mingw-w64/mingw-w64-v${version}.tar.bz2";
+      sha256 = "0p01vm5kx1ixc08402z94g1alip4vx66gjpvyi9maqyqn2a76h0c";
+    };
+  };
+
+  mingw_w64_headers = nixpkgs.stdenv.mkDerivation {
+    name = "mingw-w64-${mingw_w64.version}-headers";
+    src = mingw_w64.src;
+    preConfigure = "cd mingw-w64-headers";
+    configureFlags = "--without-crt";
+  };
+
 in {
   inherit binutils nixpkgs arch;
+  inherit mingw_w64 mingw_w64_headers;
 }
