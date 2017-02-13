56cdffdc6c08eb13ffc2f783153b44e9a380663b
David Grayson <davidegrayson@gmail.com>
2017-02-13 09:41:36 -0800

    Some changes to mingw-w64 that probably should have been committed before.

--- mingw-w64/builder.sh
+++ mingw-w64/builder.sh
@@ -2,9 +2,10 @@ source $stdenv/setup
 
 unset CC
 
-tar -xf $src
+cp -r $src mingw-w64
+chmod -R u+w mingw-w64
 
-cd mingw-w64-v$version
+cd mingw-w64
 for patch in $patches
 do
   echo applying patch $patch
@@ -14,7 +15,7 @@ cd ..
 
 mkdir build_crt_and_headers
 cd build_crt_and_headers
-../mingw-w64-v$version/configure \
+../mingw-w64/configure \
     --host=$host --prefix=$out
 make
 make install
@@ -22,7 +23,7 @@ cd ..
 
 mkdir build_winpthreads
 cd build_winpthreads
-LDFLAGS="-L${out}/lib" ../mingw-w64-v$version/mingw-w64-libraries/winpthreads/configure \
+LDFLAGS="-L${out}/lib" ../mingw-w64/mingw-w64-libraries/winpthreads/configure \
     --disable-shared --enable-static \
     --host=$host --prefix=$out
 make
