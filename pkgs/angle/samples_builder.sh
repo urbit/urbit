dafddf492e089ee901889bbadd1478292f1cf3ce
David Grayson <davidegrayson@gmail.com>
2017-02-15 20:17:46 -0800

    angle-samples: Got hello_triangle to compile but not link.

--- pkgs/angle/samples_builder.sh
+++ pkgs/angle/samples_builder.sh
@@ -4,12 +4,14 @@ cp -r $src/samples .
 cp -r $src/util .
 
 mkdir include
+mkdir include/common
+cp -r $src/src/common/{angleutils,platform,Optional}.h include/common/
 cp -r $src/include/export.h include/
-cp -r $src/util/* include/
+cp -r $src/util/{OSWindow,Event,keyboard,mouse,Timer,EGLWindow,shader_utils,random_utils}.h include/
 cp -r $src/samples/sample_util/* include/
 
 mkdir build
 cd build
 mkdir bin lib
 
-$host-g++ -I../include -I"$angle/include" -L"$angle/lib" ../samples/hello_triangle/HelloTriangle.cpp -o bin/hello_triangle
+$host-g++ -I../include -I"$angle/include" -L"$angle/lib" -DGL_GLEXT_PROTOTYPES ../samples/hello_triangle/HelloTriangle.cpp ../samples/sample_util/SampleApplication.cpp -lGLESv2 -lEGL -o bin/hello_triangle
