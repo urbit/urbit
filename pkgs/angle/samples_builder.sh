d0527b92a853de0a74a4542457df935bfade20f1
David Grayson <davidegrayson@gmail.com>
2017-02-17 08:36:48 -0800

    angle_samples: Compiled three more samples but they all quit immediately at run time.

--- pkgs/angle/samples_builder.sh
+++ pkgs/angle/samples_builder.sh
@@ -22,6 +22,28 @@ CFLAGS="$CFLAGS -DGL_APICALL= -DANGLE_EXPORT= -DGL_GLEXT_PROTOTYPES"
 LDFLAGS="-L$angle_util/lib -L$angle/lib"
 LIBS="-langle_util -lEGL_static -lGLESv2_static -lANGLE -ltranslator -lpreprocessor -langle_image_util -langle_common -ld3d9 -lgdi32"
 
+echo "compiling texture_wrap"
+$host-g++ -g $CFLAGS $LDFLAGS \
+  ../samples/texture_wrap/TextureWrap.cpp \
+  ../samples/sample_util/texture_utils.cpp \
+  ../samples/sample_util/SampleApplication.cpp \
+  $LIBS -o bin/texture_wrap${exe_suffix}
+
+echo "compiling simple_texture_2d"
+$host-g++ -g $CFLAGS $LDFLAGS \
+  ../samples/simple_texture_2d/SimpleTexture2D.cpp \
+  ../samples/sample_util/texture_utils.cpp \
+  ../samples/sample_util/SampleApplication.cpp \
+  $LIBS -o bin/simple_texture_2d${exe_suffix}
+
+echo "compiling particle_system"
+$host-g++ -g $CFLAGS $LDFLAGS \
+  ../samples/particle_system/ParticleSystem.cpp \
+  ../samples/sample_util/tga_utils.cpp \
+  ../samples/sample_util/SampleApplication.cpp \
+  $LIBS -o bin/particle_system${exe_suffix}
+cp ../samples/particle_system/smoke.tga bin/
+
 echo "compiling hello_triangle"
 $host-g++ -g $CFLAGS $LDFLAGS \
   ../samples/hello_triangle/HelloTriangle.cpp \
