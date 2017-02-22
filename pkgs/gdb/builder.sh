d484e208aa1b73b7032d79e361f6bc0438dcd24e
David Grayson <davidegrayson@gmail.com>
2017-02-21 21:05:44 -0800

    Got gdb to build successfully.

--- pkgs/gdb/builder.sh
+++ pkgs/gdb/builder.sh
@@ -1,6 +1,7 @@
 source $stdenv/setup
 
 unset CC
+unset CXX
 unset LD
 
 tar -xf $src
@@ -8,14 +9,16 @@ tar -xf $src
 mkdir build
 cd build
 
+# TODO: enable the text UI some day
 ../gdb-$version/configure \
+  --prefix=$out \
   --host=$host \
   --target=$host \
   --with-zlib=$zlib \
   --with-expat=$expat \
   --with-readline=$readline \
   --with-curses=$pdcurses \
-  --disable-tui \  # TODO: enable the text UI some day
+  --disable-tui \
   --disable-win32-registry \
   --disable-rpath
 
