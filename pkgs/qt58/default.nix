2e2bf059c2dffc999942499ea3c7ee5e32f5c658
David Grayson <davidegrayson@gmail.com>
2017-03-17 07:57:07 -0700

    qt58: Simplify the patch situation.  This should still build up to the point where it needs the special version of pkg-config.

--- pkgs/qt58/default.nix
+++ pkgs/qt58/default.nix
@@ -3,49 +3,6 @@
 let
   version = "5.8.0";
 
-  patches = [
-    # ./msys2-patches/0001-Add-link-time-optimization-support-for-Clang-GCC-and.patch
-    # ./msys2-patches/0001-qt-5.8.0-fix-sql-libraries-mingw.patch
-    # ./msys2-patches/0002-qt-5.8.0-configure-gcc-before-clang.patch
-    # ./msys2-patches/0003-qt-5.8.0-autodetect-fontconfig.patch
-    # ./msys2-patches/0005-qt-5.3.0-syncqt-fix.patch
-    # ./msys2-patches/0006-qt-5.3.0-win_flex-replace.patch
-    # ./msys2-patches/0007-qt-5.3.0-win32-g-Enable-static-builds.patch
-    # ./msys2-patches/0008-qt-5.3.0-win32-g-Add-QMAKE_EXTENSION_IMPORTLIB-defaulting-to-.patch
-    # ./msys2-patches/0009-qt-5.3.0-qmlimportscanner-Ensure-the-correct-variant-is-run.patch
-    # ./msys2-patches/0010-qt-5.3.0-qdoc-increase-stack-size-for-win32-g-too.patch
-    # ./msys2-patches/0011-qt-5.8.0-mingw-dbus-and-pkg-config.patch
-    # ./msys2-patches/0016-qt-5.8.0-win32-g++-use-qpa-genericunixfontdatabase.patch
-    # ./msys2-patches/0017-qt-5.3.0-fix-examples-building.patch
-    # ./msys2-patches/0020-qt-5.3.0-use-external-angle-library.patch
-    # ./msys2-patches/0023-qt-5.3.0-env-set-external-angle.patch
-    # ./msys2-patches/0024-qt-5.3.0-icu-add-U_LIB_SUFFIX_C_NAME.patch
-    # ./msys2-patches/0025-qt-5.8.0-force-using-make-on-msys.patch
-    # ./msys2-patches/0028-qt-5.8.0-Revert-untangle-use-of-system-vs.-shell-path-list-se.patch
-    # ./msys2-patches/0029-qt-5.8.0-Revert-fix-quoting-and-path-separators-in-qtPrepareT.patch
-    # ./msys2-patches/0030-qt-5.3.1-workaround-ansidecl-h-PTR-define-conflict.patch
-    # ./msys2-patches/0031-qt-5.5.0-workaround-BOOL-define-conflict.patch
-    # ./msys2-patches/0034-qt-5.3.2-Use-QMAKE_PREFIX_STATICLIB-in-create_cmake-prf.patch
-    # ./msys2-patches/0035-qt-5.3.2-dont-add-resource-files-to-qmake-libs.patch
-    # ./msys2-patches/0036-qt-5.3.2-win32-qt5-static-cmake-link-ws2_32-and--static.patch
-    # ./msys2-patches/0037-qt-5.4.0-Improve-cmake-plugin-detection-as-not-all-are-suffixed-Plugin.patch
-    # ./msys2-patches/0038-qt-5.5.0-cmake-Rearrange-STATIC-vs-INTERFACE-targets.patch
-    # ./msys2-patches/0039-qt-5.4.0-Make-it-possible-to-use-static-builds-of-Qt-with-CMa.patch
-    # ./msys2-patches/0040-qt-5.4.0-Generate-separated-libraries-in-prl-files-for-CMake.patch
-    # ./msys2-patches/0041-qt-5.4.0-Fix-mingw-create_cmake-prl-file-has-no-lib-prefix.patch
-    # ./msys2-patches/0042-qt-5.4.0-static-cmake-also-link-plugins-and-plugin-deps.patch
-    # ./msys2-patches/0043-qt-5.5.0-static-cmake-regex-QT_INSTALL_LIBS-in-QMAKE_PRL_LIBS_FOR_CMAKE.patch
-    # ./msys2-patches/0044-qt-5.4.0-win32-g++-enable-qtwebengine-build.patch
-    # ./msys2-patches/0045-qt-5.4.1-static-use-qminimal-platform-plugin-for-qcollectiongenerator.patch
-    # ./msys2-patches/0046-qt-5.4.1-Revert-Revert-fix-NTFS-mount-points.patch
-    # ./msys2-patches/0048-qt-5.4.2-win32-Avoid-platformNativeInterface-segfaults-with-minimal-platform.patch
-    # ./msys2-patches/0049-windows-not-use-fontconfig.patch
-    # ./msys2-patches/0050-qt-5.5.1-fix-the-trailing-backslash-in-DESTDIR-on-mingw-worka.patch
-    # ./msys2-patches/0125-qt5-windeployqt-fixes.patch
-    ./msys2-patches/0300-qt-5.8.0-cast-errors.patch
-    # ./msys2-patches/qt-5.3.0-static-qmake-conf.patch
-  ];
-
   # TODO: patch qt to not use /bin/pwd, test building it in a sandbox
 
   base = crossenv.nixpkgs.stdenv.mkDerivation rec {
@@ -57,11 +14,7 @@ let
     };
 
     patches = [
-      ./qtbase-patches/add-profile.patch
-
-      # We can get rid of this patch once mingw-w64 is fixed.
-      ./qtbase-patches/direct2d-const-error.patch
-
+      ./qtbase-arch-patches/0001-Add-profile-for-cross-compilation-with-mingw-w64.patch
       ./qtbase-arch-patches/0002-Ensure-GLdouble-is-defined-when-using-dynamic-OpenGL.patch
       ./qtbase-arch-patches/0003-Use-external-ANGLE-library.patch
       ./qtbase-arch-patches/0004-Fix-too-many-sections-assemler-error-in-OpenGL-facto.patch
@@ -97,7 +50,7 @@ let
 
     inherit (crossenv) host;
 
-    buildInputs = [ crossenv.gcc crossenv.binutils ];
+    buildInputs = [ crossenv.gcc crossenv.binutils ]; # TODO: crossenv.pkg-config
 
     builder = ./builder.sh;
 
