9e2ae31b344e3bf44f66359f61dd438a32a80e27
David Grayson <davidegrayson@gmail.com>
2017-03-17 20:17:39 -0700

    qt58: Successfully built and ran the qt58 rasterwindow example!!

--- pkgs/qt58/examples_builder.sh
+++ pkgs/qt58/examples_builder.sh
@@ -7,25 +7,41 @@ rmdir qtbase-opensource-src-$version
 
 mkdir build
 cd build
-mkdir bin moc
+mkdir bin moc obj
 
 # TODO: use $host-pkg-config to get the compiler settings
+
+cat > obj/plugins.cpp <<EOF
+#include <QtPlugin>
+Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin);
+EOF
+
+echo "compiling reference to plugins"
+$host-g++ \
+  -I $qtbase/include \
+  -I $qtbase/include/QtCore \
+  -c obj/plugins.cpp \
+  -o obj/plugins.o
 
 echo "compiling rasterwindow"
 $qtbase/bin/moc ../examples/gui/rasterwindow/rasterwindow.h > moc/rasterwindow.cpp
 $host-g++ \
+  -mwindows \
   -I . \
   -I $qtbase/include/ \
   -I $qtbase/include/QtGui \
+  -I $qtbase/include/QtCore \
   -L $qtbase/lib \
   -L $qtbase/plugins/platforms \
   ../examples/gui/rasterwindow/rasterwindow.cpp \
   ../examples/gui/rasterwindow/main.cpp \
   moc/rasterwindow.cpp \
-  -lQt5Gui -lQt5Core \
-  -lmpr -lwinmm -lqtpcre -lqtlibpng -lqtharfbuzz \
-  -lole32 -luuid -lwinmm -lws2_32 \
+  obj/plugins.o \
   -lqwindows -lqminimal -lqdirect2d -lqoffscreen \
+  -lQt5Gui -lQt5Widgets -lQt5ThemeSupport -lQt5FontDatabaseSupport \
+  -lQt5EventDispatcherSupport -lQt5Core \
+  -lmpr -lwinmm -lqtpcre -lqtlibpng -lqtharfbuzz  -lqtfreetype \
+  -lole32 -luuid -lwinmm -lws2_32 -loleaut32 -limm32 -ldwmapi \
   -o bin/rasterwindow${exe_suffix}
 
 mkdir $out
