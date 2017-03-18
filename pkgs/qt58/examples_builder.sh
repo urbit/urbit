667c7594675aaf8ceca8c84f0428fb06854f914e
David Grayson <davidegrayson@gmail.com>
2017-03-17 19:31:06 -0700

    qtbase-examples: Got the rasterwindow example to compile, but it cannot run because it does not find the windows plugin.

--- pkgs/qt58/examples_builder.sh
+++ pkgs/qt58/examples_builder.sh
@@ -7,17 +7,28 @@ rmdir qtbase-opensource-src-$version
 
 mkdir build
 cd build
+mkdir bin moc
 
 # TODO: use $host-pkg-config to get the compiler settings
 
 echo "compiling rasterwindow"
+$qtbase/bin/moc ../examples/gui/rasterwindow/rasterwindow.h > moc/rasterwindow.cpp
 $host-g++ \
+  -I . \
   -I $qtbase/include/ \
   -I $qtbase/include/QtGui \
   -L $qtbase/lib \
-  ../examples/gui/rasterwindow/*.cpp \
+  -L $qtbase/plugins/platforms \
+  ../examples/gui/rasterwindow/rasterwindow.cpp \
+  ../examples/gui/rasterwindow/main.cpp \
+  moc/rasterwindow.cpp \
   -lQt5Gui -lQt5Core \
   -lmpr -lwinmm -lqtpcre -lqtlibpng -lqtharfbuzz \
   -lole32 -luuid -lwinmm -lws2_32 \
-  -o rasterwindow${exe_suffix}
+  -lqwindows -lqminimal -lqdirect2d -lqoffscreen \
+  -o bin/rasterwindow${exe_suffix}
 
+mkdir $out
+
+cp -r bin $out
+# TODO: cp -r $qtbase/license $out
