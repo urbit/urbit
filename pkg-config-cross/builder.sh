1d2862912564910bffc8b02aa4733d41d8c127b5
David Grayson <davidegrayson@gmail.com>
2017-03-23 18:49:49 -0700

    Minor changes to pkg-config-cross.  Made it use exec.

--- pkg-config-cross/builder.sh
+++ pkg-config-cross/builder.sh
@@ -3,9 +3,9 @@ source $stdenv/setup
 mkdir -p $out/bin
 
 cat > $out/bin/$host-pkg-config <<EOF
-PKG_CONFIG_LIBDIR=\$${host_as_var}_PKG_CONFIG_LIBDIR \
-PKG_CONFIG_PATH=\$${host_as_var}_PKG_CONFIG_PATH \
-pkg-config
+PKG_CONFIG_LIBDIR=\$${host_as_var}_PKG_CONFIG_LIBDIR \\
+PKG_CONFIG_PATH=\$${host_as_var}_PKG_CONFIG_PATH \\
+exec pkg-config \$@
 EOF
 
 chmod a+x $out/bin/$host-pkg-config
