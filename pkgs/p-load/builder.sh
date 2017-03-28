fe31445722cb5e010a350f7c19172c77c06371b3
David Grayson <davidegrayson@gmail.com>
2017-03-28 09:55:29 -0700

    Use the latest version of p-load because it can handle static libusbp.

--- pkgs/p-load/builder.sh
+++ pkgs/p-load/builder.sh
@@ -3,11 +3,12 @@ echo $stdenv
 source $stdenv/setup
 
 tar -xf $src
+mv p-load-* p-load
 
 mkdir build
 cd build
 
-cmake ../p-load-$version \
+cmake ../p-load \
   -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain \
   -DCMAKE_INSTALL_PREFIX=$out
 
--- pkgs/p-load/default.nix
+++ pkgs/p-load/default.nix
@@ -3,14 +3,14 @@
 crossenv.nixpkgs.stdenv.mkDerivation rec {
   name = "p-load-${version}-${crossenv.host}";
 
-  version = "2.0.1";
+  version = "2.0.1ish";
 
   host = crossenv.host;
   cmake_toolchain = crossenv.cmake_toolchain;
 
   src = crossenv.nixpkgs.fetchurl {
-    url = "https://github.com/pololu/p-load/archive/${version}.tar.gz";
-    sha256 = "0gj55nap8xm3q97hkllzi6ni8razx1xh98bivfzlp5ipxf0w1qxp";
+    url = "https://github.com/pololu/p-load/archive/31126da.tar.gz";
+    sha256 = "1rha46s1jq6060c9mkki3rra9s4hsg2vz4cakcq91d98zb6i63ln";
   };
 
   buildInputs = [
