c4732d070638ede6a7acc20172bd457c6ba39dd7
David Grayson <davidegrayson@gmail.com>
2017-04-30 07:49:09 -0700

    pavr2: Work in progress: trying to compile the GUI, currently getting CMake errors from Qt's cmake files.
    This commit breaks the pavr2 build: it was previously working but not
    compiling the GUI.

--- pkgs/pavr2/builder.sh
+++ pkgs/pavr2/builder.sh
@@ -3,14 +3,20 @@ source $setup
 tar -xf $src
 mv pololu-usb-avr-programmer-v2-* pavr2
 
+cd pavr2
+for patch in $patches; do
+  echo applying patch $patch
+  patch -p1 -i $patch
+done
+cd ..
+
 mkdir build
 cd build
 
 PKG_CONFIG=$PKG_CONFIG_CROSS \
 cmake ../pavr2 \
   -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain \
-  -DCMAKE_INSTALL_PREFIX=$out \
-  -DENABLE_GUI=false   # TODO: enable the GUI
+  -DCMAKE_INSTALL_PREFIX=$out
 
 make
 
--- pkgs/pavr2/default.nix
+++ pkgs/pavr2/default.nix
@@ -10,7 +10,12 @@ crossenv.make_derivation rec {
     sha256 = "0mh3qyvk4k5i82zcf4q0anvm8aq1b684v91ss2kqhz80fafm6gy3";
   };
 
+  patches = [ ./megapatch.patch ];
+
   builder = ./builder.sh;
 
   cross_inputs = [ libusbp ];
+
+  # TODO: this pattern is bad because projects building native apps will find Qt
+  CMAKE_PREFIX_PATH = "${qt.base}";
 }
