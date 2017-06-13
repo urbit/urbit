f29af08f67c823bc4cf3155036cf01033e81f799
C. Guy Yarvin <cgyarvin@gmail.com>
2017-06-13 07:50:33 -0700

    Stash.

--- vere/king.c
+++ vere/king.c
@@ -326,8 +326,14 @@ _boothack_cb(uv_connect_t *conn, int status)
   if ( c3y == u3_Host.ops_u.fak ) {
     fprintf(stderr, "boot: F A K E ship with null security\r\n");
 
-    sec = 0;
-    tic = 0;
+    if ( u3_Host.ops_u.tic_c ) {
+      fprintf(stderr, "boot: fake ship doesn't need a ticket\r\n");
+      exit(1);
+    }
+    else {
+      sec = 0;
+      tic = u3k(who);
+    }
   }
   else {
     if ( 0 == u3_Host.ops_u.sec_c ) {
@@ -369,7 +375,6 @@ _boothack_cb(uv_connect_t *conn, int status)
       u3z(tuc);
     }
   }
-  tic = u3k(sec);
   pax = u3i_string(u3_Host.dir_c);
 
   mat = u3ke_jam(u3nc(c3__doom,
