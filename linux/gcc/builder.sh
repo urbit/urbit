d1eda295a7fed4c2a90773728a581de9a900c843
David (Pololu) <dev-david@pololu.com>
2017-07-13 18:49:21 -0700

    linux gcc: Stop patching linux.

--- linux/gcc/builder.sh
+++ linux/gcc/builder.sh
@@ -15,12 +15,6 @@ cd ..
 
 tar -xf $linux_src
 mv linux-$linux_version linux
-cd linux
-for patch in $patch_dir/linux-$linux_version/*; do
-  echo applying patch $patch
-  patch -p1 -i $patch
-done
-cd ..
 
 tar -xf $musl_src
 mv musl-$musl_version musl
@@ -48,15 +42,15 @@ mkdir obj_toolchain
 mkdir obj_musl
 
 MAKE="make MULTILIB_OSDIRNAMES= INFO_DEPS= infodir= ac_cv_prog_lex_root=lex.yy.c MAKEINFO=false"
-FULL_TOOLCHAIN_CONFIG="$gcc_conf --with-sysroot=/${host} --with-build-sysroot=$(pwd)/obj_sysroot "
-FULL_MUSL_CONFIG="$musl_conf CC=../obj_toolchain/gcc/xgcc\ -B\ ../obj_toolchain/gcc LIBCC=../obj_toolchain/$TARGET/libgcc/libgcc.a"
+gcc_conf="$gcc_conf --with-sysroot=/${host} --with-build-sysroot=$(pwd)/obj_sysroot "
+musl_conf="$musl_conf CC=../obj_toolchain/gcc/xgcc\ -B\ ../obj_toolchain/gcc LIBCC=../obj_toolchain/$TARGET/libgcc/libgcc.a"
 
 cd obj_toolchain
-../src_toolchain/configure $FULL_TOOLCHAIN_CONFIG
+../src_toolchain/configure $gcc_conf
 cd ..
 $MAKE -C obj_toolchain MAKE="$MAKE" all-gcc
 cd obj_musl
-bash -c "../src_musl/configure $FULL_MUSL_CONFIG"
+bash -c "../src_musl/configure $musl_conf"
 cd ..
 $MAKE -C obj_musl DESTDIR=$(pwd)/obj_sysroot install-headers
 $MAKE -C obj_toolchain MAKE="$MAKE" all-target-libgcc
@@ -64,7 +58,7 @@ $MAKE -C obj_musl
 $MAKE -C obj_musl DESTDIR=$(pwd)/obj_sysroot install
 $MAKE -C obj_toolchain MAKE="$MAKE"
 mkdir -p obj_kernel_headers/staged
-$MAKE -C src_kernel_headers ARCH=$LINUX_ARCH O=$(pwd)/obj_kernel_headers INSTALL_HDR_PATH=$(pwd)/obj_kernel_headers/staged headers_install
+$MAKE -C src_kernel_headers ARCH=$LINUX_ARCH O=$(pwd)/obj_kernel_headers INSTALL_HDR_PATH=$(pwd)/obj_kernel_headers/staged archscripts headers_install
 find obj_kernel_headers/staged/include '(' -name .install -o -name ..install.cmd ')' -exec rm {} +
 $MAKE -C obj_musl DESTDIR=$out$SYSROOT install
 $MAKE -C obj_toolchain MAKE="$MAKE" DESTDIR=$out install
