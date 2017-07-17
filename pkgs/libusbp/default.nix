de520f3b2162832867e08fc0a41d8636f2e2ff09
David (Pololu) <dev-david@pololu.com>
2017-07-17 09:34:01 -0700

    libusbp: Updated to the latest git version.

--- pkgs/libusbp/default.nix
+++ pkgs/libusbp/default.nix
@@ -1,11 +1,11 @@
 { crossenv }:
 
 let
-  version = "70a33dc";  # 1.0.2ish
+  version = "4876d89";  # 1.0.2ish
 
   src = crossenv.nixpkgs.fetchurl {
     url = "https://github.com/pololu/libusbp/archive/${version}.tar.gz";
-    sha256 = "1y0rln6cp6m3zd5k7lynrwk00w3jvqlv9qyx2cjhl6j90330n53d";
+    sha256 = "1jyvh3h33mxg0qkij5jvsbmagic9wrm9dfhnnilidykmzlqwvp2n";
   };
 
   lib = crossenv.make_derivation {
