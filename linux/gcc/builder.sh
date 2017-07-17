7d6028b46db2be3e877ff039ba082949fb9fa756
David (Pololu) <dev-david@pololu.com>
2017-07-17 09:06:00 -0700

    linux gcc: Don't use obj_sysroot.

--- linux/gcc/builder.sh
+++ linux/gcc/builder.sh
@@ -20,33 +20,31 @@ cd build
 ln -s ../gcc src_gcc
 ln -s ../musl src_musl
 
-mkdir obj_sysroot
-mkdir obj_sysroot/include
-mkdir obj_sysroot/lib
-ln -s . obj_sysroot/usr
-ln -s lib obj_sysroot/lib32
-ln -s lib obj_sysroot/lib64
-
-mkdir obj_gcc
-mkdir obj_musl
-
-gcc_conf="$gcc_conf --with-sysroot=/${host} --with-build-sysroot=$(pwd)/obj_sysroot "
+gcc_conf="$gcc_conf --with-sysroot=/$out/$host "
 musl_conf="$musl_conf CC=../obj_gcc/gcc/xgcc\ -B\ ../obj_gcc/gcc LIBCC=../obj_gcc/$host/libgcc/libgcc.a"
 
 mkdir -p $out/$host
+mkdir $out/$host/include
+mkdir $out/$host/lib
+ln -s . $out/$host/usr
+ln -s lib $out/$host/lib32
+ln -s lib $out/$host/lib64
+
 cp -r --no-preserve=mode $headers/include $out/$host
 
+mkdir obj_gcc
 cd obj_gcc
 ../src_gcc/configure $gcc_conf
 cd ..
 make -C obj_gcc all-gcc
+mkdir obj_musl
 cd obj_musl
 bash -c "../src_musl/configure $musl_conf"
 cd ..
-make -C obj_musl DESTDIR=$(pwd)/obj_sysroot install-headers
+make -C obj_musl DESTDIR=$out/$host install-headers
 make -C obj_gcc all-target-libgcc
 make -C obj_musl
-make -C obj_musl DESTDIR=$(pwd)/obj_sysroot install
+make -C obj_musl DESTDIR=$out/$host install
 make -C obj_gcc
 make -C obj_musl DESTDIR=$out/$host install
 make -C obj_gcc DESTDIR=$out install
