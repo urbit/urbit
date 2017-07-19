4108ebf7778bfd6a2f646ef36c759fa6b1f92ce5
David (Pololu) <dev-david@pololu.com>
2017-07-18 22:50:20 -0700

    linux gcc: Simplify the directories, no more symlinks.

--- linux/gcc/builder.sh
+++ linux/gcc/builder.sh
@@ -3,7 +3,7 @@ shopt -u nullglob
 unset CC CXX CFLAGS LDFLAGS
 
 tar -xf $gcc_src
-mv gcc-$gcc_version gcc
+mv gcc-* gcc
 cd gcc
 for patch in $patch_dir/gcc-$gcc_version/*; do
   echo applying patch $patch
@@ -12,36 +12,27 @@ done
 cd ..
 
 tar -xf $musl_src
-mv musl-$musl_version musl
-
-mkdir -p build
-
-cd build
-ln -s ../gcc src_gcc
-ln -s ../musl src_musl
+mv musl-* musl
 
 gcc_conf="$gcc_conf --prefix=$out"
-musl_conf="$musl_conf --prefix=$out/$host CC=../obj_gcc/gcc/xgcc\ -B\ ../obj_gcc/gcc LIBCC=../obj_gcc/$host/libgcc/libgcc.a"
+musl_conf="$musl_conf --prefix=$out/$host CC=../build_gcc/gcc/xgcc\ -B\ ../build_gcc/gcc LIBCC=../build_gcc/$host/libgcc/libgcc.a"
 
 mkdir -p $out/$host
-mkdir $out/$host/include
-mkdir $out/$host/lib
-ln -s . $out/$host/usr
 
 cp -r --no-preserve=mode $headers/include $out/$host
 
-mkdir obj_gcc
-cd obj_gcc
-../src_gcc/configure $gcc_conf
+mkdir build_gcc
+cd build_gcc
+../gcc/configure $gcc_conf
 cd ..
-make -C obj_gcc all-gcc
-mkdir obj_musl
-cd obj_musl
-bash -c "../src_musl/configure $musl_conf"
+make -C build_gcc all-gcc
+mkdir build_musl
+cd build_musl
+bash -c "../musl/configure $musl_conf"
 cd ..
-make -C obj_musl install-headers
-make -C obj_gcc all-target-libgcc
-make -C obj_musl
-make -C obj_musl install
-make -C obj_gcc
-make -C obj_gcc install
+make -C build_musl install-headers
+make -C build_gcc all-target-libgcc
+make -C build_musl
+make -C build_musl install
+make -C build_gcc
+make -C build_gcc install
