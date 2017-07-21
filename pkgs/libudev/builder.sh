e0b53db6c5baf5debf5cc8f93633a502a52cd8c5
David (Pololu) <dev-david@pololu.com>
2017-07-21 08:03:27 -0700

    Progress on compiling libudev/p-load for Linux.

--- pkgs/libudev/builder.sh
+++ pkgs/libudev/builder.sh
@@ -24,6 +24,9 @@ static_assert(sizeof(rlim_t) == SIZEOF_RLIM_T);
 static_assert(sizeof(dev_t) == SIZEOF_DEV_T);
 static_assert(sizeof(ino_t) == SIZEOF_INO_T);
 EOF
+
+CFLAGS="-Werror -DHAVE_DECL_SETNS -D_GNU_SOURCE $size_flags"
+
 rm test.o
 
 mkdir build
@@ -32,30 +35,27 @@ cd build
 # -DHAVE_SECURE_GETENV: We don't have secure_getenv but we want to avoid a header error,
 # and hopefully secure_getenv isn't actually needed by libudev.
 
-$host-gcc -c -I$fill $fill/*.c
-$host-gcc -c \
-  -D_GNU_SOURCE \
-  $size_flags \
+$host-gcc -c -Werror -I$fill $fill/*.c
+$host-gcc -c $CFLAGS \
   -I../systemd/src/libudev \
   -I../systemd/src/basic \
   -I../systemd/src/libsystemd/sd-device \
   -I../systemd/src/libsystemd/sd-hwdb \
   -I../systemd/src/systemd \
   ../systemd/src/libudev/*.c
-$host-gcc -c \
-  -D_GNU_SOURCE \
-  $size_flags \
+$host-gcc -c $CFLAGS \
   -I../systemd/src/libsystemd/sd-device \
   -I../systemd/src/basic \
   -I../systemd/src/systemd \
-  ../systemd/src/libsystemd/sd-device/device-enumerator.c
-$host-gcc -c \
-  -D_GNU_SOURCE \
-  $size_flags \
+  ../systemd/src/libsystemd/sd-device/{device-enumerator,sd-device}.c
+$host-gcc -c $CFLAGS \
+  -DPACKAGE_STRING="\"libudev $version\"" \
+  -DDEFAULT_HIERARCHY_NAME="\"hybrid\"" \
+  -DDEFAULT_HIERARCHY=CGROUP_UNIFIED_SYSTEMD \
   -I../systemd/src/basic \
   -I../systemd/src/systemd \
   -I$fill \
-  ../systemd/src/basic/{prioq,log}.c
+  ../systemd/src/basic/{alloc-util,cgroup-util,escape,extract-word,fd-util,fs-util,hashmap,hash-funcs,hexdecoct,io-util,log,mempool,path-util,proc-cmdline,parse-util,prioq,process-util,random-util,siphash24,stat-util,string-table,string-util,time-util,utf8,util}.c
 $host-ar cr libudev.a *.o
 
 mkdir -p $out/lib/pkgconfig $out/include
@@ -69,7 +69,7 @@ includedir=\${prefix}/include
 
 Name: libudev
 Version: $version
-Libs: -L\${libdir} -ludev
+Libs: -L\${libdir} -ludev -ludev -ludev
 Cflags: -I\${includedir}
 EOF
 
