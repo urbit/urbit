db1dc2d409ed958ca4f2b1ce5757dab7628d0c96
David (Pololu) <dev-david@pololu.com>
2017-07-22 23:16:47 -0700

    Got linux32.p-load to run by adding -static, not sure why that is needed.

--- pkgs/p-load/builder.sh
+++ pkgs/p-load/builder.sh
@@ -6,6 +6,9 @@ mv p-load-* p-load
 mkdir build
 cd build
 
+# TODO: why is this necessary?  What .so is p-load trying to load and why?
+export LDFLAGS="-static"
+
 cmake-cross ../p-load \
   -DCMAKE_INSTALL_PREFIX=$out
 
