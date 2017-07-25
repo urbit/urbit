57559b081e8cb9bbb2260288f893e12b8440f85a
David (Pololu) <dev-david@pololu.com>
2017-07-25 10:07:57 -0700

    pavr2: Updated to latest git version.

--- pkgs/pavr2/default.nix
+++ pkgs/pavr2/default.nix
@@ -3,11 +3,11 @@
 crossenv.make_derivation rec {
   name = "pavr2-${version}";
 
-  version = "1.0.2ish";
+  version = "4d40ae6";  # 1.0.2ish
 
   src = crossenv.nixpkgs.fetchurl {
-    url = "https://github.com/pololu/pololu-usb-avr-programmer-v2/archive/053fb01.tar.gz";
-    sha256 = "0mh3qyvk4k5i82zcf4q0anvm8aq1b684v91ss2kqhz80fafm6gy3";
+    url = "https://github.com/pololu/pololu-usb-avr-programmer-v2/archive/${version}.tar.gz";
+    sha256 = "0819klcqrss8zdqb26w8bpwzdfrk3g2nn7li2a7k87lqks3pl6lj";
   };
 
   builder = ./builder.sh;
