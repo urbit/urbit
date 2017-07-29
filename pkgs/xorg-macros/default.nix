fc886d3355973b579cca016ccdfa3522d84e7ebf
David (Pololu) <dev-david@pololu.com>
2017-07-29 15:57:03 -0700

    Got xorg-macros building.

---
+++ pkgs/xorg-macros/default.nix
@@ -0,0 +1,15 @@
+{ crossenv }:
+
+if crossenv.os != "linux" then "" else
+
+crossenv.make_derivation rec {
+  name = "xorg-macros-${version}";
+  version = "1.19.1";
+
+  src = crossenv.nixpkgs.fetchurl {
+    url = "https://www.x.org/releases/individual/util/util-macros-1.19.1.tar.gz";
+    sha256 = "1f27cmbxq0kdyvqsplxpsi9pxm5qy45lcagxr9gby2hy3pjd0aj7";
+  };
+
+  builder = ./builder.sh;
+}
