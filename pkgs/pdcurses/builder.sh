eace1d9cee45711c08ea945bdf60e77675bd40f1
David (Pololu) <dev-david@pololu.com>
2017-07-30 11:56:30 -0700

    Some work on getting pdcurses to build for Linux, but the examples have a lotof undefined reference errors.

--- pkgs/pdcurses/builder.sh
+++ pkgs/pdcurses/builder.sh
@@ -1,21 +1,26 @@
 source $setup
 
 tar -xf $src
+mv PDCurses-* pdcurses
 
 mkdir build
 cd build
 
-source_files=../PDCurses-$version/pdcurses/*.c
+source_files=../pdcurses/pdcurses/*.c
 
 if [ "$os" == "windows" ]; then
-  os_files=../PDCurses-$version/win32/*.c
+  os_files=../pdcurses/win32/*.c
+fi
+
+if [ "$os" == "linux" ]; then
+  os_files=
 fi
 
 source_files="$source_files $os_files"
 
 for s in $source_files; do
   echo "compiling $s"
-  $host-gcc -g -O2 -I../PDCurses-$version \
+  $host-gcc -g -O2 -I../pdcurses \
     -DPDC_WIDE -DPDC_FORCE_UTF8 -c "$s" -o "$(basename $s).o"
 done
 
@@ -28,5 +33,5 @@ cp libpdcurses.a $out/lib/libpdcurses.a
 # Make libcurses.a so programs like GDB can find pdcurses.
 ln -s $out/lib/libpdcurses.a $out/lib/libcurses.a
 
-cd ../PDCurses-$version
+cd ../pdcurses
 cp curses.h panel.h term.h $out/include/
