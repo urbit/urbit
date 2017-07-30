e12c52787d9b55f00f16f114246bbed2b0388049
David (Pololu) <dev-david@pololu.com>
2017-07-30 16:15:52 -0700

    libxcb examples: Got it to find its headers, maybe.

--- pkgs/libxcb/examples_builder.sh
+++ pkgs/libxcb/examples_builder.sh
@@ -3,9 +3,20 @@ source $setup
 tar -xf $src
 mv xcb-demo-* demo
 
+# There has been some bit rot.  Prepare a directory structure that matches what
+# this code expects.
+mkdir -p headers/X11/XCB
+touch headers/X11/Xlib.h
+for input in $cross_inputs; do
+  if [ -d $input/include/xcb ]; then
+    ln -s $input/include/xcb/* headers/X11/XCB
+  fi
+done
+
 mkdir build
 cd build
 
+CFLAGS="-I $(realpath ../headers)" \
 PKG_CONFIG=pkg-config-cross \
 ../demo/configure --prefix=$out $configure_flags
 
