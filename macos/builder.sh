86636dc314a143144f55bd69cede392f5a7173f8
David (Pololu) <dev-david@pololu.com>
2017-09-16 16:08:29 -0700

    macos: Massively simplify builder.sh.

--- macos/builder.sh
+++ macos/builder.sh
@@ -15,33 +15,19 @@ BASE_DIR=$PWD
 TARBALL_DIR=$BASE_DIR/tarballs
 BUILD_DIR=$BASE_DIR/build
 TARGET_DIR=$BASE_DIR/target
-PATCH_DIR=$BASE_DIR/patches
-SDK_DIR=$TARGET_DIR/SDK
-export CXXFLAGS=
 
 mkdir -p $BUILD_DIR
 mkdir -p $TARGET_DIR
-mkdir -p $SDK_DIR
 
-pushd $BUILD_DIR &>/dev/null
+cd $BUILD_DIR
 
 mkdir -p $TARGET_DIR/bin
 
 export OSXCROSS_BUILD_DIR=$BUILD_DIR
 
-# NOTE: osxcross set this to LLVM_LIB_DIR from cctools config.log,
-# so maybe we should do the same
-export OSXCROSS_LIBLTO_PATH=
-
 cd $BASE_DIR/wrapper
 
-FLAGS="$CXXFLAGS "
-
-mkdir -p ../target ../target/bin
-export PLATFORM
-export CXX
-
-OSXCROSS_CXXFLAGS="$FLAGS" make wrapper
+make wrapper
 
 mv wrapper $TARGET_DIR/bin/$host-wrapper
 
