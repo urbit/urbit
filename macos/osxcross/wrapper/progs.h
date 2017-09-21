6d91c8130f580e3147e0e3138e2e734fdd4d7c4f
David (Pololu) <dev-david@pololu.com>
2017-09-20 20:33:26 -0700

    macos wrapper: Simplified progs.h.

--- macos/osxcross/wrapper/progs.h
+++ macos/osxcross/wrapper/progs.h
@@ -19,74 +19,8 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.      *
  ***********************************************************************/
 
-namespace target {
-struct Target;
-}
-
 namespace program {
 
 using target::Target;
-
-class prog {
-public:
-  typedef int (*f1)();
-  typedef int (*f2)(int, char **);
-  typedef int (*f3)(int, char **, Target &);
-  typedef int (*f4)(Target &);
-
-  constexpr prog(const char *name, f1 fun)
-      : name(name), fun1(fun), fun2(), fun3(), fun4(), type(1) {}
-
-  constexpr prog(const char *name, f2 fun)
-      : name(name), fun1(), fun2(fun), fun3(), fun4(), type(2) {}
-
-  constexpr prog(const char *name, f3 fun)
-      : name(name), fun1(), fun2(), fun3(fun), fun4(), type(3) {}
-
-  constexpr prog(const char *name, f4 fun)
-      : name(name), fun1(), fun2(), fun3(), fun4(fun), type(4) {}
-
-  __attribute__((noreturn))
-  void operator()(int argc, char **argv, Target &target) const {
-    switch (type) {
-    case 1:
-      exit(fun1());
-    case 2:
-      exit(fun2(argc, argv));
-    case 3:
-      exit(fun3(argc, argv, target));
-    case 4:
-      exit(fun4(target));
-    }
-    __builtin_unreachable();
-  }
-
-  bool operator==(const char *name) const { return !strcmp(name, this->name); }
-
-  template<class T>
-  bool operator==(const T &name) const { return name == this->name; }
-
-  const char *name;
-
-private:
-  f1 fun1;
-  f2 fun2;
-  f3 fun3;
-  f4 fun4;
-  int type;
-};
-
-int sw_vers(int argc, char **argv, target::Target &target);
-int xcrun(int argc, char **argv, Target &target);
-int dsymutil(int argc, char **argv, target::Target &target);
-
-namespace osxcross {
-int version();
-int env(int argc, char **argv);
-int conf(Target &target);
-int cmp(int argc, char **argv);
-int man(int argc, char **argv, Target &target);
-int pkg_config(int argc, char **argv, Target &target);
-}
 
 }
