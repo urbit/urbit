e0cff045a833b90543a2ec84dbb86107cf5b970c
David (Pololu) <dev-david@pololu.com>
2017-09-24 11:41:32 -0700

    Updated p-load version.

--- pkgs/p-load/builder.sh
+++ pkgs/p-load/builder.sh
@@ -3,12 +3,21 @@ source $setup
 tar -xf $src
 mv p-load-* p-load
 
+cd p-load
+for patch in $patches; do
+  echo applying patch $patch
+  patch -p1 -i $patch
+done
+cd ..
+
+cat p-load/CMakeLists.txt
+
 mkdir build
 cd build
 
 cmake-cross ../p-load \
   -DCMAKE_INSTALL_PREFIX=$out
 
-make
+make VERBOSE=Y
 
 make install
