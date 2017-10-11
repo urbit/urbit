c2b325e45e519ec676346b492935de079b810e5c
David (Pololu) <dev-david@pololu.com>
2017-10-11 15:03:34 -0700

    Unset more variables in linux and mingw-w64 builders; now win32.hello builds again.

--- linux/headers_builder.sh
+++ linux/headers_builder.sh
@@ -1,7 +1,7 @@
 source $stdenv/setup
 shopt -u nullglob
 
-unset CC CXX LD AR AS CFLAGS LDFLAGS
+unset CC CXX CFLAGS LDFLAGS LD AR AS RANLIB SIZE STRINGS NM STRIP OBJCOPY
 
 tar -xf $src
 mv linux-$version linux
