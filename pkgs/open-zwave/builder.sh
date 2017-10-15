b8943461e99156e2308027bf4eb38bbdd9856dea
David (Pololu) <dev-david@pololu.com>
2017-10-15 08:01:40 -0700

    open-zwave builder: Remove a tmphax line.

--- pkgs/open-zwave/builder.sh
+++ pkgs/open-zwave/builder.sh
@@ -8,7 +8,6 @@ cd build
 
 for n in Driver Group Manager Msg Node Notification Options Scene Utils ZWSecurity platform/Event platform/Controller platform/FileOps platform/HidController platform/Log platform/Mutex platform/SerialController platform/Stream platform/Thread platform/TimeStamp platform/Wait platform/$platform/{Event,FileOps,Log,Mutex,SerialController,Thread,TimeStamp,Wait}Impl; do
   $host-g++ -c $CFLAGS ../ozw/cpp/src/$n.cpp -o $(basename $n).o
-  chmod a-w $(basename $n).o
 done
 
 $host-ar cr libopenzwave.a *.o
