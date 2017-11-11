69699945dcb2acd1d1ae443ce483ae055de4caf7
David (Pololu) <dev-david@pololu.com>
2017-11-10 19:41:51 -0800

    macos: Make progress on compiling ranlib, just one undefined reference error now.

--- macos/ranlib_builder.sh
+++ macos/ranlib_builder.sh
@@ -12,27 +12,24 @@ done
 rm cctools/include/foreign/mach/i386/_structs.h
 cd ..
 
-mv cctools-port/cctools/misc misc
-mv cctools-port/cctools/include include
+mv cctools-port/cctools/misc .
+mv cctools-port/cctools/include .
+mv cctools-port/cctools/libstuff .
+mv cctools-port/cctools/ld64 .
 rm -r cctools-port
 
 mkdir build
 cd build
 
-CFLAGS="-Wno-deprecated -Wno-deprecated-declarations -Wno-unused-result -Werror -Wfatal-errors -O2 -g -I../include -I../include/foreign -DPROGRAM_PREFIX=\\\"$host-\\\" -D__LITTLE_ENDIAN__ -D__private_extern__= -D__DARWIN_UNIX03"
+CFLAGS="-Wno-deprecated -Wno-deprecated-declarations -Wno-unused-result -Werror -Wfatal-errors -O2 -g -I../include -I../include/foreign -DPROGRAM_PREFIX=\\\"$host-\\\" -D__LITTLE_ENDIAN__ -D__private_extern__= -D__DARWIN_UNIX03 -DPACKAGE_NAME=\\\"cctools\\\" -DPACKAGE_VERSION=\\\"x\\\" -DEMULATED_HOST_CPU_TYPE=16777223 -DEMULATED_HOST_CPU_SUBTYPE=3"
 
 CXXFLAGS="-std=gnu++11 $CFLAGS"
 
 LDFLAGS="-ldl -lpthread"
 
-for f in ../misc/libtool.c; do
+for f in ../misc/libtool.c ../libstuff/*.c ; do
   echo "compiling $f"
   eval "gcc -c $CFLAGS $f -o $(basename $f).o"
-done
-
-for f in $(find ../ld64/src -name \*.cpp); do
-  echo "compiling $f"
-  eval "g++ -c $CXXFLAGS $f -o $(basename $f).o"
 done
 
 g++ *.o $LDFLAGS -o $host-ld
