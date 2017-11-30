148240ac7f6f0e0bd80f7f7254dbba94261dfcda
Ted Blackman <ted@tlon.io>
2017-11-29 17:18:34 -0800

    Print out localhost-only message on ames init

--- vere/ames.c
+++ vere/ames.c
@@ -326,7 +326,13 @@ u3_ames_io_init()
     num_y = u3r_byte(0, u3t(num));
 
     _ames_czar(num_y, &por_s);
-    uL(fprintf(uH, "ames: czar: %s on %d\n", u3_Host.ops_u.imp_c, por_s));
+    if ( c3y == u3_Host.ops_u.net ) {
+      uL(fprintf(uH, "ames: czar: %s on %d\n", u3_Host.ops_u.imp_c, por_s));
+    }
+    else {
+      uL(fprintf(uH, "ames: czar: %s on %d\n (localhost only)",
+                     u3_Host.ops_u.imp_c, por_s));
+    }
     u3z(num);
   }
 
