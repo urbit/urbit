c9aa26ad44ff01a164e2d8fcc51cb580043cdfe8
mikolajp <mikolajp@protonmail.com>
2018-02-25 23:40:19 +0800

    Fix libscrypt includes

--- jets/e/scr.c
+++ jets/e/scr.c
@@ -6,8 +6,8 @@
 #include <stdint.h>
 #include <errno.h>
 
-#include <libscrypt/libscrypt.h>
-#include <libscrypt/sha256.h>
+#include <libscrypt.h>
+#include <sha256.h>
 
 static int _crypto_scrypt(const uint8_t *, size_t, const uint8_t *, size_t,
     uint64_t, uint32_t, uint32_t, uint8_t *, size_t);
