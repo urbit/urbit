f826497d47c9d9dce019b892b893484d43f13fd2
mikolajp <mikolajp@protonmail.com>
2018-02-25 23:35:44 +0800

    Migrate to stock libscrypt version with meson support

--- jets/e/scr.c
+++ jets/e/scr.c
@@ -5,16 +5,10 @@
 
 #include <stdint.h>
 #include <errno.h>
-#include <libscrypt.h>
 
-/**
- * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
- * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
- * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
- */
-void	libscrypt_PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t,
-    uint64_t, uint8_t *, size_t);
-    
+#include <libscrypt/libscrypt.h>
+#include <libscrypt/sha256.h>
+
 static int _crypto_scrypt(const uint8_t *, size_t, const uint8_t *, size_t,
     uint64_t, uint32_t, uint32_t, uint8_t *, size_t);
 
