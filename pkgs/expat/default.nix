a38c58426d4f006841e58b4f38b81e43a6952f58
David (Pololu) <dev-david@pololu.com>
2018-03-03 14:48:56 -0800

    Use my mirror to download expat too.

--- pkgs/expat/default.nix
+++ pkgs/expat/default.nix
@@ -6,7 +6,9 @@ crossenv.make_derivation rec {
   version = "2.2.0";
 
   src = crossenv.nixpkgs.fetchurl {
-    url = "mirror://sourceforge/expat/expat-${version}.tar.bz2";
+    # Sourceforge went down.  The original URL we used was:
+    # mirror://sourceforge/expat/expat-${version}.tar.bz2
+    url = "https://files.tmphax.com/repo1/expat-${version}.tar.bz2";
     sha256 = "1zq4lnwjlw8s9mmachwfvfjf2x3lk24jm41746ykhdcvs7r0zrfr";
   };
 
--- pkgs/pdcurses/lib.nix
+++ pkgs/pdcurses/lib.nix
@@ -6,6 +6,7 @@ crossenv.make_derivation rec {
   version = "3.4";
 
   src = crossenv.nixpkgs.fetchurl {
+    # TODO: fix broken sourceforge download
     url = "mirror://sourceforge/pdcurses/PDCurses-${version}.tar.gz";
     sha256 = "0jz6l8552fnf1j542yhzifgknrdzrisxg158ks0l87g777a8zba6";
   };
