f10e5e9af4596942183efaec15614dc358c48803
David (Pololu) <dev-david@pololu.com>
2018-03-03 17:19:20 -0800

    Fix the sourceforge download link for pdcurses too.

--- pkgs/pdcurses/lib.nix
+++ pkgs/pdcurses/lib.nix
@@ -6,8 +6,9 @@ crossenv.make_derivation rec {
   version = "3.4";
 
   src = crossenv.nixpkgs.fetchurl {
-    # TODO: fix broken sourceforge download
-    url = "mirror://sourceforge/pdcurses/PDCurses-${version}.tar.gz";
+    # Sourceforge went down.  The original URL was:
+    # url = "mirror://sourceforge/pdcurses/PDCurses-${version}.tar.gz";
+    url = "https://files.tmphax.com/repo1/pdcurses-${version}.tar.gz";
     sha256 = "0jz6l8552fnf1j542yhzifgknrdzrisxg158ks0l87g777a8zba6";
   };
 
