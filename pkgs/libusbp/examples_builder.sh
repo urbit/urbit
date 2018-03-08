dc13566d999ef28546ea0ba3231dfe4cd836b3fe
David (Pololu) <dev-david@pololu.com>
2018-03-08 00:40:05 -0800

    Fix the build of mac.libusbp.examples.

--- pkgs/libusbp/examples_builder.sh
+++ pkgs/libusbp/examples_builder.sh
@@ -6,7 +6,7 @@ mv libusbp-* libusbp
 mkdir build
 cd build
 
-$host-g++ ../libusbp/examples/lsusb/*.cpp -o lsusb$exe_suffix \
+$host-g++ -std=gnu++11 ../libusbp/examples/lsusb/*.cpp -o lsusb$exe_suffix \
   $(pkg-config-cross --cflags --libs libusbp-1)
 
 mkdir -p $out/bin
