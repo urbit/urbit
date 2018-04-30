408cc772983d99d879bded8481c9b78094546205
David (Pololu) <dev-david@pololu.com>
2018-04-29 23:42:31 -0700

    Fix build of libxcb for Windows, and in derivations.txt don't build libraries that depend on libx11 in Windows because the build is broken.
    All derivations in derviations.txt can now be built on nixpkgs unstable.

--- pkgs/libxcb/builder.sh
+++ pkgs/libxcb/builder.sh
@@ -22,6 +22,7 @@ make install
 
 # Make static linking work.
 sed -i 's/Requires.private/Requires/' $out/lib/pkgconfig/*.pc
+sed -i 's/Libs.private/Libs/' $out/lib/pkgconfig/*.pc
 
 ln -sf $libxau/lib/pkgconfig/*.pc $out/lib/pkgconfig/
 
