f95e05026e01d63be262b53831dab6ac7e1ae9f5
David (Pololu) <dev-david@pololu.com>
2018-11-04 21:34:27 -0800

    Linux: Downgrade to binutils 2.30 to fix a segmentation fault in Musl.

--- linux/binutils/default.nix
+++ linux/binutils/default.nix
@@ -1,13 +1,16 @@
+# We cannot use binutils 2.31 because then we get a segmentation fault in our
+# hello world program, which comes from static_init_tls() in Musl 1.1.20.
+
 { native, host }:
 
 native.make_derivation rec {
   name = "binutils-${version}-${host}";
 
-  version = "2.31";
+  version = "2.30";
 
   src = native.nixpkgs.fetchurl {
     url = "mirror://gnu/binutils/binutils-${version}.tar.xz";
-    sha256 = "1gqn887nqfyd5l6gfv08m1pg4wg5fm2iys7hpz6lj87hgvgkc413";
+    sha256 = "1rhshw4m5m2pjz8g15hpiwhp52kn0pj0b5dxy0v7lwigmspbhikf";
   };
 
   patches = [
