068bf90e01d86110421a9cc23c918a04a10d1905
Benjamin Summers <benjamin@tlon.io>
2018-11-06 19:56:48 -0800

    Build with nix on OSX and Linux

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with (import <nixpkgs> {});
+
+let
+
+  osxdeps = lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    Cocoa
+    CoreServices
+  ]);
+
+  deps = [ cmark curl gcc gmp libsigsegv meson ncurses ninja pkgconfig zlib re2c ];
+
+in stdenv.mkDerivation {
+  name = "urbit";
+  buildInputs = osxdeps ++ deps;
+  NIX_LDFLAGS = lib.optionalString stdenv.isDarwin "-framework CoreServices";
+}
