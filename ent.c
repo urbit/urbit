30390fa180f9f3502086ed851393482b64421e8e
Jōshin <git@wholezero.org>
2018-12-27 11:06:22 -0800

    More robust read loop

--- ent.c
+++ ent.c
@@ -1,27 +1,38 @@
 #include <ent/ent.h>
 
-#if defined(ENT_USE_URANDOM)
-# include <assert.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <unistd.h>
+#if defined(ENT_URANDOM)
+
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 int
-ent_getentropy(void* buf, size_t buflen)
+ent_getentropy(void* buf, size_t len)
 {
   int fd;
+  ssize_t ret;
 
-  assert(buflen <= 256);
+  assert(len <= 256);
   if (-1 == (fd = open("/dev/urandom", O_RDONLY))) {
     return -1;
   }
-  if (buflen != read(fd, buf, buflen)) {
-    (void) close(fd);
-    return -1;
+  while (len != 0 && (ret = read(fd, buf, len)) != 0) {
+    if (ret == -1) {
+      if (errno == EINTR)
+        continue;
+      break;
+    }
+    len -= ret;
+    buf += ret;
   }
-  close(fd);
-  return 0;
+  (void) close(fd);
+  if (ret == 0) {
+    ret = -1;
+    errno = EIO;
+  }
+  return ret < 0 ? ret : 0;
 }
 
 #endif /* ENT_USE_URANDOM */
