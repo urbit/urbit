8ce0600e0010c011f551d50ec1ae989a795ee756
Jōshin <git@wholezero.org>
2019-01-02 13:57:06 -0800

    Simplify by using stdio
    Their read loop is probably better anyway. This also sets CLOEXEC on the
    opened file, since who knows.
    
    We can do this in the getrandom case since on small reads (len <= 256),
    getrandom doesn't fail with EINTR.

--- src/ent.c
+++ src/ent.c
@@ -20,52 +20,35 @@
 #   define _GNU_SOURCE
 #   include <unistd.h>
 #   include <sys/syscall.h>
-
 #   define ENT_DEFS (void)0
 #   define ENT_INIT() (void)0
 #   define ENT_READ(B, L) syscall(SYS_getrandom, B, L, 0)
 #   define ENT_FINI() (void)0
 # elif defined(ENT_URANDOM)
-#   include <unistd.h>
-#   include <sys/types.h>
-#   include <sys/stat.h>
-#   include <fcntl.h>
-
-#   define ENT_DEFS int fd
-#   define ENT_INIT() do {                            \
-      if ((fd = open("/dev/urandom", O_RDONLY)) < 0)  \
-        return -1;                                    \
+#   include <stdio.h>
+#   define ENT_DEFS FILE *f
+#   define ENT_INIT() do {                    \
+      if (!(f = fopen("/dev/urandom", "re"))) \
+        return -1;                            \
     } while (0)
-#   define ENT_READ(B, L) read(fd, B, L)
-#   define ENT_FINI() (void) close(fd)
+#   define ENT_READ(B, L) fread(B, 1, L, f)
+#   define ENT_FINI() (void) fclose(f)
 # endif
 
   int
   ent_getentropy(void* buf, size_t len)
   {
-    char *cuf = buf;
-    int   ret;
     ENT_DEFS;
 
     assert(len <= 256);
-    if (!len)
-      return 0;
     ENT_INIT();
-    while (len && (ret = ENT_READ(cuf, len)) != 0) {
-      if (ret < 0) {
-        if (errno == EINTR)
-          continue;
-        break;
-      }
-      len -= ret;
-      cuf += ret;
+    if (len != ENT_READ(buf, len)) {
+      ENT_FINI();
+      errno = EIO;
+      return -1;
     }
     ENT_FINI();
-    if (!ret) {
-      ret = -1;
-      errno = EIO;
-    }
-    return ret < 0 ? ret : 0;
+    return 0;
   }
 
 #else
