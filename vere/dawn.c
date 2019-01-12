7ade54973d38b9f4f0ef504d5bd39c1cafbaecc2
Ted Blackman <ted@tlon.io>
2019-01-11 18:01:54 -0800

    add a warning printf about slowness

--- vere/dawn.c
+++ vere/dawn.c
@@ -496,7 +496,7 @@ _dawn_come(u3_noun stars)
     c3_rand(eny_w);
     eny = u3i_words(16, eny_w);
 
-    fprintf(stderr, "boot: mining a comet\r\n");
+    fprintf(stderr, "boot: mining a comet. May take up to 30 minutes.\r\n");
 
     seed = u3dc("come:dawn", u3k(stars), u3k(eny));
     u3z(eny);
