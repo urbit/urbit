8cc7a3b123be7cd1e3b3c693209a2de458c371e6
David Grayson <davidegrayson@gmail.com>
2019-02-01 20:51:38 -0800

    pkgconf: Provide pkg.m4.

--- native/pkgconf/builder.sh
+++ native/pkgconf/builder.sh
@@ -1,12 +1,13 @@
 source $setup
 
 tar -xf $src
+mv pkgconf-* pkgconf
 
 mkdir build
 
 cd build
 
-../pkgconf-$version/configure \
+../pkgconf/configure \
   --prefix=$out \
   --with-system-libdir=/no-system-libdir/ \
   --with-system-includedir=/no-system-includedir/
@@ -17,5 +18,8 @@ make install
 
 ln -s $out/bin/pkgconf $out/bin/pkg-config
 
+mkdir -p $out/share/aclocal
+cp ../pkgconf/pkg.m4 $out/share/aclocal/
+
 mkdir $out/license
-cp ../pkgconf-$version/COPYING $out/license/LICENSE
+cp ../pkgconf/COPYING $out/license/LICENSE
