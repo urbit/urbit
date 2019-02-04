0fa9e2c39e4b50a8318b4d58501c22d04bf93871
David Grayson <davidegrayson@gmail.com>
2019-02-03 21:07:23 -0800

    wrappers (pkg-config-cross and cmake-cross): Add a shebang line at the to fix the weird exec format error that was happening when Meson tried to run a wrapper.

--- native/wrappers/builder.sh
+++ native/wrappers/builder.sh
@@ -3,12 +3,14 @@ source $setup
 mkdir -p $out/bin
 
 cat > $out/bin/pkg-config-cross <<EOF
+#!$(which bash)
 PKG_CONFIG_LIBDIR=\$PKG_CONFIG_CROSS_PATH \\
 PKG_CONFIG_PATH=\$PKG_CONFIG_CROSS_PATH \\
 exec pkg-config \$@
 EOF
 
 cat > $out/bin/cmake-cross <<EOF
+#!$(which bash)
 PKG_CONFIG=pkg-config-cross \\
 CMAKE_PREFIX_PATH=\$CMAKE_CROSS_PREFIX_PATH \\
 exec cmake -DCMAKE_TOOLCHAIN_FILE=\$cmake_toolchain \$@
