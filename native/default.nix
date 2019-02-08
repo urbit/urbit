fd77c95e082bd9f3deadb756994edda108efbd65
David Grayson <davidegrayson@gmail.com>
2019-02-08 09:40:33 -0800

    Remove gnu_config.  Nothing is using it.

--- native/default.nix
+++ native/default.nix
@@ -33,20 +33,12 @@ let
 
   pkgconf = import ./pkgconf { env = native_base; };
 
-  # TODO: move this to its own directory in pkgs so it's easier to
-  # remember to update it
-  gnu_config = nixpkgs.fetchgit {
-    url = "https://git.savannah.gnu.org/git/config.git";
-    rev = "286a38db91ea2dce1749ab7d1d9ea5ae344a16c1";
-    sha256 = "086z8298d6sy75kqj0f09dpqni347j1d7sjagdj926zhkc584wii";
-  };
-
   native = native_base // {
     default_native_inputs = native_base.default_native_inputs ++ [
       pkgconf
     ];
 
-    inherit pkgconf gnu_config;
+    inherit pkgconf;
 
     make_derivation = import ../make_derivation.nix native;
   };
