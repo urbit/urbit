ff7819991122d46baf80047b61a2d707d17b8710
Benjamin Summers <benjamin@tlon.io>
2019-03-07 14:34:29 -0800

    cat, cap, con

--- pkg/urbit/jets/c/cap.c
+++ pkg/urbit/jets/c/cap.c
@@ -1,36 +1,31 @@
-/* j/3/cap.c
-**
-*/
+////////////////////////////////////////////////////////////////////////////////
+// j/3/cap.c
+////////////////////////////////////////////////////////////////////////////////
+
 #include "all.h"
 
+u3_noun u3qc_cap(u3_atom a) {
+  c3_w met_w = u3r_met(0, a);
 
-/* functions
-*/
-  u3_noun
-  u3qc_cap(u3_atom a)
-  {
-    c3_w met_w = u3r_met(0, a);
-
-    if ( met_w < 2 ) {
-      return u3m_bail(c3__exit);
-    }
-    else if ( (1 == u3r_bit((met_w - 2), a)) ) {
-      return 3;
-    } else {
-      return 2;
-    }
+  if ( met_w < 2 ) {
+    return u3m_bail(c3__exit);
   }
-  u3_noun
-  u3wc_cap(u3_noun cor)
-  {
-    u3_noun a;
 
-    if ( (u3_none == (a = u3r_at(u3x_sam, cor))) ||
-         (c3n == u3ud(a)) )
-    {
-      return u3m_bail(c3__exit);
-    } else {
-      return u3qc_cap(a);
-    }
+  if ( (1 == u3r_bit((met_w - 2), a)) ) {
+    return u3a_cat31_unsafe(3);
   }
+
+  return u3a_cat31_unsafe(2);
+}
+
+u3_noun u3wc_cap(u3_noun cor) {
+  u3_noun a = u3r_at(u3x_sam, cor);
+
+  if ( u3a_is_none(a) || (c3n == u3ud(a)) )
+  {
+    return u3m_bail(c3__exit);
+  }
+
+  return u3qc_cap(a);
+}
 
--- pkg/urbit/jets/c/cat.c
+++ pkg/urbit/jets/c/cat.c
@@ -6,53 +6,48 @@
 
 /* functions
 */
-  u3_noun
-  u3qc_cat(u3_atom a,
-           u3_atom b,
-           u3_atom c)
-  {
-    if ( !_(u3a_is_cat(a)) || (a >= 32) ) {
-      return u3m_bail(c3__fail);
-    }
-    else {
-      c3_g   a_g = a;
-      c3_w   lew_w = u3r_met(a_g, b);
-      c3_w   ler_w = u3r_met(a_g, c);
-      c3_w   all_w = (lew_w + ler_w);
+u3_noun u3qc_cat(u3_atom a, u3_atom b, u3_atom c) {
+  c3_w a_w = u3a_get_cat31(a);
 
-      if ( 0 == all_w ) {
-        return 0;
-      } else {
-        c3_w* sal_w = u3a_slaq(a_g, all_w);
-
-        if ( 0 == sal_w ) {
-          return u3m_bail(c3__fail);
-        }
-        else {
-          u3r_chop(a_g, 0, lew_w, 0, sal_w, b);
-          u3r_chop(a_g, 0, ler_w, lew_w, sal_w, c);
-        }
-        // return u3a_moot(sal_w);
-        return u3a_malt(sal_w);
-      }
-    }
+  if ( a_w >= 32 ) {
+    return u3m_bail(c3__fail);
   }
 
-  u3_noun
-  u3wc_cat(u3_noun cor)
-  {
-    u3_noun a, b, c;
+  c3_g   a_g = a_w;
+  c3_w   lew_w = u3r_met(a_g, b);
+  c3_w   ler_w = u3r_met(a_g, c);
+  c3_w   all_w = (lew_w + ler_w);
 
-    if ( (c3n == u3r_mean(cor, u3x_sam_2, &a,
-                                u3x_sam_6, &b,
-                                u3x_sam_7, &c, 0)) ||
-         (c3n == u3ud(a)) ||
-         (c3n == u3ud(b)) ||
-         (c3n == u3ud(c)) )
-    {
-      return u3m_bail(c3__exit);
-    } else {
-      return u3qc_cat(a, b, c);
-    }
+  if ( 0 == all_w ) {
+    return u3a_cat31_unsafe(0);
   }
+
+  c3_w* sal_w = u3a_slaq(a_g, all_w);
+
+  if ( 0 == sal_w ) {
+    return u3m_bail(c3__fail);
+  }
+
+  u3r_chop(a_g, 0, lew_w, 0, sal_w, b);
+  u3r_chop(a_g, 0, ler_w, lew_w, sal_w, c);
+
+  return u3a_malt(sal_w);
+}
+
+u3_noun u3wc_cat(u3_noun cor)
+{
+  u3_noun a, b, c;
+
+  if ( (c3n == u3r_mean(cor, u3x_sam_2, &a,
+                             u3x_sam_6, &b,
+                             u3x_sam_7, &c, 0)) ||
+       (c3n == u3ud(a)) ||
+       (c3n == u3ud(b)) ||
+       (c3n == u3ud(c)) )
+  {
+    return u3m_bail(c3__exit);
+  }
+
+  return u3qc_cat(a, b, c);
+}
 
