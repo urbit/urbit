a7416f7c5fac20353515eac8ee52c9c42cabc675
Benjamin Summers <benjamin@tlon.io>
2019-04-16 15:08:22 -0700

    Updated nixcrpkgs and moved OSX SDK out of that repo.

--- nix/nixcrpkgs.nix
+++ nix/nixcrpkgs.nix
@@ -1,7 +1,8 @@
 let
 
-  nixpkgs = import ./nixpkgs.nix;
+  nixpkgs  = import ./nixpkgs.nix;
+  sdk_file = ../bin/MacOSX10.11.sdk.tar.xz;
 
 in
 
-import ./nixcrpkgs/top.nix { inherit nixpkgs; }
+import ./nixcrpkgs/top.nix { inherit sdk_file nixpkgs; }
