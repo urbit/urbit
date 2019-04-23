2a1d0bcca79cafdd5ccec8922b8a5615327cd70f
Benjamin Summers <benjamin@tlon.io>
2019-04-23 16:14:23 -0700

    Pass in OSX SDK as parameter.

--- nix/nixcrpkgs.nix
+++ nix/nixcrpkgs.nix
@@ -1,8 +1,8 @@
 let
 
-  nixpkgs  = import ./nixpkgs.nix;
-  sdk_file = ../bin/MacOSX10.11.sdk.tar.xz;
+  nixpkgs = import ./nixpkgs.nix;
+  osx_sdk = ../bin/MacOSX10.11.sdk.tar.xz;
 
 in
 
-import ./nixcrpkgs/top.nix { inherit nixpkgs; }
+import ./nixcrpkgs/top.nix { inherit osx_sdk nixpkgs; }
