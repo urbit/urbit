47d5b1c7574bc476a208f81f94476a4ebfc7b27a
Benjamin Summers <benjamin@tlon.io>
2019-04-23 17:11:49 -0700

    Download the SDK from suspicious github url.

--- nix/nixcrpkgs.nix
+++ nix/nixcrpkgs.nix
@@ -1,7 +1,11 @@
 let
 
   nixpkgs = import ./nixpkgs.nix;
-  osx_sdk = ../bin/MacOSX10.11.sdk.tar.xz;
+
+  osx_sdk = builtins.fetchurl {
+    sha256 = "89aa34dfe5bcbc7d53d3c55a84b35ac810ecfbcdd16a64c9667992b0c36c60c4";
+    url = "https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.11.sdk.tar.xz";
+  };
 
 in
 
