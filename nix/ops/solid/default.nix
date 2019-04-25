e11f26d0f6958ef63aa38c9ce929297a8af110c9
Benjamin Summers <benjamin@tlon.io>
2019-04-25 13:32:27 -0700

    Oops: Make nix/ops/{brass,solid}/* work with the debug flag.

--- nix/ops/solid/default.nix
+++ nix/ops/solid/default.nix
@@ -1,10 +1,19 @@
-{ pkgs, tlon, deps, urbit, fakezod, arvo }:
+{ pkgs, tlon, deps, fakezod, arvo, debug }:
+
+let
+
+  urbitExe = if debug
+             then "${tlon.urbit-debug}/bin/urbit-debug -g"
+             else "${tlon.urbit}/bin/urbit";
+
+in
 
 pkgs.stdenv.mkDerivation rec {
   name        = "solid";
   builder     = ./builder.sh;
-  buildInputs = [ urbit tlon.herb pkgs.coreutils ];
+  buildInputs = [ tlon.herb ];
 
+  URBIT   = urbitExe
   FAKEZOD = fakezod;
   ARVO    = arvo;
 }
