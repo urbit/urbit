1a32118a6e446bd8ae50d51f42b032aba2e9ae91
Benjamin Summers <benjamin@tlon.io>
2019-04-27 18:48:36 -0700

    Got cross-compiled lmdb build working.

--- nix/nixcrpkgs/pkgs/lmdb/builder.sh
+++ nix/nixcrpkgs/pkgs/lmdb/builder.sh
@@ -4,6 +4,13 @@ cp -r $src ./src
 chmod -R u+w ./src
 cd src/libraries/liblmdb
 
-mkdir ../out
-DESTDIR="../out" make install
-cp -r ../out/usr/local "$out"
+sed -i 's/liblmdb.a liblmdb..SOEXT.$/liblmdb.a/' Makefile
+sed -i "s/gcc/$host-gcc/"                        Makefile
+sed -i "s/ar/$host-ar/"                          Makefile
+sed -i 's/^CC.*/CC = '"$host-gcc/"               Makefile
+
+cat Makefile
+
+make CFLAGS+="-fPIC"
+
+make DESTDIR="$out" prefix=/ install
