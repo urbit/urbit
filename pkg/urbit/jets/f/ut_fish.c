61b957cc5b1d59e7933a7428c1ba0056ab8afa56
pilfer-pandex <47340789+pilfer-pandex@users.noreply.github.com>
2019-05-17 15:00:23 -0700

    decapitate fish:ut

--- pkg/urbit/jets/f/ut_fish.c
+++ pkg/urbit/jets/f/ut_fish.c
@@ -3,144 +3,18 @@
 */
 #include "all.h"
 
-/* functions
-*/
-  static u3_noun
-  _fish_in(u3_noun, u3_noun, u3_atom, u3_noun);
-
-  static u3_noun
-  _fish_fork(u3_noun van,
-             u3_noun p_sut,
-             u3_atom axe,
-             u3_noun vit)
-  {
-    if ( u3_nul == p_sut ) {
-      return u3nc(1, 1);
-    }
-    else {
-      u3_noun hed = _fish_in(van, u3h(p_sut), axe, vit);
-      u3_noun tal = _fish_fork(van, u3t(p_sut), axe, vit);
-      u3_noun pro = u3qf_flor(hed, tal);
-
-      u3z(hed);
-      u3z(tal);
-
-      return pro;
-    }
-  }
-
-  static u3_noun
-  _fish_in(u3_noun van,
-           u3_noun sut,
-           u3_atom axe,
-           u3_noun vit)
-  {
-    u3_noun p_sut, q_sut;
-
-    if ( c3y == u3ud(sut) ) switch ( sut ) {
-      default: return u3m_bail(c3__fail);
-
-      case c3__noun: {
-        return u3nc(1, 0);
-      }
-      case c3__void: {
-        return u3nc(1, 1);
-      }
-    }
-    else switch ( u3h(sut) ) {
-      default: return u3m_bail(c3__fail);
-
-      case c3__atom: {
-        if ( (c3n == u3r_trel(sut, 0, &p_sut, &q_sut)) ) {
-          return u3m_bail(c3__fail);
-        } else {
-          if ( c3n == u3du(q_sut) ) {
-            u3_noun ton = u3nt(3, 0, u3k(axe));
-            u3_noun pro = u3qf_flip(ton);
-
-            u3z(ton);
-            return pro;
-          } else {
-            return u3nt(5,
-                        u3nc(1, u3k(u3t(q_sut))),
-                        u3nc(0, u3k(axe)));
-          }
-        }
-      }
-      case c3__cell: {
-        if ( (c3n == u3r_trel(sut, 0, &p_sut, &q_sut)) ) {
-          return u3m_bail(c3__fail);
-        } else {
-          u3_noun hut = u3nt(3, 0, u3k(axe));
-          u3_noun lef = u3qc_peg(axe, 2);
-          u3_noun rit = u3qc_peg(axe, 3);
-          u3_noun hed = _fish_in(van, p_sut, lef, vit);
-          u3_noun tal = _fish_in(van, q_sut, rit, vit);
-          u3_noun hob = u3qf_flan(hed, tal);
-          u3_noun vug = u3qf_flan(hut, hob);
-
-          u3z(hob);
-          u3z(tal);
-          u3z(hed);
-          u3z(rit);
-          u3z(lef);
-          u3z(hut);
-
-          return vug;
-        }
-      }
-      case c3__core: {
-        return u3m_error("fish-core");
-      }
-      case c3__hint:
-      case c3__face: {
-        if ( (c3n == u3r_trel(sut, 0, &p_sut, &q_sut)) ) {
-          return u3m_bail(c3__fail);
-        } else {
-          return _fish_in(van, q_sut, axe, vit);
-        }
-      }
-      case c3__fork: p_sut = u3t(sut);
-      {
-        u3_noun yed = u3qdi_tap(p_sut);
-        u3_noun ret = _fish_fork(van, yed, axe, vit);
-
-        u3z(yed);
-        return ret;
-      }
-      case c3__hold: {
-        p_sut = u3t(sut);
-        {
-          if ( (c3y == u3qdi_has(vit, sut)) ) {
-            //  u3_noun dun = u3qfu_dunq(van, "type", sut);
-            u3_noun niz = u3qfu_shep
-              (van, "axis", 'd', u3k(axe));
-
-            //  u3t_push(u3nc(c3__mean, dun));
-            u3t_push(u3nc(c3__mean, niz));
-
-            return u3m_error("fish-loop");
-          } else {
-            u3_noun zoc = u3qdi_put(vit, sut);
-            u3_noun fop = u3qfu_repo(van, sut);
-            u3_noun pro = _fish_in(van, fop, axe, zoc);
-
-            u3z(fop);
-            u3z(zoc);
-
-            return pro;
-          }
-        }
-      }
-    }
-  }
-
   static u3_noun
   _cqfu_fish(u3_noun van,
              u3_noun sut,
              u3_atom axe)
   {
-    return _fish_in(van, sut, axe, u3_nul);
+    u3_noun von = u3i_molt(u3k(van), u3x_sam, u3k(sut), 0);
+    u3_noun gat = u3j_cook("_cqfu_fish-fish", von, "fish");
+
+    gat = u3i_molt(gat, u3x_sam, u3k(axe), 0);
+
+    return u3n_nock_on(gat, u3k(u3x_at(u3x_bat, gat)));
+
   }
 
 
@@ -157,7 +31,7 @@
     {
       return u3m_bail(c3__fail);
     } else {
-      return _cqfu_fish(van, sut, axe);
+      return u3qfu_fish(van, sut, axe);
     }
   }
 
