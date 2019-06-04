c0ef07d6edd6463e69dd29c9cc8bd26c577105f3
Logan Allen <loganallc@gmail.com>
2019-06-04 15:00:54 -0700

    Added header bar for navigating home

---
+++ apps/chat/src/js/components/lib/icons/icon-home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class IconHome extends Component {
+  render() {
+    return (
+      <img src="/~chat/img/Home.png" width={32} height={32} />
+    );
+  }
+}
--- apps/chat/src/js/components/skeleton.js
+++ apps/chat/src/js/components/skeleton.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 
+import { HeaderBar } from '/components/lib/header-bar.js';
 
 export class Skeleton extends Component {
   render() {
     return (
-      <div className="cf h-100 w-100 absolute flex">
-        <div className="fl h-100 br overflow-x-hidden" style={{ flexBasis: 320 }}>
-          {this.props.sidebar}
-        </div>
-        <div className="h-100 fr" style={{ flexGrow: 1 }}>
-          {this.props.children}
+      <div className="h-100 w-100 absolute">
+        <HeaderBar />
+        <div className="cf w-100 absolute flex"
+          style={{
+            height: 'calc(100% - 48px)' 
+          }}>
+          <div className="fl h-100 br overflow-x-hidden" style={{ flexBasis: 320 }}>
+            {this.props.sidebar}
+          </div>
+          <div className="h-100 fr" style={{ flexGrow: 1 }}>
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
