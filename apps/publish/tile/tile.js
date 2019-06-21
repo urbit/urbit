874f6f7e3ccb99182e65de57ec96f9f34742b210
Logan Allen <loganallc@gmail.com>
2019-06-21 16:13:57 -0700

    Removed webkit link inconsistency

--- apps/publish/tile/tile.js
+++ apps/publish/tile/tile.js
@@ -37,7 +37,7 @@ export default class PublishTile extends Component {
     return (
       <div className="w-100 h-100 relative" style={{background: "#1a1a1a"}}>
         <a className="w-100 h-100 db no-underline" href="/~publish">
-          <p className="gray-30 label-regular b absolute"
+          <p className="gray label-regular b absolute"
             style={{left: 8, top: 4}}>
             Publish
           </p>
