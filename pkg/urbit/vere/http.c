86d56e24c552efa388875e2d4b06e84325157157
Elliot Glaysher <elliot@tlon.io>
2019-07-12 14:23:51 -0700

    Maybe this fixes the transfer encoding issue. ¯\_(ツ)_/¯

--- pkg/urbit/vere/http.c
+++ pkg/urbit/vere/http.c
@@ -514,11 +514,16 @@ _http_start_respond(u3_hreq* req_u,
                       "hosed";
 
   u3_hhed* hed_u = _http_heds_from_noun(u3k(headers));
-
+  int has_content_length = 0;
   while ( 0 != hed_u ) {
     h2o_add_header_by_str(&rec_u->pool, &rec_u->res.headers,
                           hed_u->nam_c, hed_u->nam_w, 0, 0,
                           hed_u->val_c, hed_u->val_w);
+
+    if (strncmp(hed_u->nam_c, "content-length", 14) == 0) {
+      has_content_length = 1;
+    }
+
     hed_u = hed_u->nex_u;
   }
 
@@ -532,6 +537,10 @@ _http_start_respond(u3_hreq* req_u,
   gen_u->nud_u = 0;
   gen_u->hed_u = hed_u;
   gen_u->req_u = req_u;
+
+  if (has_content_length) {
+    rec_u->res.content_length = gen_u->bod_u ? gen_u->bod_u->len_w : 0;
+  }
 
   req_u->gen_u = gen_u;
 
