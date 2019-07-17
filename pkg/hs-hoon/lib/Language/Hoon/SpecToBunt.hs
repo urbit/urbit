1408d2bcd76e3fe6c0c851575022c383ce548e71
Benjamin Summers <benjamin@tlon.io>
2019-07-16 22:38:35 -0700

    Moved language stuff to another branch.

--- pkg/hs-hoon/lib/Language/Hoon/SpecToBunt.hs
+++
@@ -1,7 +0,0 @@
-module Language.Hoon.SpecToBunt (specToBunt) where
-
-import Prelude
-import Language.Hoon.Types
-
-specToBunt :: Bool -> Spec -> Hoon
-specToBunt = undefined
--- pkg/hs-hoon/lib/Language/Hoon/SpecToMold.hs
+++
@@ -1,9 +0,0 @@
-module Language.Hoon.SpecToMold (specToMold) where
-
-import Prelude
-import Language.Hoon.Types
-
--- | factory:ax. Given a spec and a boolean (?) produces a normalizing gate.
-specToMold :: Bool -> Spec -> Hoon
-specToMold fab spec = error "TODO specToMold"
-
