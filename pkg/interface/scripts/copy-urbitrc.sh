5244f96dd43104eea467ba9a1d43edcd44460c26
Isaac Visintainer <isaac.visintainer@gmail.com>
2019-07-23 12:59:01 -0700

    moved subapps up one directory

--- pkg/interface/scripts/copy-urbitrc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp urbitrc ./apps/chat/.urbitrc
-cp urbitrc ./apps/launch/.urbitrc
-cp urbitrc ./apps/modulo/.urbitrc
-cp urbitrc ./apps/publish/.urbitrc
-cp urbitrc ./apps/weather/.urbitrc
