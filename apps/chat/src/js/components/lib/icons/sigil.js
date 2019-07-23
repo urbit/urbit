2cb76fd8e31939130a21555a2a45cbbd3a3c8926
Isaac Visintainer <isaac.visintainer@gmail.com>
2019-07-23 11:32:13 -0700

    updated sigils for moons and comets

--- apps/chat/src/js/components/lib/icons/sigil.js
+++ apps/chat/src/js/components/lib/icons/sigil.js
@@ -6,20 +6,29 @@ export class Sigil extends Component {
   render() {
     const { props } = this;
 
-    return (
-      <div 
-        className="bg-black" 
-        style={{ flexBasis: 32 }}>
-      {
-        sigil({
-          patp: props.ship,
-          renderer: reactRenderer,
-          size: props.size,
-          colors: ['black', 'white'],
-        })
-      }
-      </div>
-    );
+    console.log("sigil ship", props.ship);
+
+    if (props.ship.length > 14) {
+      return (
+        <div className="bg-black" style={{width: 32, height: 32}}>
+        </div>
+      );
+    } else {
+      return (
+        <div 
+          className="bg-black" 
+          style={{ flexBasis: 32 }}>
+        {
+          sigil({
+            patp: props.ship,
+            renderer: reactRenderer,
+            size: props.size,
+            colors: ['black', 'white'],
+          })
+        }
+        </div>
+      );
+    }
   }
 }
 
