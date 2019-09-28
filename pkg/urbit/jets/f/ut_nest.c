9896f3d16f1aaf6a9ca25425aa7d194bc47a86e1
Joe Bryan <joemfb@gmail.com>
2019-09-27 17:02:27 -0700

    u3: refactors memoization patterns for safer rc
    specifically, avoiding uncounted references when
    memoizing in the compiler

--- pkg/urbit/jets/f/ut_nest.c
+++ pkg/urbit/jets/f/ut_nest.c
@@ -3,41 +3,46 @@
 */
 #include "all.h"
 
+u3_noun
+u3wfu_nest_dext(u3_noun dext_core)
+{
+  u3_noun nest_in_core, nest_core;
+  u3_noun sut, ref, van, seg, reg, gil;
 
-  u3_noun
-  u3wfu_nest_dext(u3_noun dext_core)
+  if ( (u3_none == (nest_in_core = u3r_at(3, dext_core))) ||
+       (c3n == u3r_mean(nest_in_core, u3x_sam_2, &seg,
+                                      u3x_sam_6, &reg,
+                                      u3x_sam_7, &gil,
+                                      7, &nest_core,
+                                      0)) ||
+       (c3n == u3r_mean(nest_core, u3x_sam_3, &ref,
+                                   u3x_con, &van,
+                                   0)) ||
+       (u3_none == (sut = u3r_at(u3x_sam, van))) )
   {
-    u3_noun nest_in_core, nest_core;
-    u3_noun sut, ref, van, seg, reg, gil;
+    return u3m_bail(c3__fail);
+  }
+  else {
+    c3_m  fun_m = 141 + c3__dext + ((!!u3r_at(u3qfu_van_vet, van)) << 8);
+    u3_noun key = u3z_key_2(fun_m, sut, ref);
+    u3_weak pro = u3z_find(key);
 
-    if ( (u3_none == (nest_in_core = u3r_at(3, dext_core))) ||
-         (c3n == u3r_mean(nest_in_core, u3x_sam_2, &seg,
-                                        u3x_sam_6, &reg,
-                                        u3x_sam_7, &gil,
-                                        7, &nest_core,
-                                        0)) ||
-         (c3n == u3r_mean(nest_core, u3x_sam_3, &ref,
-                                     u3x_con, &van,
-                                     0)) ||
-         (u3_none == (sut = u3r_at(u3x_sam, van))) )
-    {
-      return u3m_bail(c3__fail);
-    } else {
-      c3_m    fun_m = 141 + c3__dext + ((!!u3r_at(u3qfu_van_vet, van)) << 8);
-      u3_noun pro   = u3z_find_2(fun_m, sut, ref);
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(dext_core), u3k(u3x_at(u3x_bat, dext_core)));
 
-      if ( u3_none != pro ) {
-        return pro;
+      if ( ((c3y == pro) && (u3_nul == reg)) ||
+           ((c3n == pro) && (u3_nul == seg)) )
+      {
+        return u3z_save(key, pro);
       }
       else {
-        pro = u3n_nock_on(u3k(dext_core), u3k(u3x_at(u3x_bat, dext_core)));
-
-        if ( ((c3y == pro) && (u3_nul == reg)) ||
-             ((c3n == pro) && (u3_nul == seg)) )
-        {
-          return u3z_save_2(fun_m, sut, ref, pro);
-        }
-        else return pro;
+        u3z(key);
+        return pro;
       }
     }
   }
+}
--- pkg/urbit/noun/zave.c
+++ pkg/urbit/noun/zave.c
@@ -3,87 +3,63 @@
 */
 #include "all.h"
 
+/* u3z_key(): construct a memo cache-key.  Arguments retained.
+*/
+u3_noun
+u3z_key(c3_m fun, u3_noun one)
+{
+  return u3nc(fun, u3k(one));
+}
+u3_noun
+u3z_key_2(c3_m fun, u3_noun one, u3_noun two)
+{
+  return u3nt(fun, u3k(one), u3k(two));
+}
+u3_noun
+u3z_key_3(c3_m fun, u3_noun one, u3_noun two, u3_noun tri)
+{
+  return u3nq(fun, u3k(one), u3k(two), u3k(tri));
+}
+u3_noun
+u3z_key_4(c3_m fun, u3_noun one, u3_noun two, u3_noun tri, u3_noun qua)
+{
+  return u3nc(fun, u3nq(u3k(one), u3k(two), u3k(tri), u3k(qua)));
+}
+
 /* u3z_find(): find in memo cache.  Arguments retained.
 */
 u3_weak
-u3z_find(c3_m fun, u3_noun one)
+u3z_find(u3_noun key)
+{
+  return u3h_get(u3R->cax.har_p, key);
+}
+u3_weak
+u3z_find_m(c3_m fun, u3_noun one)
 {
   u3_noun key = u3nc(fun, u3k(one));
-  u3_noun val;
-
-  val = u3h_get(u3R->cax.har_p, key);
-  u3z(key);
-  return val;
-}
-u3_weak
-u3z_find_2(c3_m fun, u3_noun one, u3_noun two)
-{
-  u3_noun key = u3nt(fun, u3k(one), u3k(two));
-  u3_noun val;
-
-  val = u3h_get(u3R->cax.har_p, key);
-  u3z(key);
-  return val;
-}
-u3_weak
-u3z_find_3(c3_m fun, u3_noun one, u3_noun two, u3_noun tri)
-{
-  u3_noun key = u3nq(fun, u3k(one), u3k(two), u3k(tri));
-  u3_noun val;
-
-  val = u3h_get(u3R->cax.har_p, key);
-  u3z(key);
-  return val;
-}
-u3_weak
-u3z_find_4(c3_m fun, u3_noun one, u3_noun two, u3_noun tri, u3_noun qua)
-{
-  u3_noun key = u3nc(fun, u3nq(u3k(one), u3k(two), u3k(tri), u3k(qua)));
-  u3_noun val;
+  u3_weak val;
 
   val = u3h_get(u3R->cax.har_p, key);
   u3z(key);
   return val;
 }
 
-/* u3z_save*(): save in memo cache.
+/* u3z_save(): save in memo cache. TRANSFER key; RETAIN val
 */
 u3_noun
-u3z_save(c3_m fun, u3_noun one, u3_noun val)
+u3z_save(u3_noun key, u3_noun val)
+{
+  u3h_put(u3R->cax.har_p, key, u3k(val));
+  u3z(key);
+  return val;
+}
+
+/* u3z_save_m(): save in memo cache. Arguments retained.
+*/
+u3_noun
+u3z_save_m(c3_m fun, u3_noun one, u3_noun val)
 {
   u3_noun key = u3nc(fun, u3k(one));
-
-  u3h_put(u3R->cax.har_p, key, u3k(val));
-  u3z(key);
-  return val;
-}
-u3_noun
-u3z_save_2(c3_m fun, u3_noun one, u3_noun two, u3_noun val)
-{
-  u3_noun key = u3nt(fun, u3k(one), u3k(two));
-
-  u3h_put(u3R->cax.har_p, key, u3k(val));
-  u3z(key);
-  return val;
-}
-u3_noun
-u3z_save_3(c3_m fun, u3_noun one, u3_noun two, u3_noun tri, u3_noun val)
-{
-  u3_noun key = u3nq(fun, u3k(one), u3k(two), u3k(tri));
-
-  u3h_put(u3R->cax.har_p, key, u3k(val));
-  u3z(key);
-  return val;
-}
-u3_noun
-u3z_save_4(c3_m fun,
-             u3_noun one,
-             u3_noun two,
-             u3_noun tri,
-             u3_noun qua,
-             u3_noun val)
-{
-  u3_noun key = u3nc(fun, u3nq(u3k(one), u3k(two), u3k(tri), u3k(qua)));
 
   u3h_put(u3R->cax.har_p, key, u3k(val));
   u3z(key);
