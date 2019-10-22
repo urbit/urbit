536e56d6755774c78c075590b5dd2c0f5cd58628
Jared Tobin <jared@tlon.io>
2019-10-23 05:49:00 +0800

    vere: change default bootstrap pill URI
    Vere will attempt to download a pill from https://bootstrap.urbit.org if
    not given one explicitly.  These pill filenames have traditionally had
    the format:
    
      urbit-$URBIT_VERSION.pill
    
    but this is inconsistent with the manner in which versions are referred
    to elsewhere.  For example, release binaries are packaged in tarballs
    with the names:
    
      urbit-linux64-v$URBIT_VERSION.tgz
      urbit-darwin-v$URBIT_VERSION.tgz
    
    Note the 'v' prepended to URBIT_VERSION.
    
    From v0.9.2 forward it is expected that bootstrap pills will also use
    the 'v' prefix, i.e.
    
      urbit-v$URBIT_VERSION.pill
    
    so this commit makes the appropriate change in the daemon.

--- pkg/urbit/daemon/main.c
+++ pkg/urbit/daemon/main.c
@@ -298,7 +298,7 @@ _main_getopt(c3_i argc, c3_c** argv)
            && u3_Host.ops_u.url_c == 0
            && u3_Host.ops_u.git == c3n ) {
     u3_Host.ops_u.url_c =
-      "https://bootstrap.urbit.org/urbit-" URBIT_VERSION ".pill";
+      "https://bootstrap.urbit.org/urbit-v" URBIT_VERSION ".pill";
   }
   else if ( u3_Host.ops_u.nuu == c3y
            && u3_Host.ops_u.url_c == 0
