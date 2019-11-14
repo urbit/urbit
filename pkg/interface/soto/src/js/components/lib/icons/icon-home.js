859954f5e10e2ad6c22606c66ce358488a8b422c
Matilde Park <matilde@park.computer>
2019-11-14 13:48:48 -0500

    interface: add soto source code
    This adds the source code for Soto's Landscape interface to the
    interface pkg directory, matching the build workflow for our other
    Landscape applications as well as their dependencies.

---
+++ pkg/interface/soto/src/js/components/lib/icons/icon-home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class IconHome extends Component {
+  render() {
+    return (
+      <img src="/~launch/img/Home.png" width={32} height={32} />
+    );
+  }
+}
---
+++ pkg/interface/soto/src/js/components/root.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import classnames from 'classnames';
+import _ from 'lodash';
+import { HeaderBar } from './lib/header-bar';
+import { History } from './history';
+import { Input } from './input';
+import { api } from '../api';
+import { store } from '../store';
+
+export class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.state;
+    store.setStateHandler(this.setState.bind(this));
+    let ship = window.ship;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div style={{height: "100vh"}}>
+        <HeaderBar/>
+        <Route exact path="/~soto" render={ () => {
+          return (
+            <div className="pa3 bg-black mono gray3 w-100"
+            style={{lineHeight: "1.4", height: "calc(100% - 48px)", cursor: "text"}}>
+              <History history={this.state.txt}/>
+              <Input ship={ship} cursor={this.state.cursor} prompt={this.state.prompt} input={this.state.input}/>
+            </div>
+          )}}
+        />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
