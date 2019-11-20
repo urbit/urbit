c166dff91cb3c3f45f752cc80683ad888e5b528e
Joe Bryan <joemfb@gmail.com>
2019-11-19 16:44:32 -0800

    u3: adds jam variants to measure and encode in separate passes

--- pkg/urbit/include/noun/serial.h
+++ pkg/urbit/include/noun/serial.h
@@ -11,3 +11,16 @@
       */
         c3_w*
         u3s_jam_fib(u3_noun a, c3_w* bit_w);
+
+      /* u3s_jam_met(): measure a noun for jam, calculating backrefs
+      */
+        c3_d
+        u3s_jam_met(u3_noun a, u3p(u3h_root)* bak_p);
+
+      /* u3s_jam_buf(): jam [a] into [buf_w], without allocation
+      **
+      **   using backrefs in [bak_p], as computed by u3s_jam_met
+      **   can only encode up to c3_w bits
+      */
+        void
+        u3s_jam_buf(u3_noun a, u3p(u3h_root) bak_p, c3_w* buf_w);
