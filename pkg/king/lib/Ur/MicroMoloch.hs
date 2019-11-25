70b77ae55bedf00f73a670d60312942afd075498
Benjamin Summers <benjamin@tlon.io>
2019-11-24 20:40:42 -0800

    MicroMoloch interpreter gets working jet dashboard.

--- pkg/king/lib/Ur/MicroMoloch.hs
+++ pkg/king/lib/Ur/MicroMoloch.hs
@@ -10,9 +10,10 @@ module Ur.MicroMoloch where
 import Ur.Common hiding (flat)
 import GHC.Natural
 
-import Control.Lens (view, from, (^.))
-import Data.Flat    (Flat, flat, unflat)
-import Noun.Atom    (atomBytes)
+import Control.Lens     (from, view, (^.))
+import Data.Flat        (Flat, flat, unflat)
+import Noun.Atom        (atomBytes)
+import System.IO.Unsafe (unsafePerformIO)
 
 --------------------------------------------------------------------------------
 
@@ -26,89 +27,91 @@ data Exp
     {- 00 -} = Var Integer
     {- 01 -} | Lam Exp
     {- 02 -} | App Exp Exp
-
     {- 03 -} | Lit Nat
     {- 04 -} | Inc Exp
     {- 05 -} | Fun Exp
-
     {- 06 -} | Jet Exp
     {- 07 -} | Fir Exp Exp
-
     {- 08 -} | Eva Exp
     {- 09 -} | Dum Exp
-  deriving (Eq, Ord, Show, Generic)
+  deriving (Eq, Ord, Generic)
   deriving anyclass (Flat, NFData)
 
 
 -- Printing --------------------------------------------------------------------
 
-prettyExp :: Exp -> String
-prettyExp = go
-  where
-    appList (App f x) acc = appList f (x:acc)
-    appList e         acc = (e:acc)
+instance Show Exp where
+  show = go
+    where
+      appList (App f x) acc = appList f (x:acc)
+      appList e         acc = (e:acc)
 
-    go = \case
-        Var i   → show i
-        Lam e   → "\\" <> go e
-        App f x → "("  <> intercalate " " (go <$> appList f [x]) <> ")"
-        Lit n   → "#"  <> show n
-        Inc x   → "+"  <> go x
-        Fun x   → "%"  <> go x
-        Jet x   → "@"  <> go x
-        Fir f j → "!"  <> go (App f j)
-        Eva x   → "?"  <> go x
-        Dum x   → "_"  <> go x
-
-printExp :: Exp -> IO ()
-printExp = putStrLn . pack . prettyExp
+      go = \case
+          Var i   → show i
+          Lam e   → "\\" <> go e
+          App f x → "("  <> intercalate " " (go <$> appList f [x]) <> ")"
+          Lit n   → "#"  <> show n
+          Inc x   → "+"  <> go x
+          Fun x   → "%"  <> go x
+          Jet x   → "@"  <> go x
+          Fir f j → "!"  <> go (App f j)
+          Eva x   → "?"  <> go x
+          Dum x   → "_"  <> go x
 
 
 -- Simple Expressions ----------------------------------------------------------
 
-{-
-    zero = \z i → z
-    succ n = \z i → i (n z i)
--}
+--  zero   = \z i → z
+--  succ n = \z i → i (n z i)
 natFun :: Nat -> Exp
-natFun 0 = Lam $ Lam $ Var 1
-natFun n = Lam $ Lam $ App (Var 0) (natFun (pred n) `App` Var 1 `App` Var 0)
+natFun 0 = Lam $ Lam (Var 1)
+natFun n = Lam $ Lam (Var 0 `App` (natFun (pred n) `App` Var 1 `App` Var 0))
 
-{-
-    lef x l r = l x
-    rit x l r = l x
--}
+--  lef x l r = l x
+--  rit x l r = l x
 lef, rit :: Exp -> Exp
-lef x = Lam $ Lam (App (Var 1) x)
-rit x = Lam $ Lam (App (Var 0) x)
+lef x = Lam $ Lam (Var 1 `App` x)
+rit x = Lam $ Lam (Var 0 `App` x)
 
-{-
-    () = \x→x
--}
+--  () = \x→x
 unit :: Exp
 unit = Lam (Var 0)
 
 
--- Interpreter -----------------------------------------------------------------
+-- Ig -- Values with no semantic meaning ---------------------------------------
+
+newtype Ig a = Ig a
+
+instance Eq (Ig a) where
+  x == y = True
+
+instance Ord (Ig a) where
+  compare x y = EQ
+
+instance Show (Ig a)
+  where
+    show = const "Ig"
+
+
+-- Interpreter Values ----------------------------------------------------------
 
 data Val
     = VFun Exp [Val]
     | VNat Nat
-    | VJet Val
+    | VJet Val (Ig (Exp → [Val] → IO Val))
   deriving (Eq, Ord, Show)
 
-prettyVal :: Val -> String
-prettyVal = prettyExp . valExp
-    -- VFun b ev → valExp
-    -- VFun b ev → prettyExp b <> "[" <> intercalate " " (prettyVal <$> ev) <> "]"
-    -- VNat n    → "#" <> show n
-    -- VJet x    → "@" <> prettyVal x
+-- instance Show Val where
+    -- show = show . valExp
 
-printVal :: Val -> IO ()
-printVal = putStrLn . pack . prettyVal
+
+-- Interpreter -----------------------------------------------------------------
 
 run :: Exp -> IO ()
-run = go 1 [] >=> printVal
+run = runEnv [] >=> print
+
+runEnv :: [Val] -> Exp -> IO Val
+runEnv = go 1
   where
     go :: Nat -> [Val] -> Exp -> IO Val
     go d env exp = do
@@ -116,7 +119,7 @@ run = go 1 [] >=> printVal
         let loop = go (succ d)
 
         -- let pre = replicate (fromIntegral d) '#' <> "  "
-        -- putStrLn $ pack (pre <> prettyExp exp)
+        -- putStrLn $ pack (pre <> show exp)
         -- for_ (zip [0..] env) $ \(i, v) → do
             -- let pre = "\t[" <> show i <> "]\t"
             -- putStrLn $ pack (pre <> prettyVal v)
@@ -132,10 +135,10 @@ run = go 1 [] >=> printVal
                           pure $ VNat $ succ n
             Fun x   -> do VNat n <- loop env x
                           loop [] (natFun n)
-            Jet x   -> VJet <$> loop env x
-            Fir f j -> do VFun b fenv <- loop env f
-                          VJet x      <- loop env j
-                          loop (x : fenv) b
+            Jet x   -> do xv ← loop env x
+                          pure (jet xv)
+            Fir f j -> do VJet x (Ig jf) <- loop env j
+                          jf f env
             Eva x   -> do VNat n <- loop env x
                           loop [] (eval n)
             Dum x   -> do xv <- loop env x
@@ -146,188 +149,117 @@ run = go 1 [] >=> printVal
 
         pure res
 
+
+-- Jets ------------------------------------------------------------------------
+
+dashboard :: Map Val (Text, Exp, [Val] → IO Val)
+dashboard = mapFromList
+    [ (incBody, ("INC", fire1Exp, incFast))
+    , (addBody, ("ADD", fire2Exp, addFast))
+    , (mulBody, ("MUL", fire2Exp, mulFast))
+    ]
+  where
+    incFast, addFast, mulFast :: [Val] → IO Val
+
+    incFast = \case (VNat x:_)        → pure $ VNat (succ x)
+                    _                 → error "bad-inc"
+    addFast = \case (VNat x:VNat y:_) → pure $ VNat (x + y)
+                    _                 → error "bad-add"
+    mulFast = \case (VNat x:VNat y:_) → pure $ VNat (x * y)
+                    _                 → error "bad-mul"
+
+    incBody = unsafePerformIO $ runEnv [] incRaw
+    addBody = unsafePerformIO $ runEnv [] addClose
+    mulBody = unsafePerformIO $ runEnv [] mulClose
+
+    fire1Exp = Lam (Var 0 `App` Var 1)
+    fire2Exp = Lam (Var 0 `App` Var 2 `App` Var 1)
+
+jet :: Val → Val
+jet jetVal = lookup jetVal dashboard & \case
+    Just (nm, exp, exe) → VJet jetVal $ Ig (match nm exp exe)
+    Nothing             → VJet jetVal $ Ig fallback
+  where
+    match :: Text → Exp → ([Val] → IO Val) -> Exp → [Val] → IO Val
+    match nam expect exe actual env =
+        (expect == actual) & \case
+            True  → putStrLn nam       >> exe env
+            False → putStrLn "NOMATCH" >> fallback actual env
+
+    fallback :: Exp -> [Val] -> IO Val
+    fallback exp env = do
+        runEnv env exp >>= \case
+            VFun b env → runEnv (jetVal : env) b
+            _          → error "Fir expects a function"
+
+
+-- Convert values to expressions for `uneval` ----------------------------------
+
 valExp :: Val -> Exp
 valExp = \case
     VNat n     → Lit n
-    VJet x     → Jet (valExp x)
+    VJet x _   → Jet (valExp x)
     VFun b []  → Lam b
     VFun b env → shift (negate $ fromIntegral $ length env)
-               $ foldl' (\exp (i,v) -> subst i (valExp v) exp) b
+               $ foldl' (\exp (i,v) -> subst i (valExp v) exp) (Lam b)
                $ zip [0..] env
-
-
--- Reductions ------------------------------------------------------------------
-
-{-| Substitute all occurrences of a variable with an expression
-
-> subst n C B  ~  B[x@n := C]
--}
-subst :: Integer -> Exp -> Exp -> Exp
-subst n e' e = case e of
-    Lam b   -> Lam (subst (succ n) (shift 1 e') b)
-    App f a -> App (subst n e' f)  (subst n e' a)
-    Var n'  -> if n == n' then e' else e
-    Lit n   -> Lit n
-    Inc x   -> Inc (subst n e' x)
-    Fun x   -> Fun (subst n e' x)
-    Jet x   -> Jet (subst n e' x)
-    Fir j a -> Fir (subst n e' j) (subst n e' a)
-    Eva x   -> Eva (subst n e' x)
-    Dum x   -> Dum (subst n e' x)
-
-{-| @shift n x@ adds @n@ to the index of all free variables named @x@ within an
-    `Exp`
--}
-shift :: Integer -> Exp -> Exp
-shift d e0 = go e0 0
   where
-    go e c = case e of
-        Lam b   -> Lam (go  b $! succ c)
-        App f a -> App (go f c) (go a c)
-        Var n   -> Var $! if n >= c then n + d else n
+    {-| Substitute all occurrences of a variable with an expression
+
+    > subst n C B  ~  B[x@n := C]
+    -}
+    subst :: Integer -> Exp -> Exp -> Exp
+    subst n e' e = case e of
+        Lam b   -> Lam (subst (succ n) (shift 1 e') b)
+        App f a -> App (subst n e' f)  (subst n e' a)
+        Var n'  -> if n == n' then e' else e
         Lit n   -> Lit n
-        Inc x   -> Inc (go x c)
-        Fun x   -> Fun (go x c)
-        Jet f   -> Jet (go f c)
-        Fir j a -> Fir (go j c) (go a c)
-        Eva x   -> Eva (go x c)
-        Dum x   -> Dum (go x c)
+        Inc x   -> Inc (subst n e' x)
+        Fun x   -> Fun (subst n e' x)
+        Jet x   -> Jet (subst n e' x)
+        Fir j a -> Fir (subst n e' j) (subst n e' a)
+        Eva x   -> Eva (subst n e' x)
+        Dum x   -> Dum (subst n e' x)
 
--- | Returns whether a variable is free in an expression
-freeIn :: Integer -> Exp -> Bool
-freeIn n = go
-  where
-    go = \case
-        Lam b   -> freeIn (succ n) b
-        Var n'  -> n == n'
-        App f x -> go f || go x
-        Lit _   -> False
-        Inc x   -> go x
-        Fun x   -> go x
-        Jet x   -> go x
-        Fir f x -> go f || go x
-        Eva x   -> go x
-        Dum x   -> go x
-
--- | Reduce an expression to weak-head normal form
-whnf :: Exp -> Exp
-whnf = \case
-    App f a -> app a (whnf f)
-    e       -> e
-  where
-    app a = \case
-        Lam b -> appLam a b
-        f'    -> App f' a
-
-    appLam a b = whnf (shift (-1) b')
+    {-| @shift n x@ adds @n@ to the index of all free variables named @x@
+        within an `Exp`
+    -}
+    shift :: Integer -> Exp -> Exp
+    shift d e0 = go e0 0
       where
-        a' = shift 1 a
-        b' = subst 0 a' b
+        go e c = case e of
+            Lam b   -> Lam (go  b $! succ c)
+            App f a -> App (go f c) (go a c)
+            Var n   -> Var $! if n >= c then n + d else n
+            Lit n   -> Lit n
+            Inc x   -> Inc (go x c)
+            Fun x   -> Fun (go x c)
+            Jet f   -> Jet (go f c)
+            Fir j a -> Fir (go j c) (go a c)
+            Eva x   -> Eva (go x c)
+            Dum x   -> Dum (go x c)
 
-{-| Reduce an expression to its normal form, performing both beta reduction and
-    eta reduction
 
-    `normalize` does not type-check the expression.  You may want to type-check
-    expressions before normalizing them since normalization can convert an
-    ill-typed expression into a well-typed expression.
--}
-normalize :: Exp -> Exp
-normalize e = case e of
-    Lam b -> case b' of App f a -> app f a
-                        _       -> Lam b'
-      where
-        b'    = normalize b
-        app f = whnf >>> \case
-            Var v' | 0 == v' && not (0 `freeIn` f) -> shift (-1) f
-            Var _                                  -> Lam b'
-            _                                      -> Lam b'
-
-    App f a -> normalize f & \case
-        Lam b -> normalize (shift (-1) b')
-                   where
-                     a' = shift 1 (normalize a)
-                     b' = subst 0 a' b
-        f'    -> App f' (normalize a)
-
-    Var _ -> e
-    Lit _ -> e
-
-    Inc x -> normalize x & \case
-       Lit n  -> Lit (succ n)
-       xn     -> Inc xn
-
-    Fun x -> normalize x & \case
-        Lit n -> natFun n
-        e     -> Fun e
-
-    Jet f   -> Jet (normalize f)
-    Fir f j -> Fir (normalize f) (normalize j)
-    Eva x   -> Eva (normalize x)
-    Dum x   -> Dum (normalize x)
-
-exe :: Exp -> Exp
-exe e = case e of
-    Lam b -> case b' of
-        App f a -> app f a
-        _       -> Lam b'
-      where
-        b'    = exe b
-        app f = whnf >>> \case
-            Var v' | 0 == v' && not (0 `freeIn` f) -> shift (-1) f
-            Var _                                  -> Lam b'
-            _                                      -> Lam b'
-
-    App f a -> exe f & \case
-        Lam b -> exe (shift (-1) b')
-                   where
-                     a' = shift 1 (exe a)
-                     b' = subst 0 a' b
-        f'    -> App f' (exe a)
-
-    Var _ -> e
-    Lit _ -> e
-
-    Inc x -> exe x & \case
-       Lit n  -> Lit (succ n)
-       xn     -> Inc xn
-
-    Fun x -> exe x & \case
-        Lit n -> natFun n
-        e     -> Fun e
-
-    Jet f   -> Jet (exe f)
-
-    Fir f j -> exe j & \case Jet x -> exe (App f x)
-                             j'    -> Fir f j'
-
-    Eva x   -> exe x & \case Lit n -> exe (eval n)
-                             x'    -> Eva x'
-
-    Dum x   -> Lit (dump (exe x))
-
+-- Eval and Dump ---------------------------------------------------------------
 
 eval :: Nat -> Exp
 eval expNat =
+    -- maybe (lef unit) rit (decodeExp expNat)
     decodeExp expNat & \case
         Just exp -> exp
         Nothing  -> error "bad eval"
-    -- maybe (lef unit) rit $ do
-        -- decodeExp expNat
+  where
+    decodeExp :: Nat -> Maybe Exp
+    decodeExp n =
+        unflat (n ^. atomBytes) & \case
+            Left err -> Nothing
+            Right ex -> pure ex
 
 dump :: Exp -> Nat
-dump = encodeExp
-
-decodeExp :: Nat -> Maybe Exp
-decodeExp n =
-    unflat (n ^. atomBytes) & \case
-        Left err -> Nothing
-        Right ex -> pure ex
-
-encodeExp :: Exp -> Nat
-encodeExp = view (from atomBytes) . flat
+dump = view (from atomBytes) . flat
 
 
--- Examples --------------------------------------------------------------------
+-- Jetted Increment, Add, and Multiply -----------------------------------------
 
 jet1 :: Exp
 jet1 =  Lam $ Lam $ Fir (Lam (Var 0 `App` Var 1)) (Jet $ Var 1)
@@ -335,36 +267,28 @@ jet1 =  Lam $ Lam $ Fir (Lam (Var 0 `App` Var 1)) (Jet $ Var 1)
 jet2 :: Exp
 jet2 =  Lam $ Lam $ Lam $ Fir (Lam (Var 0 `App` Var 2 `App` Var 1))
                               (Jet $ Var 2)
-
+--  inc x = +(x)
 incRaw ∷ Exp
 incRaw = Lam $ Inc (Var 0)
 
-inc ∷ Exp
-inc = App jet1 incRaw
-
-{-
-    add x y = foldNat y (foldNat x 0 inc) inc
--}
+--  add inc x y = (%x (%y 0 inc) inc)
 addRaw :: Exp
-addRaw = (Lam . Lam)
-        $ (Fun $ Var 1)
-            `App` ((Fun $ Var 0) `App` Lit 0 `App` incRaw)
-            `App` incRaw
+addRaw =
+  Lam $ Lam $ Lam $ (Fun (Var 1))
+                      `App` ((Fun (Var 0)) `App` Lit 0 `App` Var 2)
+                      `App` (Var 2)
 
-{-
-    add x y = foldNat y (foldNat x 0 inc) inc
--}
-add :: Exp
-add = (App jet2 . Lam . Lam)
-    $ (Fun $ Var 1)
-        `App` ((Fun $ Var 0) `App` Lit 0 `App` inc)
-        `App` inc
+--  mul add x y = (%x 0 (add y))
+mulRaw :: Exp
+mulRaw = Lam $ Lam $ Lam $ (Fun (Var 1))
+                             `App` Lit 0
+                             `App` (Var 2 `App` Var 0)
 
-{-
-    mul x y = foldNat x 0 (add y)
--}
-mul :: Exp
-mul = (App jet2 . Lam . Lam)
-    $ (Fun $ Var 1)
-        `App` Lit 0
-        `App` (add `App` Var 0)
+addClose, mulClose :: Exp
+addClose = addRaw `App` inc
+mulClose = mulRaw `App` add
+
+inc, add, mul :: Exp
+inc = jet1 `App` incRaw
+add = jet2 `App` addClose
+mul = jet2 `App` mulClose
