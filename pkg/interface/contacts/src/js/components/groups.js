1ea0e59e64fa7796695607dcc3a3d5318cba1c48
Matilde Park <matilde@park.computer>
2019-11-26 18:33:38 -0500

    root -> new navigation

--- pkg/interface/contacts/src/js/components/groups.js
+++ pkg/interface/contacts/src/js/components/groups.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+import { Route, Link } from 'react-router-dom';
+
 export class Groups extends Component {
   // drawer to the left
   render() {
     return (
-      <div className="br b--gray4 h-100 flex-basis-100-s flex-basis-300-ns">
+      <div className="br b--gray4 h-100 flex-basis-100-s flex-basis-300-ns relative">
         <h2 className="f9 pa4 gray2 c-default">Your Root Identity</h2>
         <h2 className="f9 pa4 gray2 c-default">Your Groups</h2>
+        <div
+          className="dt bt b--gray4 absolute w-100"
+          style={{ bottom: 0, height: 48 }}>
+          <Link to="/~contacts/new" className="dtc v-mid">
+            <p className="f9 pl4 black bn">Create New Group</p>
+          </Link>
+        </div>
       </div>
     );
   }
