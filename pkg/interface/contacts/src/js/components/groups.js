258e4d8f1a767854cf55be55cafb563ca27c3c20
Matilde Park <matilde@tlon.io>
2019-12-03 22:56:16 -0500

    iterate all groups with relative links

--- pkg/interface/contacts/src/js/components/groups.js
+++ pkg/interface/contacts/src/js/components/groups.js
@@ -7,12 +7,23 @@ export class Groups extends Component {
   // drawer to the left
 
   render() {
-    let groupItems = this.props.contacts;
+    const { props, state } = this;
+
+    let groupItems = Object.keys(props.contacts)
+    .map((path) => {
+      return (
+        <GroupsItem
+        key={path}
+        link={path}
+        group={props.contacts[path]}/>
+      )
+    });
 
     return (
       <div className="br b--gray4 h-100 flex-basis-100-s flex-basis-300-ns relative">
         <h2 className="f9 pa4 gray2 c-default">Your Root Identity</h2>
         <h2 className="f9 pa4 gray2 c-default">Your Groups</h2>
+        {groupItems}
         <div
           className="dt bt b--gray4 absolute w-100"
           style={{ bottom: 0, height: 48 }}>
