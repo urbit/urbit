a5e68f280dbd188e953396e940f39d827f93278b
Matilde Park <matilde@tlon.io>
2020-01-07 20:14:39 -0500

    soto: add "dead" alt keys to ignored list
    alt+n produces a ~, on top of a key called "Dead". This ignores "dead"
    to send the ~ and function as normal.

--- pkg/interface/soto/src/js/components/input.js
+++ pkg/interface/soto/src/js/components/input.js
@@ -19,7 +19,8 @@ export class Input extends Component {
     let ignoredKeys = ["Meta", "Alt", "Control", "Escape", "Shift",
                        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
                        "F9", "F10", "F11", "F12", "Backspace", "Unidentified",
-                       "Delete", "Insert", "Home", "PageUp", "PageDown", "End"
+                       "Delete", "Insert", "Home", "PageUp", "PageDown", "End",
+                       "Dead"
                       ];
 
   // submit on enter
