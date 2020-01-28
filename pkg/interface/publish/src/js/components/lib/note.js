70fcbf9e706d4d5d5d0ee784345ad3f2721df3eb
Jimmy <jyng02@gmail.com>
2020-01-27 16:03:19 -0800

    creating scaffold of post view

--- pkg/interface/publish/src/js/components/lib/note.js
+++ pkg/interface/publish/src/js/components/lib/note.js
@@ -9,7 +9,22 @@ export class Note extends Component {
   render() {
     return (
       <div>
-        <Comments/>
+      <div className="f7 sans">Title</div>
+      <div className="f7 mono gray-60">Author</div>
+      <Body/>
+      <Previous className="pv10 bt br bb b-gray">
+        <h7>Previous Post</h7>
+        <h7>Title</h7>
+        <h7 className="gray">Date</h7>
+      </Previous>
+      <Next className="pv10 bt bl bb b-gray">
+      <h7>Next Post</h7>
+      <h7>Title</h7>
+      <h7 className="gray">Date</h7>
+      </Next>
+
+
+      <Comments/>
       </div>
     )
   }
