eb2a8ba6af09959aa668d38bd1d0961dd19dab33
Jared Tobin <jared@tlon.io>
2020-02-25 17:39:35 +0400

    build: fix arvo-ropsten build
    Employ the same force/rcp usage pattern used in the regular Arvo
    derivation.

--- nix/pkgs/arvo-ropsten/builder.sh
+++ nix/pkgs/arvo-ropsten/builder.sh
@@ -25,42 +25,61 @@ sed --in-place \
 cp -r tmp $out
 chmod -R u+w $out
 
-rm $out/.gitignore
-rm $out/.travis.yml
+rm -f $out/.gitignore
+rm -f $out/.travis.yml
 rm -rf $out/.travis
+
+rcp() {
+  if [ ! -f $1 ]; then
+    >&2 echo "WARNING: $1 does not exist!"
+  else
+    cp $1 $2
+  fi
+}
+
+# chat
 
 mkdir -p $out/app/chat/css
 mkdir -p $out/app/chat/js
 
-cp $landscape/chat/index.css $out/app/chat/css/index.css
-cp $landscape/chat/index-min.js $out/app/chat/js/index-min.js
-cp $landscape/chat/tile-min.js $out/app/chat/js/tile-min.js
+rcp $landscape/chat/index.css $out/app/chat/css/index.css
+rcp $landscape/chat/index.js $out/app/chat/js/index.js
+rcp $landscape/chat/tile.js $out/app/chat/js/tile.js
+
+# clock
 
 mkdir -p $out/app/clock/js
 
-cp $landscape/clock/tile-min.js $out/app/clock/js/tile-min.js
+rcp $landscape/clock/tile.js $out/app/clock/js/tile.js
+
+# launch
 
 mkdir -p $out/app/launch/css
 mkdir -p $out/app/launch/js
 
-cp $landscape/launch/index.css $out/app/launch/css/index.css
-cp $landscape/launch/index-min.js $out/app/launch/js/index-min.js
+rcp $landscape/launch/index.css $out/app/launch/css/index.css
+rcp $landscape/launch/index.js $out/app/launch/js/index.js
 
+# publish
 mkdir -p $out/app/publish/css
 mkdir -p $out/app/publish/js
 
-cp $landscape/publish/index.css $out/app/publish/css/index.css
-cp $landscape/publish/index-min.js $out/app/publish/js/index-min.js
-cp $landscape/publish/tile-min.js $out/app/publish/js/tile-min.js
+rcp $landscape/publish/index.css $out/app/publish/css/index.css
+rcp $landscape/publish/index.js $out/app/publish/js/index.js
+rcp $landscape/publish/tile.js $out/app/publish/js/tile.js
+
+# soto
 
 mkdir -p $out/app/soto/css
 mkdir -p $out/app/soto/js
 
-cp $landscape/soto/index.css $out/app/soto/css/index.css
-cp $landscape/soto/index-min.js $out/app/soto/js/index-min.js
-cp $landscape/soto/tile-min.js $out/app/soto/js/tile-min.js
+rcp $landscape/soto/index.css $out/app/soto/css/index.css
+rcp $landscape/soto/index.js $out/app/soto/js/index.js
+rcp $landscape/soto/tile.js $out/app/soto/js/tile.js
+
+# weather
 
 mkdir -p $out/app/weather/js
 
-cp $landscape/weather/tile-min.js $out/app/weather/js/tile-min.js
+rcp $landscape/weather/tile.js $out/app/weather/js/tile.js
 
