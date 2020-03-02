6dd84b467e519f24f76d4e97c547656ba6412947
Matilde Park <matilde@tlon.io>
2020-03-02 18:38:30 -0500

    contacts: add bg-gray0 to root page

--- pkg/interface/contacts/src/js/components/root.js
+++ pkg/interface/contacts/src/js/components/root.js
@@ -50,7 +50,7 @@ export class Root extends Component {
                 contacts={contacts}
                 groups={groups}
                 invites={invites}>
-                <div className="h-100 w-100 overflow-x-hidden bg-white dn db-ns">
+                <div className="h-100 w-100 overflow-x-hidden bg-white bg-gray0-d dn db-ns">
                   <div className="pl3 pr3 pt2 dt pb3 w-100 h-100">
                     <p className="f9 pt3 gray2 w-100 h-100 dtc v-mid tc">
                       Select a group to begin.
