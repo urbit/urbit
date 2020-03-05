ed71bd5bb063c9fbea78a25b740016f8437ffc82
Benjamin Summers <benjamin@tlon.io>
2020-03-04 22:29:05 -0800

    Don't freeze up while computing expensive things.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,16 +1,20 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Frontend where
 
-import Prelude
+import ClassyPrelude
+import Prelude ()
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (eval, liftJSM)
 import Urbit.Atom
 import Reflex.Dom.Widget.Input
+import Reflex.Host.Class
 
 import Obelisk.Frontend
 import Obelisk.Configs
@@ -27,8 +31,75 @@ import Urbit.Moon.Repl     (evalTextFast, evalText)
 
 --------------------------------------------------------------------------------
 
+fastResult
+  :: ( TriggerEvent t m
+     , MonadHold t m
+     , Reflex t
+     , PerformEvent t m
+     , MonadIO (Performable m)
+     )
+  => Dynamic t Text
+  -> m (Dynamic t Text)
+fastResult txt = do
+  (e, f) <- newTriggerEvent
+  performEvent_ $ fmap (compute fastBrain f evalTextFast) $ updated txt
+  res <- holdDyn "Thinking..." e
+  pure res
+
+slowResult
+  :: ( TriggerEvent t m
+     , MonadHold t m
+     , Reflex t
+     , PerformEvent t m
+     , MonadIO (Performable m)
+     )
+  => Dynamic t Text
+  -> m (Dynamic t Text)
+slowResult txt = do
+  (e, f) <- newTriggerEvent
+  performEvent_ $ fmap (compute slowBrain f evalText) $ updated txt
+  res <- holdDyn "Thinking..." e
+  pure res
+
+
+  -- on text change
+  --   kill running thread
+  --   result becomes "Thinking..."
+  --   computation starts
+  --   on computation finish
+  --     text becomes result.
+
+fastBrain :: MVar (Maybe (Async ()))
+fastBrain = unsafePerformIO (newMVar Nothing)
+
+slowBrain :: MVar (Maybe (Async ()))
+slowBrain = unsafePerformIO (newMVar Nothing)
+
+compute
+  :: MonadIO m
+  => MVar (Maybe (Async ()))
+  -> (Text -> IO ())
+  -> (Text -> IO Text)
+  -> Text
+  -> m ()
+compute ref cb exec txt = do
+  takeMVar ref >>= maybe (pure ()) cancel
+  liftIO (cb "Thinking...")
+  tid <- liftIO $ async $ (exec txt >>= liftIO . cb)
+  putMVar ref (Just tid)
+
 slow
-  :: (Monad m, MonadSample s m, Reflex s, DomBuilder s m, PostBuild s m) => m ()
+  :: ( Monad m
+     , MonadSample s m
+     , Reflex s
+     , DomBuilder s m
+     , PostBuild s m
+     , PerformEvent s m
+     , MonadHold s m
+     , TriggerEvent s m
+     , MonadIO (Performable m)
+     )
+  => m ()
 slow = do
   el "h1" (text "Slow")
 
@@ -37,11 +108,23 @@ slow = do
     $  inputElement
     $  (def & inputElementConfig_initialValue .~ "(K K K)")
 
-  el "pre" $ dynText $ unsafePerformIO . evalText <$> val
+  res <- slowResult val
+
+  el "pre" (dynText res)
 
 
 fast
-  :: (Monad m, MonadSample s m, Reflex s, DomBuilder s m, PostBuild s m) => m ()
+  :: ( Monad m
+     , MonadSample s m
+     , Reflex s
+     , DomBuilder s m
+     , PostBuild s m
+     , PerformEvent s m
+     , MonadHold s m
+     , TriggerEvent s m
+     , MonadIO (Performable m)
+     )
+  => m ()
 fast = do
   el "h1" (text "Fast")
 
@@ -50,7 +133,9 @@ fast = do
     $  inputElement
     $  (def & inputElementConfig_initialValue .~ "(K K K)")
 
-  el "pre" $ dynText $ unsafePerformIO . evalTextFast <$> val
+  res <- fastResult val
+
+  el "pre" $ dynText res
 
 
 -- This runs in a monad that can be run on the client or the server.
