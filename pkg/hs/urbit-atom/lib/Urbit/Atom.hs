cabd2c24059ade0f5cf8f7d9d9e52e1ab0a2fdc7
Benjamin Summers <benjamin@tlon.io>
2020-03-06 14:05:17 -0800

    autofmt Urbit.Atom

--- pkg/hs/urbit-atom/lib/Urbit/Atom.hs
+++ pkg/hs/urbit-atom/lib/Urbit/Atom.hs
@@ -4,11 +4,17 @@
 -}
 
 module Urbit.Atom
-    ( Atom
-    , atomBytes, bytesAtom
-    , atomWords, wordsAtom
-    , utf8Atom, atomUtf8, atomUtf8Exn, atomUtf8Lenient
-    ) where
+  ( Atom
+  , atomBytes
+  , bytesAtom
+  , atomWords
+  , wordsAtom
+  , utf8Atom
+  , atomUtf8
+  , atomUtf8Exn
+  , atomUtf8Lenient
+  )
+where
 
 import Prelude
 
@@ -29,33 +35,33 @@ type Atom = Natural
 --------------------------------------------------------------------------------
 
 -- | Cast an atom to a vector. Does not copy.
-atomWords :: Atom → Vector Word
+atomWords :: Atom -> Vector Word
 atomWords = I.natWords
 
 -- | Cast a vector to an atom. Does not copy unless given a slice.
-wordsAtom :: Vector Word → Atom
+wordsAtom :: Vector Word -> Atom
 wordsAtom = I.wordsNat
 
 -- | Dump an atom to a bytestring.
-atomBytes ∷ Atom → ByteString
+atomBytes :: Atom -> ByteString
 atomBytes = I.pillBytes . I.natPill
 
 -- | Load a bytestring into an atom.
-bytesAtom ∷ ByteString → Atom
+bytesAtom :: ByteString -> Atom
 bytesAtom = I.pillNat . I.bytesPill
 
 -- | Encode a utf8-encoded atom from text.
-utf8Atom ∷ T.Text → Atom
+utf8Atom :: T.Text -> Atom
 utf8Atom = bytesAtom . T.encodeUtf8
 
 -- | Interpret an atom as utf8 text.
-atomUtf8 ∷ Atom → Either T.UnicodeException T.Text
+atomUtf8 :: Atom -> Either T.UnicodeException T.Text
 atomUtf8 = T.decodeUtf8' . atomBytes
 
 -- | Interpret an atom as utf8 text, throwing an exception on bad unicode.
-atomUtf8Exn ∷ Atom → T.Text
+atomUtf8Exn :: Atom -> T.Text
 atomUtf8Exn = T.decodeUtf8 . atomBytes
 
 -- | Interpret an atom as utf8 text, replacing bad unicode characters.
-atomUtf8Lenient ∷ Atom → T.Text
+atomUtf8Lenient :: Atom -> T.Text
 atomUtf8Lenient = T.decodeUtf8With T.lenientDecode . atomBytes
