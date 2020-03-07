12aba8cdbd2c9c35ba2518bb749b4923f8a16437
Benjamin Summers <benjamin@tlon.io>
2020-03-06 19:34:53 -0800

    vere: Hack to get sigsegv building on ARMv6

--- nix/nixcrpkgs/pkgs/libsigsegv/builder.sh
+++ nix/nixcrpkgs/pkgs/libsigsegv/builder.sh
@@ -5,6 +5,13 @@ tar -xf $src
 mkdir build
 cd build
 
+# Hack to get this building on ARM
+if [ $host = armv6-linux-musleabi ]
+then
+  sed -i 's/fault-linux-arm-old.h/fault-linux-arm.h/' \
+    ../libsigsegv-$version/configure
+fi
+
 ../libsigsegv-$version/configure \
   --host=$host \
   --prefix=$out \
