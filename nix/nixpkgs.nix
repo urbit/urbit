a08a785b3b47602fa528b1fc5f002f3cbd23e03d
Benjamin Summers <benjamin@tlon.io>
2020-03-06 18:01:17 -0800

    vere: Bump nixpkgs version.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,9 @@
 let
 
-  rev  = "61c3169a0e17d789c566d5b241bfe309ce4a6275";
-  hash = "0qbycg7wkb71v20rchlkafrjfpbk2fnlvvbh3ai9pyfisci5wxvq";
+  rev  = "d50d46941ee4445ffcc257c41cf60a73f31c367d";
+  hash = "0drg5r3ak3myv7cifikv68d2iyb6h55531r3d4drp2sfmchc9l1v";
   pkgs = builtins.fetchTarball {
-    name = "nixpkgs-2019-01-15";
+    name = "nixpkgs-2020-03-06";
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
     sha256 = hash;
   };
