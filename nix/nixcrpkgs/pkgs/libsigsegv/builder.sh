82632885459cf23b803dca5aa1368e483cd95746
botter <55599302+botter-nidnul@users.noreply.github.com>
2020-03-07 18:09:52 -0600

    hack to get libsigsegv to configure when cross compiling

--- nix/nixcrpkgs/pkgs/libsigsegv/builder.sh
+++ nix/nixcrpkgs/pkgs/libsigsegv/builder.sh
@@ -5,6 +5,13 @@ tar -xf $src
 mkdir build
 cd build
 
+# Hack
+if [ $host = aarch64-linux-musleabi ]
+then
+  sed -i 's/^CFG_FAULT=$/CFG_FAULT=fault-linux-arm.h/' \
+    ../libsigsegv-$version/configure
+fi
+
 ../libsigsegv-$version/configure \
   --host=$host \
   --prefix=$out \
