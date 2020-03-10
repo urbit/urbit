6e21ba593ced223b92cef105218e2f154b7b1b3a
ixv <isaacv@tlon.io>
2020-03-10 12:17:34 -0700

    Merge pull request #2410 from urbit/mp/os1/invite-launch
    launch: subscribe to invite-view, show group invite count in tile

--- pkg/interface/launch/src/js/components/header.js
+++ pkg/interface/launch/src/js/components/header.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
 import { Sigil } from './sigil';
+import _ from 'lodash';
 
 export default class Header extends Component {
   render() {
+
+    let data = _.get(this.props.data, "invites", false);
+    let inviteNum = 0;
+
+    if (data && "/contacts" in data) {
+      inviteNum = Object.keys(data["/contacts"]).length;
+    }
+
+    let numNotificationsElem =
+      inviteNum > 0 ? (
+        <a href="/~groups">
+          <p
+            className="absolute ph1 br2 ba b--gray2 green2 white-d f9 lh-solid"
+            title={"Invitations to new groups"}
+            style={{
+              bottom: "-2",
+              fontWeight: 600,
+              fontSize: "8pt",
+              padding: "0.15rem 0.4rem",
+              right: 64
+            }}>
+            {inviteNum > 99 ? "99+" : inviteNum}
+          </p>
+        </a>
+      ) : (
+        <a href="/~groups">
+          <p
+            className="absolute ph1 br2 ba b--gray2 gray2 white-d f9 lh-solid"
+            title={"No new invitations to new groups"}
+            style={{
+              bottom: "-2",
+              fontWeight: 600,
+              fontSize: "8pt",
+              padding: "0.15rem 0.4rem",
+              right: 64
+            }}>
+            0
+            </p>
+          </a>
+        );
+
     return (
       <header
         className="bg-white bg-gray0-d w-100 justify-between relative tc pt3"
@@ -16,6 +58,7 @@ export default class Header extends Component {
           Home
         </span>
         <div className="absolute right-1 lh-copy" style={{ top: 12 }}>
+          {numNotificationsElem}
           <Sigil
             ship={"~" + window.ship}
             size={16} color={"#000000"}
