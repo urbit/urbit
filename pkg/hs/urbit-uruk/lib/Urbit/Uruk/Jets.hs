3536d90e901ac2daddf9a4752cdf2fee323633c0
Benjamin Summers <benjamin@tlon.io>
2020-03-30 10:13:54 -0700

    urbit-uruk: Finished matching of data jets.

--- pkg/hs/urbit-uruk/lib/Urbit/Uruk/Jets.hs
+++ pkg/hs/urbit-uruk/lib/Urbit/Uruk/Jets.hs
@@ -1,7 +1,7 @@
 {-
    DONE Numberic jets are not matched, and therefore not evaluated in
         jet dashboard. What do?
-   TODO Need to handle data jets too.
+   DONE Need to handle data jets too.
 -}
 
 module Urbit.Uruk.Jets where
@@ -69,9 +69,9 @@ djBody :: DataJet -> Val
 djBody = \case
   NAT 0 -> N S :& N K
   NAT n -> skSucc :& djBody (NAT $ pred n)
-  Sn  n -> iterate ((NB :& (NB :& NS) :& NB) :&) NS !! (fromIntegral n - 1)
-  Bn  n -> iterate ((NB :& NB) :&) NB               !! (fromIntegral n - 1)
-  Cn  n -> iterate ((NB :& (NB :& NC) :& NB) :&) NC !! (fromIntegral n - 1)
+  Sn  n -> iterate (NB :& (NB :& NS) :& NB :&) NS !! (fromIntegral n - 1)
+  Bn  n -> iterate (NB :& NB :&)               NB !! (fromIntegral n - 1)
+  Cn  n -> iterate (NB :& (NB :& NC) :& NB :&) NC !! (fromIntegral n - 1)
 
 jetBody :: Either DataJet SingJet -> Val
 jetBody = either djBody sjBody
@@ -100,23 +100,30 @@ natMatch (NS :& (NS :& (NK :& NS) :& NK) :& n) = succ <$> natMatch n
 natMatch _ = Nothing
 
 snMatch :: Val -> Maybe Pos
-snMatch = error "TODO"
+snMatch NS                            = Just 1
+snMatch (NB :& (NB :& NS) :& NB :& r) = succ <$> snMatch r
+snMatch _                             = Nothing
 
 bnMatch :: Val -> Maybe Pos
-bnMatch = error "TODO"
+bnMatch NB              = Just 1
+bnMatch (NB :& NB :& n) = succ <$> bnMatch n
+bnMatch _               = Nothing
 
 cnMatch :: Val -> Maybe Pos
-cnMatch = error "TODO"
+cnMatch NC                            = Just 1
+cnMatch (NB :& (NB :& NC) :& NB :& n) = succ <$> cnMatch n
+cnMatch _                             = Nothing
+
+pattern SnTag = N (DataJet (NAT 28275))
+pattern BnTag = N (DataJet (NAT 28258))
+pattern CnTag = N (DataJet (NAT 28259))
 
 djMatch :: (Pos, Val, Val) -> Maybe DataJet
 djMatch (2, N K, natMatch -> Just n) = Just (NAT n)
-djMatch (r, N (DataJet (NAT 28275)), snMatch -> Just p) | r + 2 == p =
-  Just (Sn p)
-djMatch (r, N (DataJet (NAT 28258)), bnMatch -> Just p) | r + 2 == p =
-  Just (Bn p)
-djMatch (r, N (DataJet (NAT 28259)), cnMatch -> Just p) | r + 2 == p =
-  Just (Cn p)
-djMatch (_, _, _) = Nothing
+djMatch (r, SnTag, snMatch -> Just p) | r == p + 2 = Just (Sn p)
+djMatch (r, BnTag, bnMatch -> Just p) | r == p + 2 = Just (Bn p)
+djMatch (r, CnTag, cnMatch -> Just p) | r == p + 2 = Just (Cn p)
+djMatch _                            = Nothing
 
 jetMatch :: (Pos, Val, Val) -> Maybe (Either DataJet SingJet)
 jetMatch tup =  (Right <$> sjMatch tup) <|> (Left <$> djMatch tup)
