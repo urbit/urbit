29aeecfc716cfbabcb00c7ce54dc7c3210268dfe
Gavin <gvnatknsn@gmail.com>
2020-04-29 15:48:25 -0700

    webpack with css importer

--- pkg/interface/src/apps/chat/api.js
+++ pkg/interface/src/apps/chat/api.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import _ from 'lodash';
 import { uuid } from '../../lib/util';
 import store from './store';
@@ -60,7 +59,7 @@ class UrbitApi {
 
   action(appl, mark, data) {
     return new Promise((resolve, reject) => {
-      this.channel.poke(ship, appl, mark, data,
+      this.channel.poke(window.ship, appl, mark, data,
         (json) => {
           resolve(json);
         },
--- pkg/interface/src/index.js
+++ pkg/interface/src/index.js
@@ -1,7 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 // import "./fonts/font.css";
-import App from "./App";
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
