9a63ff3d37143807052b63f2deb5c4a80e4990e8
Brendan Hay <brendan.g.hay@gmail.com>
2020-06-08 10:35:32 +0200

    build: updating nixpkgs to 2020-06-06

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,11 +1,10 @@
 let
 
-  rev  = "61c3169a0e17d789c566d5b241bfe309ce4a6275";
-  hash = "0qbycg7wkb71v20rchlkafrjfpbk2fnlvvbh3ai9pyfisci5wxvq";
+  rev  = "0a11634a29c1c9ffe7bfa08fc234fef2ee978dbb";
   pkgs = builtins.fetchTarball {
-    name = "nixpkgs-2019-01-15";
+    name = "nixpkgs-2020-06-06";
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = hash;
+    sha256 = "0vj5k3djn1wlwabzff1kiiy3vs60qzzqgzjbaiwqxacbvlrci10y";
   };
 
 in
