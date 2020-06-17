08b4a475896830a4b57be4ee47ffa156109d0364
Matilde Park <matilde@tlon.io>
2020-06-17 19:13:34 -0400

    publish, links: restore full height

--- pkg/interface/src/apps/links/components/skeleton.js
+++ pkg/interface/src/apps/links/components/skeleton.js
@@ -21,7 +21,7 @@ export class Skeleton extends Component {
       ? props.invites['/link'] : {};
 
     return (
-      <div className={'absolute w-100 ' + popoutWindow}>
+      <div className={'absolute w-100 ' + popoutWindow} style={{ height: 'calc(100% - 45px)' }}>
         <div className={'cf w-100 h-100 flex ' + popoutBorder}>
         <ChannelsSidebar
             active={props.active}
