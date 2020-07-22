8240218cd0ffa648f56e6515d85eae3ea1fd874c
Gavin Atkinson <gvnatknsn@protonmail.com>
2020-07-22 11:01:47 -0700

    omnibox beginnings

--- pkg/interface/src/App.js
+++ pkg/interface/src/App.js
@@ -1,11 +1,13 @@
 import { hot } from 'react-hot-loader/root';
 import 'react-hot-loader';
 import * as React from 'react';
+import {useState} from 'react'
 import { BrowserRouter as Router, Route, withRouter, Switch } from 'react-router-dom';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import './css/indigo-static.css';
 import './css/fonts.css';
 import light from './themes/light';
+import { Box, Text, Row, Icon, Rule } from '@tlon/indigo-react';
 
 import LaunchApp from './apps/launch/app';
 import ChatApp from './apps/chat/app';
@@ -29,6 +31,54 @@ const Root = styled.div`
   padding: 0;
   margin: 0;
 `;
+
+// const CommandResult = ({icon, text, subtext, isSelected}) => {
+//   if (isSelected === false) {
+//     return (
+//       <Row py='2' px='2'>
+//         <Icon icon={icon}  mr='1' />
+//         <Text mr='1'>{text}</Text>
+//         <Text gray>{subtext}</Text>
+//       </Row>
+//     )
+//   }
+//   return (
+//     <Row py='2' px='2' backgroundColor='blue'>
+//       <Icon fill='white' icon={icon} mr='1' />
+//       <Text color='white' mr='1'>{text}</Text>
+//       <Text color='white'>{subtext}</Text>
+//     </Row>
+//   )
+// }
+
+const CommandResult = ({icon, text, subtext, isSelected}) => {
+  const [isHovered, setHover] = useState(isSelected)
+  
+  return (
+    <Row
+      py='2'
+      px='2'
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      backgroundColor={isHovered ? 'blue' : 'white'}
+      >
+        {
+          isHovered
+            ? <>
+                <Icon fill='white' icon={icon} mr='1' />
+                <Text color='white' mr='1'>{text}</Text>
+                <Text color='white'>{subtext}</Text>
+              </>
+            : <>
+                <Icon fill='black' icon={icon} mr='1' />
+                <Text mr='1'>{text}</Text>
+                <Text gray>{subtext}</Text>
+              </>
+        }
+
+    </Row>
+  )
+}
 
 
 const StatusBarWithRouter = withRouter(StatusBar);
@@ -66,12 +116,63 @@ class App extends React.Component {
         <Root>
           <Router>
             <StatusBarWithRouter props={this.props}
-            associations={associations}
-            invites={this.state.invites}
-            api={this.api}
-            connection={this.state.connection}
-            subscription={this.subscription}
-            />
+              associations={associations}
+              invites={this.state.invites}
+              api={this.api}
+              connection={this.state.connection}
+              subscription={this.subscription}
+              />
+            
+
+            <Box backgroundColor='gray' width='100vw' height='100vh' position='absolute' top='0' right='0' zIndex='9'>
+              <Row justifyContent='center'>
+                <Box
+                  borderRadius=''
+                  mt='20vh'
+                  width='600px'
+                  borderRadius='2'
+                  backgroundColor='white'>
+                  <Row py='3' px='2'>
+                    <Text>Input</Text>
+                  </Row>
+                  <Rule/>
+
+                  <CommandResult 
+                    icon='ChevronEast'
+                    text='Join'
+                    subtext='an existing collection, stream, or chat'
+                    isSelected={true}
+                  />
+
+                  <CommandResult 
+                    icon='ChevronEast'
+                    text='Create'
+                    subtext='a new collection, stream, or chat'
+                    isSelected={false}
+                  />
+
+                  <CommandResult 
+                    text='Publish'
+                    subtext=''
+                    isSelected={false}
+                  />
+
+                  <CommandResult 
+                    text='Collections'
+                    subtext=''
+                    isSelected={false}
+                  />
+
+                  <CommandResult 
+                    text='Chat'
+                    subtext=''
+                    isSelected={false}
+                  />
+
+                </Box>
+              </Row>
+            </Box>
+
             <ModuleContext>
             <Switch>
               <Route exact path="/"
