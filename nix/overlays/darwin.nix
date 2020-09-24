eda2f0833353116100719b29d35bcbd780fbc0a2
Brendan Hay <brendan.g.hay@gmail.com>
2020-09-24 14:04:18 +0200

    WIP

--- nix/overlays/darwin.nix
+++ nix/overlays/darwin.nix
@@ -16,7 +16,7 @@ let
 in {
   libiconv = overrideAttrsWhen isDarwin prev.libiconv (old: {
     nativeBuildInputs =
-      optionalsNull old.nativeBuildInputs ++ [
+      optionalsNull (old.nativeBuildInputs or {}) ++ [
         prev.buildPackages.stdenv.cc
       ];
   });
