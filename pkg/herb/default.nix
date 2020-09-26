2e15b58cd9f64b2dbfe2768e9ab8ff1f6160461e
Brendan Hay <brendan.g.hay@gmail.com>
2020-09-26 12:05:55 +0200

    WIP

--- pkg/herb/default.nix
+++ pkg/herb/default.nix
@@ -1,10 +1,19 @@
 { lib, stdenvNoCC, python }:
 
+# We don't want to use the following - as withPackages actually creates
+# a wrapper script to correctly set PYTHONPATH. This script is then set
+# as the shebang for herb - which is not allowed on Darwin.
+# python.withPackages (py: [ py.requests ])) ];
+#
+# Instead, calling wrapPythonPrograms allows cross platform usage.
+
 stdenvNoCC.mkDerivation {
   name  = "herb";
   src = ./herb;
 
-  buildInputs = [ (python.withPackages (py: [ py.requests ])) ];
+  nativeBuildInputs = [ python.pkgs.wrapPython ];
+  buildInputs = [ python python.pkgs.requests ];
+  pythonPath = [ python.pkgs.requests ];
 
   phases = [ "installPhase" "fixupPhase" ];
 
@@ -12,5 +21,9 @@ stdenvNoCC.mkDerivation {
     mkdir -p $out/bin 
     cp $src $out/bin/herb
     chmod +x $out/bin/herb
+  '';
+
+  postFixup = ''
+    wrapPythonPrograms
   '';
 }
