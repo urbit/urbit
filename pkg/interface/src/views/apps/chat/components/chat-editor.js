fc7f8600fbc1c31ea66a6642a478189befe2cee9
Matilde Park <matilde.park@sunshinegardens.org>
2020-10-05 19:12:33 -0400

    chat: resolve conflicts in #3646

--- pkg/interface/src/views/apps/chat/components/chat-editor.js
+++ pkg/interface/src/views/apps/chat/components/chat-editor.js
@@ -136,7 +136,7 @@ export default class ChatEditor extends Component {
           'chat fr h-100 flex bg-gray0-d lh-copy w-100 items-center ' +
           (inCodeMode ? ' code' : '')
         }
-        style={{ flexGrow: 1, paddingBottom: '3px', maxHeight: '224px', width: 'calc(100% - 72px)' }}>
+        style={{ flexGrow: 1, paddingBottom: '3px', maxHeight: '224px', width: 'calc(100% - 88px)' }}>
         <CodeEditor
           value={message}
           options={options}
