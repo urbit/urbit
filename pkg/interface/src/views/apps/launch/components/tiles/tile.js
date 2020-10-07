9d5645f8d7e4eaa4fe281454a1a15c3d4c77e6dd
Matilde Park <matilde@tlon.io>
2020-10-07 00:32:29 -0400

    interface: fix default app tiles linking as anchor

--- pkg/interface/src/views/apps/launch/components/tiles/tile.js
+++ pkg/interface/src/views/apps/launch/components/tiles/tile.js
@@ -10,7 +10,7 @@ import { Box } from "@tlon/indigo-react";
 export default class Tile extends React.Component {
   render() {
     const { bg, to, p, ...props } = this.props;
-    
+
     let childElement = (
       <Box p={typeof p === 'undefined' ? 2 : p} bg={bg} width="100%" height="100%">
         {props.children}
@@ -18,12 +18,12 @@ export default class Tile extends React.Component {
     );
 
     if (to) {
-      if (routeList.indexOf(to) === -1) {
+      if (routeList.indexOf(to) !== -1 || to === '/~landscape/home' || to === '/~profile') {
         childElement= (<Link to={to}>{childElement}</Link>);
       } else {
         childElement= (<a href={to}>{childElement}</a>);
       }
-      
+
     }
 
     return (
