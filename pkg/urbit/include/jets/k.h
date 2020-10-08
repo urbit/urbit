074b0cc0b0210a28d1848254e72176c127421754
Joe Bryan <joemfb@gmail.com>
2020-10-08 14:20:30 -0700

    u3: cleans up testing protocol, enables gc in mug tests

--- pkg/urbit/include/jets/k.h
+++ pkg/urbit/include/jets/k.h
@@ -5,6 +5,7 @@
   /** Tier 1.
   **/
     u3_noun u3ka_add(u3_noun a, u3_noun b);
+    u3_noun u3ka_dec(u3_atom a);
     u3_noun u3ka_sub(u3_noun a, u3_noun b);
     u3_noun u3ka_mul(u3_noun a, u3_noun b);
     u3_noun u3ka_gth(u3_noun a, u3_noun b);
