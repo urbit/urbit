48667bf5d59e314fb35345402ea7c9dadac66374
timlucmiptev <info@timlucmiptev.space>
2020-11-26 11:38:30 +0200

    zuse installer

---
+++ install-zuse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+usage() { printf "Usage: $0 URBIT_PIER_DIRECTORY \n" 1>&2; exit 1; }
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 2
+fi
+PIER=$1
+
+cp sys/zuse.hoon $PIER/
+echo "zuse copied"
