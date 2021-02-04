f508cd923eb0ce1933d5f5e21488b8d521ecfdf2
Liam Fitzgerald <liam.fitzgerald@tlon.io>
2021-02-05 09:07:28 +1000

    interface: truncate group title

--- pkg/interface/src/views/landscape/components/GroupSwitcher.tsx
+++ pkg/interface/src/views/landscape/components/GroupSwitcher.tsx
@@ -167,11 +167,9 @@ export function GroupSwitcher(props: {
             }
           >
             <Row flexGrow={1} alignItems="center" width='100%' minWidth='0' flexShrink={0}>
-              { metadata && <MetadataIcon mr="2" border="1" borderColor="lightGray" borderRadius="1" metadata={metadata} height="24px" width="24px" /> }
-              <Row justifyContent="space-between" mr={1} flexShrink={0} flexGrow={1} minWidth='0'>
-                <Text lineHeight="1.1" fontSize='2' fontWeight="700" overflow='hidden' display='inline-block' flexShrink='1' style={{ textOverflow: 'ellipsis', whiteSpace: 'pre' }}>{title}</Text>
+              { metadata && <MetadataIcon flexShrink={0} mr="2" border="1" borderColor="lightGray" borderRadius="1" metadata={metadata} height="24px" width="24px" /> }
+              <Text flexShrink={1} lineHeight="1.1" fontSize='2' fontWeight="700" overflow='hidden' display='inline-block' flexShrink='1' style={{ textOverflow: 'ellipsis', whiteSpace: 'pre' }}>{title}</Text>
               </Row>
-            </Row>
           </Dropdown>
           <Row pr='3' verticalAlign="middle">
             {(workspace.type === 'group') && (
