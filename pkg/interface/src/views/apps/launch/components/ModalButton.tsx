c272cdfff86ebe2fde6984e01a49325cf481e6da
James Acklin <james.acklin@gmail.com>
2021-02-15 16:00:22 -0500

    landscape: sets ModalButton label to nowrap
    Fixes urbit/landscape#435

--- pkg/interface/src/views/apps/launch/components/ModalButton.tsx
+++ pkg/interface/src/views/apps/launch/components/ModalButton.tsx
@@ -1,38 +1,33 @@
-import React  from "react"
-import { Box, Button, Icon, Text } from "@tlon/indigo-react"
-import {useModal} from "~/logic/lib/useModal";
+import React from 'react';
+import { Box, Button, Icon, Text } from '@tlon/indigo-react';
+import { useModal } from '~/logic/lib/useModal';
 
 const ModalButton = (props) => {
-  const {
-    children,
-    icon,
-    text,
-    bg,
-    color,
-    ...rest
-  } = props;
+  const { children, icon, text, bg, color, ...rest } = props;
   const { modal, showModal } = useModal({ modal: props.children });
-
 
   return (
     <>
       {modal}
       <Button
         onClick={showModal}
-        display="flex"
-        alignItems="center"
-        cursor="pointer"
+        display='flex'
+        alignItems='center'
+        cursor='pointer'
         bg={bg}
         p={2}
         borderRadius={2}
-        boxShadow="0 0 0px 1px inset"
-        color="scales.black20"
+        boxShadow='0 0 0px 1px inset'
+        color='scales.black20'
         {...rest}
       >
-        <Icon icon={props.icon} mr={2} color={color}></Icon><Text color={color}>{props.text}</Text>
+        <Icon icon={props.icon} mr={2} color={color}></Icon>
+        <Text color={color} className='nowrap'>
+          {props.text}
+        </Text>
       </Button>
     </>
   );
-}
+};
 
 export default ModalButton;
