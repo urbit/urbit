72a0588a52061798792a36105adb28a04314a26e
Liam Fitzgerald <liam.fitzgerald@tlon.io>
2021-02-19 13:21:56 +1000

    tutorial: update mainnet location

--- pkg/interface/config/webpack.dev.js
+++ pkg/interface/config/webpack.dev.js
@@ -120,11 +120,11 @@ module.exports = {
   plugins: [
     new UrbitShipPlugin(urbitrc),
     new webpack.DefinePlugin({
-      'process.env.TUTORIAL_HOST': JSON.stringify('~hastuc-dibtux'),
+      'process.env.TUTORIAL_HOST': JSON.stringify('~difmex-passed'),
       'process.env.TUTORIAL_GROUP': JSON.stringify('beginner-island'),
-      'process.env.TUTORIAL_CHAT': JSON.stringify('chat-1704'),
-      'process.env.TUTORIAL_BOOK': JSON.stringify('book-9695'),
-      'process.env.TUTORIAL_LINKS': JSON.stringify('link-2827'),
+      'process.env.TUTORIAL_CHAT': JSON.stringify('introduce-yourself-7010'),
+      'process.env.TUTORIAL_BOOK': JSON.stringify('guides-9684'),
+      'process.env.TUTORIAL_LINKS': JSON.stringify('community-articles-2143'),
     })
 
     // new CleanWebpackPlugin(),
