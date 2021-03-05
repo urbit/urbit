45b2ea4096699279a76f65cc4767e6e11ba2dd44
Logan Allen <loganallc@gmail.com>
2021-03-05 15:21:41 -0600

    interface + -group-create thread: group homepage asks if you want to enable feed

--- pkg/interface/src/views/landscape/components/Home/GroupHome.js
+++ pkg/interface/src/views/landscape/components/Home/GroupHome.js
@@ -3,10 +3,14 @@ import { Col } from '@tlon/indigo-react';
 
 import { EmptyGroupHome } from './EmptyGroupHome';
 import { GroupFeed } from './GroupFeed';
+import { AddFeedBanner } from './AddFeedBanner';
 
 
 export function GroupHome(props) {
   //  TODO: store a backend config for whether the feed is enabled or not
+  console.log(props);
+  const metadata = props.associations?.groups[props.groupPath]?.metadata;
+  const askFeedBanner = metadata && metadata.module === null;
   const isFeedEnabled = false;
 
   return (
@@ -16,6 +20,13 @@ export function GroupHome(props) {
       display="flex"
       p='4'
     >
+      { askFeedBanner ? (
+        <AddFeedBanner 
+          api={props.api}
+          groupPath={props.groupPath}
+          group={props.groups[props.groupPath]}
+        /> 
+      ) : null }
       { isFeedEnabled ? (
         <GroupFeed {...props} />
       ) : (
