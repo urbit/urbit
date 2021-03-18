c5b82acffbaf6dc06470a7778a5c1de8c76b20bd
mirtyl-wacdec <ycl2@protonmail.com>
2021-03-18 20:40:59 +0700

    landscape: transliterate non-latin characters into ascii

--- pkg/interface/src/logic/lib/util.ts
+++ pkg/interface/src/logic/lib/util.ts
@@ -4,6 +4,7 @@ import f, { memoize } from "lodash/fp";
 import bigInt, { BigInteger } from "big-integer";
 import { Contact } from '~/types';
 import useSettingsState from '../state/settings';
+import anyAscii from 'any-ascii';
 
 export const MOBILE_BROWSER_REGEX = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i;
 
@@ -17,7 +18,7 @@ export const MOMENT_CALENDAR_DATE = {
 };
 
 export const getModuleIcon = (mod: string) => {
- if (mod === 'link') {
+  if (mod === 'link') {
     return 'Collection';
   }
   return _.capitalize(mod);
@@ -50,7 +51,7 @@ export function daToUnix(da: BigInteger) {
 }
 
 export function unixToDa(unix: number) {
-  const timeSinceEpoch =  bigInt(unix).multiply(DA_SECOND).divide(bigInt(1000));
+  const timeSinceEpoch = bigInt(unix).multiply(DA_SECOND).divide(bigInt(1000));
   return DA_UNIX_EPOCH.add(timeSinceEpoch);
 }
 
@@ -300,7 +301,7 @@ export function stringToTa(str: string) {
 
 export function amOwnerOfGroup(groupPath: string) {
   if (!groupPath)
-return false;
+    return false;
   const groupOwner = /(\/~)?\/~([a-z-]{3,})\/.*/.exec(groupPath)?.[2];
   return window.ship === groupOwner;
 }
@@ -319,11 +320,13 @@ export function getContactDetails(contact: any) {
 }
 
 export function stringToSymbol(str: string) {
+  const ascii = anyAscii(str);
+  console.log(ascii);
   let result = '';
-  for (let i = 0; i < str.length; i++) {
-    const n = str.charCodeAt(i);
+  for (let i = 0; i < ascii.length; i++) {
+    const n = ascii.charCodeAt(i);
     if ((n >= 97 && n <= 122) || (n >= 48 && n <= 57)) {
-      result += str[i];
+      result += ascii[i];
     } else if (n >= 65 && n <= 90) {
       result += String.fromCharCode(n + 32);
     } else {
@@ -337,7 +340,6 @@ export function stringToSymbol(str: string) {
   }
   return result;
 }
-
 /**
  * Formats a numbers as a `@ud` inserting dot where needed
  */
@@ -355,7 +357,7 @@ export function numToUd(num: number) {
 export function usePreventWindowUnload(shouldPreventDefault: boolean, message = 'You have unsaved changes. Are you sure you want to exit?') {
   useEffect(() => {
     if (!shouldPreventDefault)
-return;
+      return;
     const handleBeforeUnload = (event) => {
       event.preventDefault();
       return message;
@@ -371,7 +373,7 @@ return;
 }
 
 export function pluralize(text: string, isPlural = false, vowel = false) {
-  return isPlural ? `${text}s`: `${vowel ? 'an' : 'a'} ${text}`;
+  return isPlural ? `${text}s` : `${vowel ? 'an' : 'a'} ${text}`;
 }
 
 // Hide is an optional second parameter for when this function is used in class components
@@ -399,9 +401,9 @@ export const useHovering = (): useHoveringInterface => {
 
 const DM_REGEX = /ship\/~([a-z]|-)*\/dm--/;
 export function getItemTitle(association: Association) {
-  if(DM_REGEX.test(association.resource)) {
-    const [,,ship,name] = association.resource.split('/');
-    if(ship.slice(1) === window.ship) {
+  if (DM_REGEX.test(association.resource)) {
+    const [, , ship, name] = association.resource.split('/');
+    if (ship.slice(1) === window.ship) {
       return cite(`~${name.slice(4)}`);
     }
     return cite(ship);
