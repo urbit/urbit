bf38e79520e3cf27e76e59267fffca689f954740
~locpyl-tidnyd <locpyl-tidnyd@protonmail.com>
2021-03-27 20:12:27 +0000

    u3: centralize use of u3_Signal

--- pkg/urbit/noun/manage.c
+++ pkg/urbit/noun/manage.c
@@ -126,7 +126,7 @@ static void _cm_overflow(void *arg1, void *arg2, void *arg3)
   (void)(arg1);
   (void)(arg2);
   (void)(arg3);
-  siglongjmp(u3_Signal, c3__over);
+  u3m_signal(c3__over);
 }
 
 /* _cm_signal_handle(): handle a signal in general.
@@ -138,7 +138,7 @@ _cm_signal_handle(c3_l sig_l)
     sigsegv_leave_handler(_cm_overflow, NULL, NULL, NULL);
   }
   else {
-    siglongjmp(u3_Signal, sig_l);
+    u3m_signal(sig_l);
   }
 }
 
