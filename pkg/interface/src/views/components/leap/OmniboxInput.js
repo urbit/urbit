b42d54349f0896b346ebc65c501f43fcb93791e1
James Acklin <james.acklin@gmail.com>
2021-04-11 17:26:35 -0400

    leap: prettier js/tsx
    fixes urbit/landscape#598

--- pkg/interface/src/views/components/leap/OmniboxInput.js
+++ pkg/interface/src/views/components/leap/OmniboxInput.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { BaseInput } from '@tlon/indigo-react';
 
@@ -6,33 +5,31 @@ export class OmniboxInput extends Component {
   render() {
     const { props } = this;
     return (
-    <BaseInput
-      ref={(el) => {
-        this.input = el;
+      <BaseInput
+        ref={(el) => {
+          this.input = el;
           if (el && document.activeElement.isSameNode(el)) {
             el.blur();
             el.focus();
           }
-        }
-      }
-      width='100%'
-      p='2'
-      backgroundColor='white'
-      color='black'
-      border='1px solid transparent'
-      borderRadius='2'
-      maxWidth='calc(600px - 1.15rem)'
-      fontSize='1'
-      style={{ boxSizing: 'border-box' }}
-      placeholder='Search...'
-      onKeyDown={props.control}
-      onChange={props.search}
-      spellCheck={false}
-      value={props.query}
-    />
+        }}
+        width='100%'
+        p='2'
+        backgroundColor='white'
+        color='black'
+        border='1px solid transparent'
+        borderRadius='2'
+        maxWidth='calc(600px - 1.15rem)'
+        fontSize='1'
+        style={{ boxSizing: 'border-box' }}
+        placeholder='Search...'
+        onKeyDown={props.control}
+        onChange={props.search}
+        spellCheck={false}
+        value={props.query}
+      />
     );
   }
 }
 
 export default OmniboxInput;
-
