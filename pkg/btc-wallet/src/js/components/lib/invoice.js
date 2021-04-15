2391185916dc981d49fb07fef886c34b04bd5e56
pkova <pyry.kovanen@gmail.com>
2021-04-15 15:38:51 +0300

    btc: add final screen for sending flow

--- pkg/btc-wallet/src/js/components/lib/invoice.js
+++ pkg/btc-wallet/src/js/components/lib/invoice.js
@@ -13,6 +13,8 @@ import * as bitcoin from 'bitcoinjs-lib';
 import * as kg from 'urbit-key-generation';
 import * as bip39 from 'bip39';
 
+import Sent from './sent.js'
+
 window.bitcoin = bitcoin;
 window.kg = kg;
 window.bip39 = bip39;
@@ -24,6 +26,7 @@ export default class Invoice extends Component {
     this.state = {
       masterTicket: '',
       ready: false,
+      sent: false,
     };
 
     this.checkTicket = this.checkTicket.bind(this);
@@ -35,7 +38,7 @@ export default class Invoice extends Component {
     let command = {
       'broadcast-tx': psbtHex
     }
-    this.props.api.btcWalletCommand(command);
+    return this.props.api.btcWalletCommand(command)
   }
 
   sendBitcoin(ticket, psbt) {
@@ -59,7 +62,7 @@ export default class Invoice extends Component {
              .toHex();
 
     console.log({hex});
-    this.broadCastTx(hex);
+    this.broadCastTx(hex).then(res => this.setState({sent: true}));
   }
 
   checkTicket(e){
@@ -71,85 +74,102 @@ export default class Invoice extends Component {
 
   render() {
     const { stopSending, payee, denomination, denomAmount, satsAmount } = this.props;
+    const { sent } = this.state;
     return (
       <>
-        <Row
-          justifyContent='space-between'
-          alignItems='center'
-        >
-          <Text bold fontSize={1}>Invoice</Text>
-          <Icon
-            icon='X'
-            cursor='pointer'
-            onClick={() => stopSending()}
-          />
-        </Row>
-        <Box
-          mt={4}
-          backgroundColor='rgba(0, 159, 101, 0.05)'
-          borderRadius='12px'
-        >
-          <Box
-            padding={4}
+        { sent ?
+          <Sent
+            payee={payee}
+            denomination={denomination}
+            denomAmount={denomAmount}
+            satsAmount={satsAmount}
+          /> :
+          <Col
+            height='400px'
+            width='100%'
+            backgroundColor='white'
+            borderRadius='32px'
+            mb={5}
+            p={5}
           >
-            <Row>
-              <Text fontWeight='500'>You are sending</Text>
-            </Row>
             <Row
-              mt={2}
+              justifyContent='space-between'
+              alignItems='center'
             >
-              <Text
-                color='green'
-              >{`$${denomAmount} ${denomination}`}</Text>
-              <Text
-                ml={2}
-                color='gray'
-              >{`${satsAmount} sats`}</Text>
+              <Text bold fontSize={1}>Invoice</Text>
+              <Icon
+                icon='X'
+                cursor='pointer'
+                onClick={() => stopSending()}
+              />
             </Row>
+            <Box
+              mt={4}
+              backgroundColor='rgba(0, 159, 101, 0.05)'
+              borderRadius='12px'
+            >
+              <Box
+                padding={4}
+              >
+                <Row>
+                  <Text fontWeight='500'>You are sending</Text>
+                </Row>
+                <Row
+                  mt={2}
+                >
+                  <Text
+                    color='green'
+                  >{`$${denomAmount} ${denomination}`}</Text>
+                  <Text
+                    ml={2}
+                    color='gray'
+                  >{`${satsAmount} sats`}</Text>
+                </Row>
+                <Row
+                  mt={2}
+                >
+                  <Text>To:</Text>
+                  <Text
+                    ml={2}
+                  >{payee}</Text>
+                </Row>
+              </Box>
+            </Box>
+            <Box mt={3} mb={2}>
+              <Text fontSize='14px' fontWeight='500'>
+                Master Key
+              </Text>
+            </Box>
+            <Input
+              value={this.state.masterTicket}
+              fontSize="14px"
+              type="password"
+              name="masterTicket"
+              obscure={value => value.replace(/[^~-]+/g, '••••••')}
+              placeholder="••••••-••••••-••••••-••••••"
+              autoCapitalize="none"
+              autoCorrect="off"
+              onChange={this.checkTicket}
+            />
             <Row
-              mt={2}
+              flexDirection='row-reverse'
+              mt={4}
             >
-              <Text>To:</Text>
-              <Text
-                ml={2}
-              >{payee}</Text>
+              <Button
+                primary
+                children='Send BTC'
+                mr={3}
+                fontSize={1}
+                borderRadius='24px'
+                py='24px'
+                px='24px'
+                onClick={() => this.sendBitcoin(this.state.masterTicket, store.state.psbt)}
+                disabled={!this.state.ready}
+                style={{cursor: this.state.ready ? "pointer" : "default"}}
+              />
             </Row>
-          </Box>
-        </Box>
-        <Box mt={3} mb={2}>
-          <Text fontSize='14px' fontWeight='500'>
-            Master Key
-          </Text>
-        </Box>
-        <Input
-          value={this.state.masterTicket}
-          fontSize="14px"
-          type="password"
-          name="masterTicket"
-          obscure={value => value.replace(/[^~-]+/g, '••••••')}
-          placeholder="••••••-••••••-••••••-••••••"
-          autoCapitalize="none"
-          autoCorrect="off"
-          onChange={this.checkTicket}
-        />
-        <Row
-          flexDirection='row-reverse'
-          mt={4}
-        >
-          <Button
-            primary
-            children='Send BTC'
-            mr={3}
-            fontSize={1}
-            borderRadius='24px'
-            py='24px'
-            px='24px'
-            onClick={() => this.sendBitcoin(this.state.masterTicket, store.state.psbt)}
-            disabled={!this.state.ready}
-            style={{cursor: this.state.ready ? "pointer" : "default"}}
-          />
-        </Row>
-
+          </Col>
+        }
       </>
     );
   }
