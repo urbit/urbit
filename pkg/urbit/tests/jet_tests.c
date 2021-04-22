1ca0207b208a6b197df628fe6a6abc4cd8b46683
Joe Bryan <joemfb@gmail.com>
2021-04-22 15:42:03 -0700

    u3: adds basic tests for u3s_sift_ud()

--- pkg/urbit/tests/jet_tests.c
+++ pkg/urbit/tests/jet_tests.c
@@ -9,6 +9,107 @@ _setup(void)
   u3m_pave(c3y);
 }
 
+static inline c3_i
+_ud_good(c3_w num_w, const c3_c* num_c)
+{
+  u3_weak out;
+  if ( num_w != (out = u3s_sift_ud_bytes(strlen(num_c), (c3_y*)num_c)) ) {
+    if ( u3_none == out ) {
+      fprintf(stderr, "sift_ud: %s fail; expected %u\r\n", num_c, num_w);
+    }
+    else {
+      fprintf(stderr, "sift_ud: %s wrong; expected %u: actual %u\r\n", num_c, num_w, out);
+    }
+    return 0;
+  }
+
+  return 1;
+}
+
+static inline c3_i
+_ud_fail(const c3_c* num_c)
+{
+  u3_weak out;
+  if ( u3_none != (out = u3s_sift_ud_bytes(strlen(num_c), (c3_y*)num_c)) ) {
+    u3m_p("out", out);
+    fprintf(stderr, "sift_ud: %s expected fail\r\n", num_c);
+    return 0;
+  }
+
+  return 1;
+}
+
+static c3_i
+_test_sift_ud(void)
+{
+  c3_i ret_i = 1;
+
+  ret_i &= _ud_good(0, "0");
+  ret_i &= _ud_good(1, "1");
+  ret_i &= _ud_good(12, "12");
+  ret_i &= _ud_good(123, "123");
+  ret_i &= _ud_good(1234, "1.234");
+  ret_i &= _ud_good(12345, "12.345");
+  ret_i &= _ud_good(123456, "123.456");
+  ret_i &= _ud_good(1234567, "1.234.567");
+  ret_i &= _ud_good(12345678, "12.345.678");
+  ret_i &= _ud_good(123456789, "123.456.789");
+  ret_i &= _ud_good(100000000, "100.000.000");
+  ret_i &= _ud_good(101101101, "101.101.101");
+  ret_i &= _ud_good(201201201, "201.201.201");
+  ret_i &= _ud_good(302201100, "302.201.100");
+
+  ret_i &= _ud_fail("01");
+  ret_i &= _ud_fail("02");
+  ret_i &= _ud_fail("003");
+  ret_i &= _ud_fail("1234");
+  ret_i &= _ud_fail("1234.5");
+  ret_i &= _ud_fail("1234.567.8");
+  ret_i &= _ud_fail("1234.56..78.");
+  ret_i &= _ud_fail("123.45a");
+
+  {
+    c3_c* num_c = "4.294.967.296";
+    u3_weak out = u3s_sift_ud_bytes(strlen(num_c), (c3_y*)num_c);
+    u3_atom pro = u3qc_bex(32);
+
+    if ( u3_none == out ) {
+      fprintf(stderr, "sift_ud: (bex 32) fail\r\n");
+      ret_i = 0;
+    }
+
+    if ( c3n == u3r_sing(pro, out) ) {
+      u3m_p("out", out);
+      fprintf(stderr, "sift_ud: (bex 32) wrong\r\n");
+      ret_i = 0;
+    }
+
+    u3z(out); u3z(pro);
+  }
+
+
+  {
+    c3_c* num_c = "340.282.366.920.938.463.463.374.607.431.768.211.456";
+    u3_weak out = u3s_sift_ud_bytes(strlen(num_c), (c3_y*)num_c);
+    u3_atom pro = u3qc_bex(128);
+
+    if ( u3_none == out ) {
+      fprintf(stderr, "sift_ud: (bex 128) fail\r\n");
+      ret_i = 0;
+    }
+
+    if ( c3n == u3r_sing(pro, out) ) {
+      u3m_p("out", out);
+      fprintf(stderr, "sift_ud: (bex 128) wrong\r\n");
+      ret_i = 0;
+    }
+
+    u3z(out); u3z(pro);
+  }
+
+  return ret_i;
+}
+
 static c3_i
 _test_en_base16(void)
 {
@@ -399,6 +500,11 @@ static c3_i
 _test_jets(void)
 {
   c3_i ret_i = 1;
+
+  if ( !_test_sift_ud() ) {
+    fprintf(stderr, "test jets: sift_ud: failed\r\n");
+    ret_i = 0;
+  }
 
   if ( !_test_base16() ) {
     fprintf(stderr, "test jets: base16: failed\r\n");
