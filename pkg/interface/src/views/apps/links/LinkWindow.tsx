80cf0bef4c52c8091935f6412efcc56ab8912691
James Acklin <james.acklin@gmail.com>
2021-05-10 18:42:07 -0400

    links: remove stray semicolon

--- pkg/interface/src/views/apps/links/LinkWindow.tsx
+++ pkg/interface/src/views/apps/links/LinkWindow.tsx
@@ -82,7 +82,7 @@ class LinkWindow extends Component<LinkWindowProps, {}> {
     }
     return (
       <Box ref={ref}>
-        <LinkItem key={index.toString()} {...linkProps} />;
+        <LinkItem key={index.toString()} {...linkProps} />
       </Box>
     );
   });
