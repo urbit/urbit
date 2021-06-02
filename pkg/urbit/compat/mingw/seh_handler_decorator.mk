39032679ff124cb23719cc3a9911dbeb82654f49
~locpyl-tidnyd <locpyl-tidnyd@protonmail.com>
2021-06-03 02:35:13 +0300

    vere: mingw: fix seh_handler_decorator.mk

--- pkg/urbit/compat/mingw/seh_handler_decorator.mk
+++ pkg/urbit/compat/mingw/seh_handler_decorator.mk
@@ -1,6 +1,7 @@
-sehdexe := build/seh_handler_decorator.exe
+sehdexe := build/seh_handler_decorator
 CCDEPS  := $(CCDEPS) $(sehdexe)
 CCEXTRA  = -E -o -|$(sehdexe) $<|$(CC) $(CFLAGS) -x cpp-output -
 
 $(sehdexe): compat/mingw/seh_handler_decorator.cc
+	@mkdir -p ./build
 	@$(CC) $< -o $@
