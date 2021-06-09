393b72ab000c31db77b05e9a2ea5e177f8b5c5ab
ryjm <jraydermiller@gmail.com>
2021-06-09 04:58:16 -0400

    king: don't kill on C-d

--- shell.nix
+++ shell.nix
@@ -56,6 +56,7 @@ in pkgsLocal.hs.shellFor {
     pkgs.nixfmt
     pkgs.shfmt
     pkgs.stack
+    pkgs.haskell-language-server
     (import pkgs.sources.niv { }).niv
   ] ++ mergeFrom "buildInputs";
 
