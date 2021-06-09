5071b3996dceb8fccf7b96e9aa78c1dd5d875f1f
Liam Fitzgerald <liam.fitzgerald@tlon.io>
2021-06-09 14:04:41 +1000

    interface: move s3 subs to airlock

--- pkg/interface/src/logic/state/storage.ts
+++ pkg/interface/src/logic/state/storage.ts
@@ -1,34 +1,49 @@
-import { BaseState, createState } from './base';
+import { reduce } from '../reducers/s3-update';
+import _ from 'lodash';
+import { createState, createSubscription, reduceStateN } from './base';
 
 export interface GcpToken {
   accessKey: string;
   expiresIn: number;
 }
 
-export interface StorageState extends BaseState<StorageState> {
+export interface StorageState {
   gcp: {
     configured?: boolean;
     token?: GcpToken;
-  },
+  };
   s3: {
     configuration: {
       buckets: Set<string>;
       currentBucket: string;
     };
     credentials: any | null; // TODO better type
-  }
+  };
 }
 
 // @ts-ignore investigate zustand types
-const useStorageState = createState<StorageState>('Storage', {
-  gcp: {},
-  s3: {
-    configuration: {
-      buckets: new Set(),
-      currentBucket: ''
-    },
-    credentials: null
-  }
-}, ['s3']);
+const useStorageState = createState<StorageState>(
+  'Storage',
+  {
+    gcp: {},
+    s3: {
+      configuration: {
+        buckets: new Set(),
+        currentBucket: ''
+      },
+      credentials: null
+    }
+  },
+  ['s3', 'gcp'],
+  [
+    (set, get) =>
+      createSubscription('s3-store', '/all', (e) => {
+        const d = _.get(e, 's3-update', false);
+        if (d) {
+          reduceStateN(get(), d, reduce);
+        }
+      })
+  ]
+);
 
 export default useStorageState;
