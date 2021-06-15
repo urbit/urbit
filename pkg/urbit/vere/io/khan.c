0d366011215daa33bad88b5ac2d77dd8a291ae4d
Jōshin <git@wholezero.org>
2021-06-15 22:47:22 +0000

    khan: open socket on %born completion

--- pkg/urbit/vere/io/khan.c
+++ pkg/urbit/vere/io/khan.c
@@ -21,65 +21,6 @@
 
 static const c3_c URB_SOCK_PATH[] = ".urb/khan.sock";
 
-/* _khan_io_talk(): notify %khan that we're live
-*/
-static void
-_khan_io_talk(u3_auto* car_u)
-{
-  u3_khan* cop_u = (u3_khan*)car_u;
-
-  u3_noun wir = u3nt(c3__khan,
-                     u3dc("scot", c3__uv, cop_u->sev_l),
-                     u3_nul);
-  u3_noun cad = u3nc(c3__born, u3_nul);
-
-  u3_auto_plan(car_u, u3_ovum_init(0, c3__k, wir, cad));
-}
-
-/* _khan_io_kick(): apply effects.
-*/
-static c3_o
-_khan_io_kick(u3_auto* car_u, u3_noun wir, u3_noun cad)
-{
-  u3_khan* cop_u = (u3_khan*)car_u;
-
-  u3_noun tag, dat, i_wir;
-  c3_o ret_o;
-
-  if (  (c3n == u3r_cell(wir, &i_wir, 0))
-     || (c3n == u3r_cell(cad, &tag, &dat))
-     || (c3__khan != i_wir) )
-  {
-    ret_o = c3n;
-  }
-  else {
-    ret_o = c3y;
-    // TODO do something
-  }
-
-  u3z(wir); u3z(cad);
-  return ret_o;
-}
-
-/* _khan_io_exit(): unlink socket.
-*/
-static void
-_khan_io_exit(u3_auto* car_u)
-{
-  u3_khan* cop_u = (u3_khan*)car_u;
-  c3_c*    pax_c = u3_Host.dir_c;
-  c3_w     len_w = strlen(pax_c) + 1 + sizeof(URB_SOCK_PATH);
-  c3_c*    paf_c = c3_malloc(len_w);
-  c3_i     wit_i;
-
-  wit_i = snprintf(paf_c, len_w, "%s/%s", pax_c, URB_SOCK_PATH);
-  c3_assert(wit_i > 0);
-  c3_assert(len_w == (c3_w)wit_i + 1);
-
-  unlink(paf_c);
-  c3_free(paf_c);
-}
-
 /* _khan_conn_cb(): socket connection callback.
 */
 static void
@@ -126,6 +67,119 @@ _khan_conn_cb(uv_stream_t* sem_u, c3_i tas_i)
   // socket, keeping one in text mode, and sending a 0x80 over the other.
 }
 
+/* _khan_born_news(): initialization complete, open socket.
+*/
+static void
+_khan_born_news(u3_ovum* egg_u, u3_ovum_news new_e)
+{
+  u3_auto* car_u = egg_u->car_u;
+  u3_khan* cop_u = (u3_khan*)car_u;
+
+  if ( u3_ovum_done == new_e ) {
+    car_u->liv_o = c3y;
+
+    // Open socket. The full socket path is limited to about 108 characters, and
+    // we want it to be relative to the pier. So we save our current path, chdir
+    // to the pier, open the socket at the desired path, then chdir back.
+    // Hopefully there aren't any threads.
+    {
+      c3_c pax_c[2048];
+
+      // XX needs better error handling
+      if ( NULL == getcwd(pax_c, sizeof(pax_c)) ) {
+        c3_assert(!"khan-getcwd");
+      }
+      else {
+        if ( 0 != chdir(u3_Host.dir_c) ) {
+          c3_assert(!"khan-chdir");
+        }
+        else {
+          // TODO handle errors
+          unlink(URB_SOCK_PATH);
+          uv_pipe_init(u3L, &cop_u->pyp_u, 0);
+          uv_pipe_bind(&cop_u->pyp_u, URB_SOCK_PATH);
+          uv_listen((uv_stream_t*)&cop_u->pyp_u, 0, _khan_conn_cb);
+
+          if ( 0 != chdir(pax_c) ) {
+            c3_assert(!"khan-back");
+          }
+        }
+      }
+    }
+  }
+}
+
+/* _khan_born_bail(): nonessential failure; log it and keep going.
+*/
+static void
+_khan_born_bail(u3_ovum* egg_u, u3_noun lud)
+{
+  u3l_log("khan: %%born failure; socket not opened\n");
+}
+
+/* _khan_io_talk(): notify %khan that we're live
+*/
+static void
+_khan_io_talk(u3_auto* car_u)
+{
+  u3_khan* cop_u = (u3_khan*)car_u;
+
+  u3_noun wir = u3nt(c3__khan,
+                     u3dc("scot", c3__uv, cop_u->sev_l),
+                     u3_nul);
+  u3_noun cad = u3nc(c3__born, u3_nul);
+
+  u3_auto_peer(
+    u3_auto_plan(car_u, u3_ovum_init(0, c3__k, wir, cad)),
+    0,
+    _khan_born_news,
+    _khan_born_bail);
+}
+
+/* _khan_io_kick(): apply effects.
+*/
+static c3_o
+_khan_io_kick(u3_auto* car_u, u3_noun wir, u3_noun cad)
+{
+  u3_khan* cop_u = (u3_khan*)car_u;
+
+  u3_noun tag, dat, i_wir;
+  c3_o ret_o;
+
+  if (  (c3n == u3r_cell(wir, &i_wir, 0))
+     || (c3n == u3r_cell(cad, &tag, &dat))
+     || (c3__khan != i_wir) )
+  {
+    ret_o = c3n;
+  }
+  else {
+    ret_o = c3y;
+    // TODO do something
+  }
+
+  u3z(wir); u3z(cad);
+  return ret_o;
+}
+
+/* _khan_io_exit(): unlink socket.
+*/
+static void
+_khan_io_exit(u3_auto* car_u)
+{
+  u3_khan* cop_u = (u3_khan*)car_u;
+  c3_c*    pax_c = u3_Host.dir_c;
+  c3_w     len_w = strlen(pax_c) + 1 + sizeof(URB_SOCK_PATH);
+  c3_c*    paf_c = c3_malloc(len_w);
+  c3_i     wit_i;
+
+  wit_i = snprintf(paf_c, len_w, "%s/%s", pax_c, URB_SOCK_PATH);
+  c3_assert(wit_i > 0);
+  c3_assert(len_w == (c3_w)wit_i + 1);
+
+  unlink(paf_c);
+  c3_free(paf_c);
+}
+
 /* u3_khan(): initialize control plane socket.
 */
 u3_auto*
@@ -147,35 +201,6 @@ u3_khan_io_init(u3_pier* pir_u)
     now = u3_time_in_tv(&tim_u);
     cop_u->sev_l = u3r_mug(now);
     u3z(now);
-  }
-
-  // Open socket. The full socket path is limited to about 108 characters, and
-  // we want it to be relative to the pier. So we save our current path, chdir
-  // to the pier, open the socket at the desired path, then chdir back.
-  // Hopefully there aren't any threads.
-  {
-    c3_c pax_c[2048];
-
-    // XX needs better error handling
-    if ( NULL == getcwd(pax_c, sizeof(pax_c)) ) {
-      c3_assert(!"khan-getcwd");
-    }
-    else {
-      if ( 0 != chdir(u3_Host.dir_c) ) {
-        c3_assert(!"khan-chdir");
-      }
-      else {
-        // TODO handle errors
-        unlink(URB_SOCK_PATH);
-        uv_pipe_init(u3L, &cop_u->pyp_u, 0);
-        uv_pipe_bind(&cop_u->pyp_u, URB_SOCK_PATH);
-        uv_listen((uv_stream_t*)&cop_u->pyp_u, 0, _khan_conn_cb);
-
-        if ( 0 != chdir(pax_c) ) {
-          c3_assert(!"khan-back");
-        }
-      }
-    }
   }
 
   return car_u;
