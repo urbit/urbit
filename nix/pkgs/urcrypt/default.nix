ceb1bdb3ec2e4b6233d99c37f2fa900f796fdc24
Joe Bryan <joemfb@gmail.com>
2021-07-29 12:01:40 -0400

    urcrypt: overrides configurePhase, removes builder.sh

--- nix/pkgs/urcrypt/default.nix
+++ nix/pkgs/urcrypt/default.nix
@@ -1,13 +1,17 @@
 { stdenv, autoconf, automake, libtool, m4, pkgconfig, openssl, gmp, secp256k1, scrypt, libaes_siv }:
 
 stdenv.mkDerivation rec {
-  name    = "urcrypt";
-  builder = ./builder.sh;
-  src     = ../../../pkg/urcrypt;
+  name = "urcrypt";
+  src  = ../../../pkg/urcrypt;
 
   nativeBuildInputs =
     [ autoconf automake libtool m4 pkgconfig ];
 
   buildInputs =
     [ openssl gmp secp256k1 scrypt libaes_siv ];
+
+  configurePhase = ''
+    ./autogen.sh
+    ./configure --prefix=$out $configureFlags
+  '';
 }
