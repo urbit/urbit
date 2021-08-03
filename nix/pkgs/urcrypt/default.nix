d0fc560e71027032ead2d53866b060c4f795c6f9
Joe Bryan <joemfb@gmail.com>
2021-08-02 22:22:10 -0400

    urcrypt: use nix autoreconfHook

--- nix/pkgs/urcrypt/default.nix
+++ nix/pkgs/urcrypt/default.nix
@@ -1,17 +1,12 @@
-{ stdenv, autoconf, automake, libtool, m4, pkgconfig, openssl, gmp, secp256k1, scrypt, libaes_siv }:
+{ stdenv, autoreconfHook, pkgconfig, openssl, gmp, secp256k1, scrypt, libaes_siv }:
 
 stdenv.mkDerivation rec {
   name = "urcrypt";
   src  = ../../../pkg/urcrypt;
 
   nativeBuildInputs =
-    [ autoconf automake libtool m4 pkgconfig ];
+    [ autoreconfHook pkgconfig ];
 
   buildInputs =
     [ openssl gmp secp256k1 scrypt libaes_siv ];
-
-  configurePhase = ''
-    ./autogen.sh
-    ./configure --prefix=$out $configureFlags
-  '';
 }
