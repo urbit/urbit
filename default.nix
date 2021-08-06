8051b6a753239529e81cc57a4713e83a6f780e78
Joe Bryan <joemfb@gmail.com>
2021-08-06 10:47:49 -0400

    build: force static urcrypt on darwin

--- default.nix
+++ default.nix
@@ -115,7 +115,7 @@ let
 
     urbit = callPackage ./nix/pkgs/urbit { inherit enableStatic; };
 
-    urcrypt = callPackage ./nix/pkgs/urcrypt { };
+    urcrypt = callPackage ./nix/pkgs/urcrypt { inherit enableStatic; };
 
     docker-image = callPackage ./nix/pkgs/docker-image { };
 
--- nix/pkgs/urcrypt/default.nix
+++ nix/pkgs/urcrypt/default.nix
@@ -1,8 +1,16 @@
-{ stdenv, autoreconfHook, pkgconfig, openssl, gmp, secp256k1, scrypt, libaes_siv }:
+{ stdenv, autoreconfHook, pkgconfig, openssl, gmp, secp256k1, scrypt, libaes_siv
+, enableStatic ? stdenv.hostPlatform.isStatic }:
 
 stdenv.mkDerivation rec {
   name = "urcrypt";
   src  = ../../../pkg/urcrypt;
+
+  # XX why are these required for darwin?
+  dontDisableStatic = enableStatic;
+
+  configureFlags = if enableStatic
+    then [ "--disable-shared" "--enable-static" ]
+    else [];
 
   nativeBuildInputs =
     [ autoreconfHook pkgconfig ];
