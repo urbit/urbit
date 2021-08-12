dd402655d6affdfb8baca0353e5079d2b7c5506d
Liam Fitzgerald <liam@tlon.io>
2021-08-12 14:33:05 +1000

    @urbit/webpack-conf: add webpack configuration package

---
+++ pkg/npm/webpack-conf/index.js
@@ -0,0 +1,166 @@
+const _ = require("lodash");
+const path = require("path");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+function devServer(base, urbitrc = {}) {
+  const router = _.mapKeys(
+    urbitrc.FLEET || {},
+    (_v, k) => `${key}.localhost:9000`
+  );
+  const proxy = !urbitrc.URL
+    ? {}
+    : {
+        index: "",
+        proxy: {
+          [`/apps/${base}/desk.js`]: {
+            changeOrigin: true,
+            target: "http://localhost:80",
+            router,
+          },
+          [`!/apps/${base}`]: {
+            changeOrigin: true,
+            target: "http://localhost:80",
+            router,
+          },
+        },
+      };
+  return {
+    contentBase: path.join(process.cwd(), "./dist"),
+    hot: true,
+    port: 9000,
+    host: "0.0.0.0",
+    disableHostCheck: true,
+    historyApiFallback: true,
+    publicPath: `/apps/${base}`,
+    ...proxy,
+  };
+}
+
+function loadTsDev() {
+  return [
+    {
+      test: /\.(j|t)sx?$/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/typescript",
+            [
+              "@babel/preset-react",
+              {
+                runtime: "automatic",
+                development: true,
+                importSource: "@welldone-software/why-did-you-render",
+              },
+            ],
+          ],
+          plugins: [
+            "@babel/transform-runtime",
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/plugin-proposal-optional-chaining",
+            "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel",
+          ],
+        },
+      },
+      exclude:
+        /node_modules\/(?!(@tlon\/indigo-dark|@tlon\/indigo-light|@tlon\/indigo-react)\/).*/,
+    },
+  ];
+}
+
+function loadTsProd() {
+  return [
+    {
+      test: /\.(j|t)sx?$/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/typescript",
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "lodash",
+            "@babel/transform-runtime",
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/plugin-proposal-optional-chaining",
+            "@babel/plugin-proposal-class-properties",
+          ],
+        },
+      },
+      exclude:
+        /node_modules\/(?!(@tlon\/indigo-dark|@tlon\/indigo-light|@tlon\/indigo-react)\/).*/,
+    },
+  ];
+}
+
+const optimization = isDev
+  ? undefined
+  : {
+      minimize: true,
+      usedExports: true,
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
+        },
+      },
+    };
+
+const config = (base, urbitrc) => ({
+  mode: isDev ? "development" : "production",
+  entry: {
+    app: "./src/index.js",
+  },
+  module: {
+    rules: [
+      ...(isDev ? loadTsDev() : loadTsProd()),
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff2|png|svg|jpg|jpeg)/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+    fallback: {
+      path: false,
+      http: false,
+    },
+  },
+  devtool: isDev ? "inline-source-map" : "source-map",
+  devServer: devServer(base, urbitrc),
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(process.cwd(), "./dist"),
+    publicPath: `/apps/${base}`,
+    clean: true,
+  },
+  optimization,
+});
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  config,
+  loadTs: isDev ? loadTsDev : loadTsProd,
+  devServer,
+};
