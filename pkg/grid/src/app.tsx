952d5c0c3899f50187fb8afab0a74f75f74c8740
Liam Fitzgerald <liam@tlon.io>
2021-08-23 14:58:29 +1000

    grid: update entrypoints for new api

--- pkg/grid/src/app.tsx
+++ pkg/grid/src/app.tsx
@@ -2,12 +2,17 @@ import React, { useEffect } from 'react';
 import Mousetrap from 'mousetrap';
 import { BrowserRouter, Switch, Route, useHistory } from 'react-router-dom';
 import { Grid } from './pages/Grid';
+import useDocketState from './state/docket';
 
 const AppRoutes = () => {
   const { push } = useHistory();
 
   useEffect(() => {
     window.name = 'grid';
+
+    const { fetchAllies, fetchCharges } = useDocketState.getState();
+    fetchCharges();
+    fetchAllies();
 
     Mousetrap.bind(['command+/', 'ctrl+/'], () => {
       push('/leap/search');
--- pkg/grid/src/pages/Grid.tsx
+++ pkg/grid/src/pages/Grid.tsx
@@ -1,8 +1,9 @@
-import { map, pick } from 'lodash-es';
+import { map, omit } from 'lodash-es';
 import React, { FunctionComponent, useEffect } from 'react';
 import { Route, RouteComponentProps } from 'react-router-dom';
 import { MenuState, Nav } from '../nav/Nav';
-import useDocketState from '../state/docket';
+import useDocketState, { useCharges } from '../state/docket';
+import { useKilnState } from '../state/kiln';
 import { RemoveApp } from '../tiles/RemoveApp';
 import { SuspendApp } from '../tiles/SuspendApp';
 import { Tile } from '../tiles/Tile';
@@ -12,11 +13,15 @@ type GridProps = RouteComponentProps<{
 }>;
 
 export const Grid: FunctionComponent<GridProps> = ({ match }) => {
-  const { charges, fetchCharges } = useDocketState((s) => pick(s, ['charges', 'fetchCharges']));
+  const charges = useCharges();
   const chargesLoaded = Object.keys(charges).length > 0;
 
   useEffect(() => {
+    const { fetchCharges, fetchAllies } = useDocketState.getState();
+    const { fetchVats } = useKilnState.getState();
     fetchCharges();
+    fetchAllies();
+    fetchVats();
   }, []);
 
   return (
@@ -30,7 +35,9 @@ export const Grid: FunctionComponent<GridProps> = ({ match }) => {
         {chargesLoaded && (
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 px-4 md:px-8 w-full max-w-6xl">
             {charges &&
-              map(charges, (charge, desk) => <Tile key={desk} docket={charge} desk={desk} />)}
+              map(omit(charges, 'grid'), (charge, desk) => (
+                <Tile key={desk} charge={charge} desk={desk} />
+              ))}
           </div>
         )}
         <Route exact path="/app/:desk/suspend">
