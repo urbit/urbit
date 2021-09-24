b7750ff6055ffb7163e30d222f99883c34f5008f
Hunter Miller <hunter@tlon.io>
2021-09-24 12:43:59 -0500

    landscape: service worker scope off

--- pkg/interface/src/register-sw.js
+++ pkg/interface/src/register-sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/apps/landscape/serviceworker.js', {
-      scope: '/apps/landscape'
+      scope: '/apps/landscape/'
     }).then((reg) => {
     });
   });
