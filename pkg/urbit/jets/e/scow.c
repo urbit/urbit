bdaec0ba914fda29c48be5252a550bf7e8c7e8a7
Sigilante <57601680+sigilante@users.noreply.github.com>
2021-12-27 13:22:24 -0600

    Should have ud/uv/uw working.

--- pkg/urbit/jets/e/scow.c
+++ pkg/urbit/jets/e/scow.c
@@ -4,6 +4,8 @@
 #include "all.h"
 
 #include <ctype.h>
+#include <stdio.h>  // for fprintf
+#include <math.h>   // for ceil/floor
 
 static
 c3_y to_digit(c3_y tig)
@@ -54,9 +56,18 @@ _y_co_two(c3_w src, c3_y* a, c3_y* b)
   *a = to_digit(src / 10);
 }
 
-#define DOT 0x2e
-#define HEP 0x2d
-#define SIG 0x7e
+//
+static
+u3_noun
+_add_year(c3_w year, u3_noun out)
+{
+  while (year > 0) {
+    out = u3nc(to_digit(year % 10), out);
+    year = year / 10;
+  }
+
+  return out;
+}
 
 static
 u3_noun
@@ -65,123 +76,190 @@ _print_da(u3_noun cor, u3_atom raw_da)
   u3_noun hok = u3j_cook("u3we_scow_print_da", u3k(cor), "yore");
   u3_noun yod = u3n_slam_on(hok, u3k(raw_da));
 
-  u3_atom aus = 0;
+  u3_noun out = 0;
 
   u3_atom age, year, month, day, hour, min, sec, f;
   if (c3n == u3r_mean(yod, 4, &age,
-                           5, &year,
-                           6, &month,
-                          14, &day,
-                          30, &hour,
-                          62, &min,
-                         126, &sec,
-                         127, &f,
-                           0)) {
+                      5, &year,
+                      6, &month,
+                      14, &day,
+                      30, &hour,
+                      62, &min,
+                      126, &sec,
+                      127, &f,
+                      0)) {
     return u3m_bail(c3__exit);
   }
 
-  // ~____.__.__..__.__.__..ffff
   if (f != 0) {
-    for (u3_noun cur = f;
+    u3_noun f_list = u3qb_flop(f);
+
+    for (u3_noun cur = f_list;
          _(u3a_is_cell(cur));
          cur = u3t(cur)) {
       if (_(u3a_is_cat(u3h(cur)))) {
         c3_y a, b, c, d;
         _x_co_four(u3h(cur), &a, &b, &c, &d);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), d);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), c);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), b);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), a);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
-        aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+        out = u3nq('.', a, b, u3nt(c, d, out));
       } else {
         // No way to deal with big atoms. fall back.
         u3z(yod);
+        u3z(out);
+        u3z(f_list);
         return u3_none;
       }
     }
+
+    u3z(f_list);
+    out = u3nc('.', out);
   }
 
-  // ~yyyy.mm.dd
-  //  if there isn't a hex list and the h/m/s are all 0, skip printing hours.
+  // if there isn't a hex list and the h/m/s are all 0, skip printing hours.
   if (f != 0 || hour != 0 || min != 0 || sec != 0) {
     if (!_(u3a_is_cat(hour)) ||
         !_(u3a_is_cat(min)) ||
         !_(u3a_is_cat(sec))) {
       // Input is weird, fallback to nock.
       u3z(yod);
+      u3z(out);
       return u3_none;
     }
 
     c3_y sa, sb, ma, mb, ha, hb;
     _y_co_two(sec, &sa, &sb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), sb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), sa);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+    out = u3nq('.', sa, sb, out);
 
     _y_co_two(min, &ma, &mb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), mb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), ma);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+    out = u3nq('.', ma, mb, out);
 
     _y_co_two(hour, &ha, &hb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), hb);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), ha);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+    out = u3nq('.', ha, hb, out);
 
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+    out = u3nc('.', out);
   }
 
-  // ~yyyy.mm.dd..hh.mm.ss..ffff
-  //  We always print the Y.M.D. Unlike others, these numbers are unconstrained
-  //  by length, but in practice, the month number and day number can only be up
-  //  to two digits because of +yore. We still need to remove 0 prefixes,
-  //  though.
+  // We always print the Y.M.D. Unlike others, these numbers are unconstrained
+  // by length, but in practice, the month number and day number can only be up
+  // to two digits because of +yore. We still need to remove 0 prefixes,
+  // though.
   if (!_(u3a_is_cat(day)) || day > 99 ||
       !_(u3a_is_cat(month)) || month > 99 ||
       !_(u3a_is_cat(year))) {
     // Input is weird, fallback to nock.
     u3z(yod);
+    u3z(out);
     return u3_none;
   }
 
   c3_y da, db;
   _y_co_two(day, &da, &db);
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), db);
+  out = u3nc(db, out);
   if (da != '0') {
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), da);
+    out = u3nc(da, out);
   }
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+  out = u3nc('.', out);
 
   c3_y ma, mb;
   _y_co_two(month, &ma, &mb);
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), mb);
+  out = u3nc(mb, out);
   if (ma != '0') {
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), ma);
+    out = u3nc(ma, out);
   }
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+  out = u3nc('.', out);
 
   // suffix the year with a '-' for BC dates
   if (age == c3n) {
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), HEP);
+    out = u3nc('-', out);
   }
 
   // The year part is the only place where we have to explicitly loop over the
   // input because it can be arbitrarily large or small.
-  while (year > 0) {
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), to_digit(year % 10));
-    year = year / 10;
-  }
+  out = _add_year(year, out);
 
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), SIG);
+  out = u3nc('~', out);
 
   u3z(yod);
-  return aus;
+  return out;
 }
 
-#undef DOT
-#undef HEP
-#undef SIG
+static
+u3_noun
+_print_p(u3_atom cor, u3_atom p)
+{
+  // Scramble the raw number to the concealed version.
+  u3_noun ob = u3j_cook("u3we_scow_ob_p", u3k(cor), "ob");
+  u3_noun hok = u3j_cook("u3we_scow_fein_p", ob, "fein");
+  u3_atom sxz = u3n_slam_on(hok, u3k(p));
+
+  // Simple galaxy case
+  if (c3y == u3qa_lth(sxz, 256)) {
+    c3_y a, b, c;
+    u3_po_to_suffix(sxz, &a, &b, &c);
+    u3z(sxz);
+    return u3nq('~', a, b, u3nc(c, 0));
+  }
+
+  u3_atom dyy = u3qc_met(4, sxz);
+  if (!_(u3a_is_cat(dyy))) {
+    u3z(sxz);
+    u3z(dyy);
+    return u3_none;
+  }
+
+  u3_noun list = 0;
+  for (c3_w imp = 0; imp != dyy; ++imp) {
+    c3_w log = u3qc_end(4, 1, sxz);
+    c3_w prefix = u3qc_rsh(3, 1, log);
+    c3_w suffix = u3qc_end(3, 1, log);
+
+    c3_y a, b, c, d, e, f;
+    u3_po_to_prefix(prefix, &a, &b, &c);
+    u3_po_to_suffix(suffix, &d, &e, &f);
+
+    if (imp % 4 == 0) {
+      if (imp != 0) {
+        list = u3nt('-', '-', list);
+      }
+    } else {
+      list = u3nc('-', list);
+    }
+
+    list = u3nq(a, b, c, u3nq(d, e, f, list));
+
+    sxz = u3qc_rsh(4, 1, sxz);
+  }
+
+  u3z(sxz);
+  return u3nc('~', list);
+}
+
+static
+u3_noun
+_print_ud(u3_atom ud)
+{
+  // number of characters printed "between" periods.
+  c3_i between = 0;
+  u3_noun list = 0;
+
+  // increase input refcount to be consumed in u3ka_div(), which will free each
+  // intermediary state.
+  u3k(ud);
+
+  do {
+    if (between == 3) {
+      list = u3nc('.', list);
+      between = 0;
+    }
+
+    list = u3nc(u3ka_add(u3qa_mod(ud, 10), '0'), list);
+    between++;
+    ud = u3ka_div(ud, 10);
+  } while (ud != 0);
+
+  return list;
+}
+
+////////////////////////////////////////////////////////////////////////////
 
 #define SIG 0x7e
 #define HEP 0x2d
@@ -727,13 +805,16 @@ _u3_po_to_suffix(u3_noun id)
 
 static
 u3_noun
-_print_p(u3_atom cor, u3_atom p)
+__print_p(c3_w p)
 {
   // Scramble the raw number to the concealed version.
   u3_noun sxz = u3qe_fein_ob(p);
   c3_w prefix, suffix;
   u3_noun pre, suf;
   u3_atom aus = 0;
+  fprintf(stderr, "p: %d\n", p);
+  u3m_p("sxz", sxz);
+  return u3_none;
 
   // Galaxies
   if (c3y == u3qa_lth(sxz, 256)) {
@@ -789,74 +870,43 @@ _print_p(u3_atom cor, u3_atom p)
 
 static
 u3_noun
-_print_ud(u3_atom ud)
+__print_ud(c3_w ud)
 {
   // number of characters printed "between" periods.
   c3_i between = 0;
-  u3_noun aus = 0;
   c3_w wrt = 0;
+  c3_y tig;
 
-  // increase input refcount to be consumed in u3ka_div(), which will free each
-  // intermediary state.
-  u3k(ud);
+  // make byte array for characters
+  //  number of digits
+  c3_w dig = (ud > 0) ? (c3_w)floor(log10((double)ud) + 1) : 1;
+  //  number of dots
+  c3_w blx = (ud > 0) ? (c3_w)floor(log10((double)ud-1.0) + 1) / 3 : 0;
+  c3_w len = dig + blx;
+  c3_y* aus = (c3_y*)u3a_malloc(len*sizeof(c3_y));
 
+  c3_w idx = len-1;
   do {
     if (between == 3) {
-      aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+      aus[idx--] = DOT;
       between = 0;
     }
 
-    wrt = u3ka_add(u3qa_mod(ud, 10), '0');
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), wrt);
+    tig = ud % 10;
+    wrt = to_digit(tig);
+    aus[idx--] = wrt;
     between++;
-    ud = u3ka_div(ud, 10);
+    ud = ud / 10;
   } while (ud != 0);
 
-  u3z(ud);
-  return aus;
+  u3_noun aus_return = u3i_bytes(len, aus);
+  u3a_free(aus);
+  u3m_p("ud> aus_return", aus_return);
+  return aus_return;
 }
 
 #undef DOT
 
-#define DOT 0x2e
-#define ZED 0x30
-#define VEE 0x76
-
-static
-u3_noun
-_print_uv(u3_atom uv)
-{
-  // number of characters printed "between" periods.
-  c3_i between = 0;
-  u3_noun aus = 0;
-  c3_w wrt = 0;
-
-  // increase input refcount to be consumed in u3ka_div(), which will free each
-  // intermediary state.
-  u3k(uv);
-
-  do {
-    if (between == 5) {
-      aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
-      between = 0;
-    }
-
-    c3_y tig = u3qa_mod(uv, 32);
-    wrt = to_digit(tig);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), wrt);
-    between++;
-    uv = u3ka_div(uv, 32);
-  } while (uv != 0);
-
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), VEE);
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), ZED);
-  u3z(uv);
-  return aus;
-}
-
-#undef DOT
-#undef ZED
-#undef VEE
 
 #define DOT 0x2e
 #define ZED 0x30
@@ -864,12 +914,132 @@ _print_uv(u3_atom uv)
 
 static
 u3_noun
+__print_uw(c3_w uw)
+{
+  // number of characters printed "between" periods.
+  c3_i between = 0;
+  c3_w wrt = 0;
+  c3_y tig;
+
+  // make byte array for characters
+  //  number of digits
+  c3_w dig = (uw > 0) ? (c3_w)floor(log10((double)uw)/log10(64.0) + 1) : 1;
+  //  number of dots
+  c3_w blx = (uw > 0) ? (c3_w)floor(log10((double)uw-1.0)/log10(64.0) + 1) / 5 : 0;
+  c3_w len = dig + blx + 2;
+  c3_y* aus = (c3_y*)u3a_malloc(len*sizeof(c3_y));
+
+  c3_w idx = len-1;
+  do {
+    if (between == 5) {
+      aus[idx--] = DOT;
+      between = 0;
+    }
+
+    tig = uw % 64;
+    wrt = to_w_digit(tig);
+    aus[idx--] = wrt;
+    between++;
+    uw = uw / 64;
+  } while (uw != 0);
+  aus[1] = WEE;
+  aus[0] = ZED;
+
+  u3_noun aus_return = u3i_bytes(len, aus);
+  u3a_free(aus);
+  u3m_p("uw> aus_return", aus_return);
+  return aus_return;
+}
+
+#undef DOT
+#undef ZED
+#undef WEE
+
+////////////////////////////////////////////////////////////////////////////
+
+static
+u3_noun
+_print_uv(u3_atom uv)
+{
+  // number of characters printed "between" periods.
+  c3_i between = 0;
+  u3_noun list = 0;
+
+  // increase input refcount to be consumed in u3ka_div(), which will free each
+  // intermediary state.
+  u3k(uv);
+
+  do {
+    if (between == 5) {
+      list = u3nc('.', list);
+      between = 0;
+    }
+
+    c3_y tig = u3qa_mod(uv, 32);
+    list = u3nc(to_digit(tig), list);
+    between++;
+    uv = u3ka_div(uv, 32);
+  } while (uv != 0);
+
+  return u3nt('0', 'v', list);
+}
+
+// works
+
+#define DOT 0x2e
+#define ZED 0x30
+#define VEE 0x76
+
+static
+u3_noun
+__print_uv(c3_w uv)
+{
+  // number of characters printed "between" periods.
+  c3_i between = 0;
+  c3_w wrt = 0;
+  c3_y tig;
+
+  // make byte array for characters
+  //  number of digits
+  c3_w dig = (uv > 0) ? (c3_w)floor(log10((double)uv)/log10(32.0) + 1) : 1;
+  //  number of dots
+  c3_w blx = (uv > 0) ? (c3_w)floor(log10((double)uv-1.0)/log10(32.0) + 1) / 5 : 0;
+  c3_w len = dig + blx + 2;
+  c3_y* aus = (c3_y*)u3a_malloc(len*sizeof(c3_y));
+
+  c3_w idx = len-1;
+  do {
+    if (between == 5) {
+      aus[idx--] = DOT;
+      between = 0;
+    }
+
+    tig = uv % 32;
+    wrt = to_digit(tig);
+    aus[idx--] = wrt;
+    between++;
+    uv = uv / 32;
+  } while (uv != 0);
+  aus[1] = VEE;
+  aus[0] = ZED;
+
+  u3_noun aus_return = u3i_bytes(len, aus);
+  u3a_free(aus);
+  u3m_p("uv> aus_return", aus_return);
+  return aus_return;
+}
+
+#undef DOT
+#undef ZED
+#undef VEE
+
+static
+u3_noun
 _print_uw(u3_atom uw)
 {
   // number of characters printed "between" periods.
   c3_i between = 0;
-  u3_noun aus = 0;
-  c3_w wrt = 0;
+  u3_noun list = 0;
 
   // increase input refcount to be consumed in u3ka_div(), which will free each
   // intermediary state.
@@ -877,26 +1047,18 @@ _print_uw(u3_atom uw)
 
   do {
     if (between == 5) {
-      aus = u3qa_add(u3qc_lsh(2, 2, aus), DOT);
+      list = u3nc('.', list);
       between = 0;
     }
 
     c3_y tig = u3qa_mod(uw, 64);
-    wrt = to_w_digit(tig);
-    aus = u3qa_add(u3qc_lsh(2, 2, aus), wrt);
+    list = u3nc(to_w_digit(tig), list);
     between++;
     uw = u3ka_div(uw, 64);
   } while (uw != 0);
 
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), WEE);
-  aus = u3qa_add(u3qc_lsh(2, 2, aus), ZED);
-  u3z(uw);
-  return aus;
+  return u3nt('0', 'w', list);
 }
-
-#undef DOT
-#undef ZED
-#undef WEE
 
 u3_noun
 u3we_scow(u3_noun cor)
@@ -908,38 +1070,16 @@ u3we_scow(u3_noun cor)
                            u3x_sam_3, &atom, 0)) {
     return u3m_bail(c3__exit);
   }
+  u3m_p("scow> %x", atom);
 
   u3_noun knot;
-  switch (mod) {
-    case c3__da:
-      knot = _print_da(cor, atom);
-      break;
-
-    case 'p':
-      knot = _print_p(cor, atom);
-      break;
-
-    case c3__ud:
-      knot = _print_ud(atom);
-      break;
-
-    case c3__uv:
-      knot = _print_uv(atom);
-      break;
-
-    case c3__uw:
-      knot = _print_uw(atom);
-      break;
-
-    default:
-      return u3_none;
-  }
-
+  knot = u3we_scot(cor);
   if (knot == u3_none) {
     return knot;
   }
   u3_noun ret = u3qe_trip(knot);
   u3z(knot);
+  u3m_p("scow> %x", ret);
   return ret;
 }
 
@@ -948,36 +1088,87 @@ u3we_scot(u3_noun cor)
 {
   u3_atom mod;
   u3_atom atom;
-  u3_atom aus;
 
   if (c3n == u3r_mean(cor, u3x_sam_2, &mod,
                            u3x_sam_3, &atom, 0)) {
     return u3m_bail(c3__exit);
   }
-      return u3_none;
+  //return u3_none;
 
+  u3_noun tape;
+  mpz_t u_mp;
   switch (mod) {
     case c3__da:
-      aus = _print_da(cor, atom);
-      return aus;
+      //return __print_da(cor, atom);
+    return u3_none;
+      tape = _print_da(cor, atom);
+      break;
 
     case 'p':
-      aus = _print_p(cor, atom);
-      return aus;
+      return u3_none;
+      u3r_mp(u_mp, atom);
+
+      // if too big, send back to Hoon
+      if (!(mpz_fits_ulong_p(u_mp))) {  // check against unsigned long int, 32 bits
+        mpz_clear(u_mp);
+        return u3_none;
+      } else {
+        c3_w uv_int = mpz_get_ui(u_mp);
+        mpz_clear(u_mp);
+        return __print_p(uv_int);
+      }
+      break;
 
     case c3__ud:
-      aus = _print_ud(atom);
-      return aus;
+      u3r_mp(u_mp, atom);
+
+      // if too big, send back to Hoon
+      if (!(mpz_fits_ulong_p(u_mp))) {  // check against unsigned long int, 32 bits
+        mpz_clear(u_mp);
+        return u3_none;
+      } else {
+        c3_w uv_int = mpz_get_ui(u_mp);
+        mpz_clear(u_mp);
+        return __print_ud(uv_int);
+      }
+      break;
 
     case c3__uv:
-      aus = _print_uv(atom);
-      return aus;
+      u3r_mp(u_mp, atom);
+
+      // if too big, send back to Hoon
+      if (!(mpz_fits_ulong_p(u_mp))) {  // check against unsigned long int, 32 bits
+        mpz_clear(u_mp);
+        return u3_none;
+      } else {
+        c3_w uv_int = mpz_get_ui(u_mp);
+        mpz_clear(u_mp);
+        return __print_uv(uv_int);
+      }
+      break;
 
     case c3__uw:
-      aus = _print_uw(atom);
-      return aus;
+      u3r_mp(u_mp, atom);
+
+      // if too big, send back to Hoon
+      if (!(mpz_fits_ulong_p(u_mp))) {  // check against unsigned long int, 32 bits
+        mpz_clear(u_mp);
+        return u3_none;
+      } else {
+        c3_w uw_int = mpz_get_ui(u_mp);
+        mpz_clear(u_mp);
+        return __print_uw(uw_int);
+      }
+      break;
 
     default:
       return u3_none;
   }
+
+  if (tape == u3_none) {
+    return tape;
+  }
+  u3_noun ret = u3qc_rap(3, tape);
+  u3z(tape);
+  return ret;
 }
