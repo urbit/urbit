afea2edc59dd1576dbb3f63a4706f505fc9dd051
fang <git@fang.io>
2022-01-07 14:02:28 +0100

    webterm: improve selection look & feel

--- pkg/interface/webterm/app.tsx
+++ pkg/interface/webterm/app.tsx
@@ -45,7 +45,8 @@ const makeTheme = (dark: boolean): ITheme => {
     foreground: fg,
     background: bg,
     brightBlack: '#7f7f7f',  // NOTE  slogs
-    cursor: fg
+    cursor: fg,
+    selection: fg
   };
 };
 
@@ -66,7 +67,9 @@ const termConfig: ITerminalOptions = {
   bellSound: bel,
   //
   //  allows text selection by holding modifier (option, or shift)
-  macOptionClickForcesSelection: true
+  macOptionClickForcesSelection: true,
+  //  prevent insertion of simulated arrow keys on-altclick
+  altClickMovesCursor: false
 };
 
 const csi = (cmd: string, ...args: number[]) => {
