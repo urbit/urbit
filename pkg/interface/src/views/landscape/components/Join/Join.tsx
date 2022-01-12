1ddd38e82caf4e4419b292481a31449cb40d400f
Hunter Miller <hunter@tlon.io>
2022-01-11 19:32:51 -0600

    join: removing unused props, fixing invite 'kind' for proper linking

--- pkg/interface/src/views/landscape/components/Join/Join.tsx
+++ pkg/interface/src/views/landscape/components/Join/Join.tsx
@@ -7,19 +7,19 @@ import {
   ManagedTextInputField,
   ManagedCheckboxField,
   ContinuousProgressBar,
-} from "@tlon/indigo-react";
-import { Formik, Form } from "formik";
-import React, { useEffect } from "react";
-import { useHistory, useLocation, useParams } from "react-router-dom";
-import useGroupState from "~/logic/state/group";
-import useInviteState, { useInviteForResource } from "~/logic/state/invite";
-import useMetadataState, { usePreview } from "~/logic/state/metadata";
-import { decline, Invite } from "@urbit/api";
-import { join, JoinRequest } from "@urbit/api/groups";
-import airlock from "~/logic/api";
-import { joinError, joinResult, joinLoad, JoinProgress } from "@urbit/api";
-import { useQuery } from "~/logic/lib/useQuery";
-import { JoinKind, JoinDesc, JoinSkeleton } from "./Skeleton";
+} from '@tlon/indigo-react';
+import { Formik, Form } from 'formik';
+import React, { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import useGroupState from '~/logic/state/group';
+import { useInviteForResource } from '~/logic/state/invite';
+import { usePreview } from '~/logic/state/metadata';
+import { decline, Invite } from '@urbit/api';
+import { join, JoinRequest } from '@urbit/api/groups';
+import airlock from '~/logic/api';
+import { joinError, joinLoad, JoinProgress } from '@urbit/api';
+import { useQuery } from '~/logic/lib/useQuery';
+import { JoinKind, JoinDesc, JoinSkeleton } from './Skeleton';
 
 interface InviteWithUid extends Invite {
   uid: string;
@@ -42,7 +42,7 @@ function JoinForm(props: {
 }) {
   const { desc, dismiss, invite } = props;
   const onSubmit = (values: FormSchema) => {
-    const [, , ship, name] = desc.group.split("/");
+    const [, , ship, name] = desc.group.split('/');
     airlock.poke(
       join(ship, name, desc.kind, values.autojoin, values.shareContact)
     );
@@ -52,26 +52,26 @@ function JoinForm(props: {
     airlock.poke(decline(desc.kind, invite.uid));
     dismiss();
   };
-  const isGroups = desc.kind === "groups";
+  const isGroups = desc.kind === 'groups';
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
       <Form>
-        <Col p="4" gapY="4">
+        <Col p='4' gapY='4'>
           {isGroups ? (
-            <ManagedCheckboxField id="autojoin" label="Join all channels" />
+            <ManagedCheckboxField id='autojoin' label='Join all channels' />
           ) : null}
-          <ManagedCheckboxField id="shareContact" label="Share identity" />
-          <Row justifyContent="space-between" width="100%">
+          <ManagedCheckboxField id='shareContact' label='Share identity' />
+          <Row justifyContent='space-between' width='100%'>
             <Button onClick={dismiss}>Dismiss</Button>
-            <Row gapX="2">
+            <Row gapX='2'>
               {!invite ? null : (
-                <Button onClick={onDecline} destructive type="button">
+                <Button onClick={onDecline} destructive type='button'>
                   Decline
                 </Button>
               )}
-              <Button primary type="submit">
-                {!invite ? "Join Group" : "Accept"}
+              <Button primary type='submit'>
+                {!invite ? 'Join Group' : 'Accept'}
               </Button>
             </Row>
           </Row>
@@ -80,10 +80,6 @@ function JoinForm(props: {
     </Formik>
   );
 }
-const REQUEST: JoinDesc = {
-  group: "/ship/~bitbet-bolbel/urbit-community",
-  kind: "groups",
-};
 
 export function JoinInitial(props: {
   invite?: InviteWithUid;
@@ -93,7 +89,7 @@ export function JoinInitial(props: {
 }) {
   const { desc, dismiss, modal, invite } = props;
   const title = (() => {
-    const name = desc.kind === "graph" ? "Group Chat" : "Group";
+    const name = desc.kind === 'graph' ? 'Group Chat' : 'Group';
     if (invite) {
       return `You've been invited to a ${name}`;
     } else {
@@ -117,11 +113,11 @@ function JoinLoading(props: {
   const { desc, request, dismiss, modal, finished } = props;
   const history = useHistory();
   useEffect(() => {
-    if (desc.kind === "graph" && request.progress === "done") {
+    if (desc.kind === 'graph' && request.progress === 'done') {
       history.push(finished);
     }
   }, [request]);
-  const name = desc.kind === "graph" ? "Group Chat" : "Group";
+  const name = desc.kind === 'graph' ? 'Group Chat' : 'Group';
   const title = `Joining ${name}, please wait`;
   const onCancel = () => {
     useGroupState.getState().abortJoin(desc.group);
@@ -129,7 +125,7 @@ function JoinLoading(props: {
   };
   return (
     <JoinSkeleton modal={modal} desc={desc} title={title}>
-      <Col maxWidth="512px" p="4" gapY="4">
+      <Col maxWidth='512px' p='4' gapY='4'>
         {joinLoad.indexOf(request.progress as any) !== -1 ? (
           <JoinProgressIndicator progress={request.progress} />
         ) : null}
@@ -139,7 +135,7 @@ function JoinLoading(props: {
             offline, or the connection between you both may be unstable.
           </Text>
         </Box>
-        <Row gapX="2">
+        <Row gapX='2'>
           <Button onClick={dismiss}>Dismiss</Button>
           <Button destructive onClick={onCancel}>
             Cancel Join
@@ -160,14 +156,14 @@ function JoinError(props: {
   const group = preview?.metadata?.title ?? desc.group;
   const title = `Joining ${group} failed`;
   const explanation =
-    request.progress === "no-perms"
-      ? "You do not have the correct permissions"
-      : "An unexpected error occurred";
+    request.progress === 'no-perms'
+      ? 'You do not have the correct permissions'
+      : 'An unexpected error occurred';
 
   return (
     <JoinSkeleton modal={modal} title={title} desc={desc}>
-      <Col p="4" gapY="4">
-        <Text fontWeight="medium">{explanation}</Text>
+      <Col p='4' gapY='4'>
+        <Text fontWeight='medium'>{explanation}</Text>
         <Row>
           <Button>Dismiss</Button>
         </Row>
@@ -186,26 +182,26 @@ export interface JoinProps {
 export function Join(props: JoinProps) {
   const { desc, modal, dismiss, redir } = props;
   const { group, kind } = desc;
-  const [, , ship, name] = group.split("/");
-  const graph = kind === "graph";
-  const finishedPath = !!redir
+  const [, , ship, name] = group.split('/');
+  const graph = kind === 'graph';
+  const finishedPath = redir
     ? redir
     : graph
     ? `/~landscape/messages/resource/chat/${ship}/${name}`
     : `/~landscape/ship/${ship}/${name}`;
 
   const history = useHistory();
-  const joinRequest = useGroupState((s) => s.pendingJoin[group]);
+  const joinRequest = useGroupState(s => s.pendingJoin[group]);
   const invite = useInviteForResource(kind, ship, name);
 
-  const isDone = joinRequest && joinRequest.progress === "done";
+  const isDone = joinRequest && joinRequest.progress === 'done';
   const isErrored =
     joinRequest && joinError.includes(joinRequest.progress as any);
   const isLoading =
     joinRequest && joinLoad.includes(joinRequest.progress as any);
 
   useEffect(() => {
-    if (isDone && desc.kind == "graph") {
+    if (isDone && desc.kind == 'graph') {
       history.push(finishedPath);
     }
   }, [isDone, desc]);
@@ -232,10 +228,6 @@ export function Join(props: JoinProps) {
   );
 }
 
-interface PromptFormProps {
-  kind: string;
-}
-
 interface PromptFormSchema {
   link: string;
 }
@@ -245,37 +237,37 @@ export interface JoinPromptProps {
 }
 
 export function JoinPrompt(props: JoinPromptProps) {
-  const { kind, dismiss } = props;
-  const { query, appendQuery } = useQuery();
+  const { dismiss } = props;
+  const { appendQuery } = useQuery();
   const history = useHistory();
   const initialValues = {
-    link: "",
+    link: ''
   };
 
   const onSubmit = async ({ link }: PromptFormSchema) => {
     const path = `/ship/${link}`;
     history.push({
-      search: appendQuery({ "join-path": path }),
+      search: appendQuery({ 'join-path': path })
     });
   };
 
   return (
-    <JoinSkeleton modal body={<Text>a</Text>} title="Join a Group">
+    <JoinSkeleton modal body={<Text>a</Text>} title='Join a Group'>
       <Formik initialValues={initialValues} onSubmit={onSubmit}>
         <Form>
-          <Col p="4" gapY="4">
+          <Col p='4' gapY='4'>
             <ManagedTextInputField
-              label="Invite Link"
-              id="link"
-              caption="Enter either a web+urbitgraph:// link or an identifier in the form ~sampel-palnet/group"
+              label='Invite Link'
+              id='link'
+              caption='Enter either a web+urbitgraph:// link or an identifier in the form ~sampel-palnet/group'
             />
-            <Row gapX="2">
-              {!!dismiss ? (
-                <Button type="button" onClick={dismiss}>
+            <Row gapX='2'>
+              {dismiss ? (
+                <Button type='button' onClick={dismiss}>
                   Dismiss
                 </Button>
               ) : null}
-              <Button type="submit" primary>
+              <Button type='submit' primary>
                 Join
               </Button>
             </Row>
@@ -289,26 +281,26 @@ export function JoinPrompt(props: JoinPromptProps) {
 function JoinProgressIndicator(props: { progress: JoinProgress }) {
   const { progress } = props;
   const percentage =
-    progress === "done" ? 100 : (joinLoad.indexOf(progress as any) + 1) * 25;
+    progress === 'done' ? 100 : (joinLoad.indexOf(progress as any) + 1) * 25;
 
   const description = (() => {
     switch (progress) {
-      case "start":
-        return "Connecting to host";
-      case "added":
-        return "Retrieving members";
-      case "metadata":
-        return "Retrieving channels";
-      case "done":
-        return "Finished";
+      case 'start':
+        return 'Connecting to host';
+      case 'added':
+        return 'Retrieving members';
+      case 'metadata':
+        return 'Retrieving channels';
+      case 'done':
+        return 'Finished';
       default:
-        return "";
+        return '';
     }
   })();
 
   return (
-    <Col gapY="2">
-      <Text color="lightGray">{description}</Text>
+    <Col gapY='2'>
+      <Text color='lightGray'>{description}</Text>
       <ContinuousProgressBar percentage={percentage} />
     </Col>
   );
@@ -323,8 +315,7 @@ export interface JoinDoneProps {
 
 export function JoinDone(props: JoinDoneProps) {
   const { desc, modal, finished, dismiss } = props;
-  const { preview, error } = usePreview(desc.group);
-  const name = desc.kind === "groups" ? "Group" : "Group Chat";
+  const name = desc.kind === 'groups' ? 'Group' : 'Group Chat';
   const title = `Joined ${name} successfully`;
   const history = useHistory();
 
@@ -334,9 +325,9 @@ export function JoinDone(props: JoinDoneProps) {
 
   return (
     <JoinSkeleton title={title} modal={modal} desc={desc}>
-      <Col p="4" gapY="4">
-        <JoinProgressIndicator progress="done" />
-        <Row gapX="2">
+      <Col p='4' gapY='4'>
+        <JoinProgressIndicator progress='done' />
+        <Row gapX='2'>
           <Button onClick={dismiss}>Dismiss</Button>
           <Button onClick={onView} primary>
             View Group
@@ -347,21 +338,20 @@ export function JoinDone(props: JoinDoneProps) {
   );
 }
 
-export function JoinRoute(props: { graph?: boolean; modal?: boolean }) {
-  const { modal = false, graph = false } = props;
+export function JoinRoute() {
   const { query } = useQuery();
   const history = useHistory();
   const { pathname } = useLocation();
-  const kind = query.get("join-kind");
-  const path = query.get("join-path");
-  const redir = query.get("redir");
+  const kind = query.get('join-kind');
+  const path = query.get('join-path');
+  const redir = query.get('redir');
   if (!kind) {
     return null;
   }
   const desc: JoinDesc = path
     ? {
         group: path,
-        kind: graph ? "graph" : "groups",
+        kind: kind as JoinKind
       }
     : undefined;
 
