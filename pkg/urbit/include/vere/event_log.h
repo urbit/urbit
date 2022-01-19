e37bac596a757ba4bdc09d21a8c39716f827269c
Peter McEvoy <peter@tlon.io>
2022-01-19 15:00:01 -0800

    event log: change to Doxygen-compatible commenting style

--- pkg/urbit/include/vere/event_log.h
+++ pkg/urbit/include/vere/event_log.h
@@ -1,30 +1,26 @@
-/* i/n/e.h
-**
-** This file is in the public domain.
-*/
-  /** Functions.
-  **/
-    /* u3e_fault(): handle a memory event with libsigsegv protocol.
-    */
-      c3_i
-      u3e_fault(void* adr_v, c3_i ser_i);
+//! @file event_log.h
 
-    /* u3e_save():
-    */
-      void
-      u3e_save(void);
 
-    /* u3e_live(): start the persistence system.  Return c3y if no image.
-    */
-      c3_o
-      u3e_live(c3_o nuu_o, c3_c* dir_c);
+//==============================================================================
+// Functions
+//==============================================================================
 
-    /* u3e_yolo(): disable dirty page tracking, read/write whole loom.
-    */
-      c3_o
-      u3e_yolo(void);
+//! TODO(peter)
+c3_i
+u3e_fault(void* adr_v, c3_i ser_i);
 
-    /* u3e_foul(): dirty all the pages of the loom.
-    */
-      void
-      u3e_foul(void);
+//! TODO(peter)
+void
+u3e_save(void);
+
+//! TODO(peter): start the persistence system.  Return c3y if no image.
+c3_o
+u3e_live(c3_o nuu_o, c3_c* dir_c);
+
+//! TODO(peter): disable dirty page tracking, read/write whole loom.
+c3_o
+u3e_yolo(void);
+
+//! TODO(peter): dirty all the pages of the loom.
+void
+u3e_foul(void);
