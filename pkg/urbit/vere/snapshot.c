f041c80abc957578eea6d701c01041710d8553ec
Peter McEvoy <peter@tlon.io>
2022-01-19 16:54:30 -0800

    snapshot: replace _el_ in type and function names with _snap_

--- pkg/urbit/vere/snapshot.c
+++ pkg/urbit/vere/snapshot.c
@@ -14,38 +14,38 @@
 typedef struct {
   c3_w pag_w;
   c3_w mug_w;
-} u3_el_line;
+} u3_snap_line;
 
 //! Memory change, control file.
 typedef struct {
-  c3_w       ver_y;     //!< version number
-  c3_w       nor_w;     //!< new page count north
-  c3_w       sou_w;     //!< new page count south
-  c3_w       pgs_w;     //!< number of changed pages
-  u3_el_line mem_u[0];  //!< per page
-} u3_el_control;
+  c3_w       ver_y;       //!< version number
+  c3_w       nor_w;       //!< new page count north
+  c3_w       sou_w;       //!< new page count south
+  c3_w       pgs_w;       //!< number of changed pages
+  u3_snap_line mem_u[0];  //!< per page
+} u3_snap_control;
 
 //! Memory change, top level.
 typedef struct {
   c3_i           ctl_i;
   c3_i           mem_i;
-  u3_el_control* con_u;
-} u3_el_patch;
+  u3_snap_control* con_u;
+} u3_snap_patch;
 
 //! Memory segment, open file.
 typedef struct {
   c3_c* nam_c;  //!< segment name
   c3_i  fid_i;  //!< open file, or 0
   c3_w  pgs_w;  //!< length in pages
-} u3_el_image;
+} u3_snap_image;
 
 //! Entire memory system.
 typedef struct {
   c3_c*       dir_c;                  //!< path to
   c3_w        dit_w[u3a_pages >> 5];  //!< touched since last save
-  u3_el_image nor_u;                  //!< north segment
-  u3_el_image sou_u;                  //!< south segment
-} u3_el_pool;
+  u3_snap_image nor_u;                //!< north segment
+  u3_snap_image sou_u;                //!< south segment
+} u3_snap_pool;
 
 #ifdef U3_SNAPSHOT_VALIDATION
 //! Image check
@@ -61,8 +61,8 @@ struct {
 // Constants
 //==============================================================================
 
-//! Event log version number.
-#define u3_el_version 1
+//! Snapshot version number.
+#define u3_snap_version 1
 
 
 //==============================================================================
@@ -70,7 +70,7 @@ struct {
 //==============================================================================
 
 //! Global memory control.
-static u3_el_pool pol_u;
+static u3_snap_pool pol_u;
 
 
 //==============================================================================
@@ -87,7 +87,7 @@ static u3_el_pool pol_u;
 //! @return c3y  image file was successfully opened (or created).
 //! @return c3n  image file could not be opened (or created).
 static c3_o
-_el_image_open(u3_el_image* img_u)
+_snap_image_open(u3_snap_image* img_u)
 {
   c3_i mod_i = O_RDWR | O_CREAT;
   c3_c ful_c[8193];
@@ -141,7 +141,7 @@ _el_image_open(u3_el_image* img_u)
 //!
 //! @param[in] pat_u  patch struct to free. Can be NULL.
 static void
-_el_patch_delete(u3_el_patch* pat_u)
+_snap_patch_delete(u3_snap_patch* pat_u)
 {
   if ( 0 != pat_u ) {
     c3_free(pat_u->con_u);
@@ -167,14 +167,14 @@ _el_patch_delete(u3_el_patch* pat_u)
 //! @return c3n  patch failed verification because of a version mismatch, a
 //!              filesystem error, or a mug mismatch.
 static c3_o
-_el_patch_verify(u3_el_patch* pat_u)
+_snap_patch_verify(u3_snap_patch* pat_u)
 {
   c3_w i_w;
 
-  if ( u3_el_version != pat_u->con_u->ver_y ) {
+  if ( u3_snap_version != pat_u->con_u->ver_y ) {
     fprintf(stderr, "loom: patch version mismatch: have %u, need %u\r\n",
                     pat_u->con_u->ver_y,
-                    u3_el_version);
+                    u3_snap_version);
     return c3n;
   }
 
@@ -214,10 +214,10 @@ _el_patch_verify(u3_el_patch* pat_u)
 //! @return pat_u  successfully opened patch.
 //! @return NULL   patch could not be opened because of a filesystem error or
 //!                mug mismatch.
-static u3_el_patch*
-_el_patch_open(void)
+static u3_snap_patch*
+_snap_patch_open(void)
 {
-  u3_el_patch* pat_u = 0;
+  u3_snap_patch* pat_u = 0;
   c3_c ful_c[8193];
   c3_i ctl_i, mem_i;
 
@@ -236,15 +236,15 @@ _el_patch_open(void)
   if ( -1 == (mem_i = open(ful_c, O_RDWR)) ) {
     close(ctl_i);
 
-    _el_patch_delete(pat_u);
+    _snap_patch_delete(pat_u);
     return 0;
   }
-  pat_u = c3_malloc(sizeof(u3_el_patch));
+  pat_u = c3_malloc(sizeof(u3_snap_patch));
   pat_u->ctl_i = ctl_i;
   pat_u->mem_i = mem_i;
   pat_u->con_u = 0;
 
-  // Read u3_el_control struct from patch's control file.
+  // Read u3_snap_control struct from patch's control file.
   {
     struct stat buf_u;
     c3_assert(-1 != fstat(pat_u->ctl_i, &buf_u));
@@ -253,15 +253,15 @@ _el_patch_open(void)
     pat_u->con_u = c3_malloc(len_w);
 
     if ( len_w != read(pat_u->ctl_i, pat_u->con_u, len_w) ||
-         len_w != sizeof(u3_el_control) +
-                  (pat_u->con_u->pgs_w * sizeof(u3_el_line)) )
+         len_w != sizeof(u3_snap_control) +
+                  (pat_u->con_u->pgs_w * sizeof(u3_snap_line)) )
     {
-      _el_patch_delete(pat_u);
+      _snap_patch_delete(pat_u);
       return 0;
     }
   }
-  if ( c3n == _el_patch_verify(pat_u) ) {
-    _el_patch_delete(pat_u);
+  if ( c3n == _snap_patch_verify(pat_u) ) {
+    _snap_patch_delete(pat_u);
     return 0;
   }
   return pat_u;
@@ -269,7 +269,7 @@ _el_patch_open(void)
 
 //! Save a page, producing a new page counter.
 static c3_w
-_el_patch_save_page(u3_el_patch* pat_u,
+_snap_patch_save_page(u3_snap_patch* pat_u,
                     c3_w         pag_w,
                     c3_w         pgc_w)
 {
@@ -307,8 +307,8 @@ _el_patch_save_page(u3_el_patch* pat_u,
 }
 
 //! Make and write current patch.
-static u3_el_patch*
-_el_patch_compose(void)
+static u3_snap_patch*
+_snap_patch_compose(void)
 {
   c3_w pgs_w = 0;
   c3_w nor_w = 0;
@@ -352,7 +352,7 @@ _el_patch_compose(void)
     return 0;
   }
   else {
-    u3_el_patch* pat_u = c3_malloc(sizeof(u3_el_patch));
+    u3_snap_patch* pat_u = c3_malloc(sizeof(u3_snap_patch));
     c3_w i_w, pgc_w;
 
     // Create and open the patch's control and memory files.
@@ -380,24 +380,24 @@ _el_patch_compose(void)
       }
     }
 
-    pat_u->con_u = c3_malloc(sizeof(u3_el_control) + (pgs_w * sizeof(u3_el_line)));
-    pat_u->con_u->ver_y = u3_el_version;
+    pat_u->con_u = c3_malloc(sizeof(u3_snap_control) + (pgs_w * sizeof(u3_snap_line)));
+    pat_u->con_u->ver_y = u3_snap_version;
     pgc_w = 0;
 
     for ( i_w = 0; i_w < nor_w; i_w++ ) {
-      pgc_w = _el_patch_save_page(pat_u, i_w, pgc_w);
+      pgc_w = _snap_patch_save_page(pat_u, i_w, pgc_w);
     }
     for ( i_w = 0; i_w < sou_w; i_w++ ) {
-      pgc_w = _el_patch_save_page(pat_u, (u3a_pages - (i_w + 1)), pgc_w);
+      pgc_w = _snap_patch_save_page(pat_u, (u3a_pages - (i_w + 1)), pgc_w);
     }
 
     pat_u->con_u->nor_w = nor_w;
     pat_u->con_u->sou_w = sou_w;
     pat_u->con_u->pgs_w = pgc_w;
 
-    // Write u3_el_control struct to patch's control file.
+    // Write u3_snap_control struct to patch's control file.
     {
-      c3_w len_w = sizeof(u3_el_control) + (pat_u->con_u->pgs_w * sizeof(u3_el_line));
+      c3_w len_w = sizeof(u3_snap_control) + (pat_u->con_u->pgs_w * sizeof(u3_snap_line));
       c3_assert(len_w == write(pat_u->ctl_i, pat_u->con_u, len_w));
     }
     return pat_u;
@@ -406,7 +406,7 @@ _el_patch_compose(void)
 
 //! Make sure patch is synced to disk.
 static void
-_el_patch_sync(u3_el_patch* pat_u)
+_snap_patch_sync(u3_snap_patch* pat_u)
 {
   if ( -1 == c3_sync(pat_u->ctl_i) ) {
     fprintf(stderr, "loom: control file sync failed: %s\r\n",
@@ -423,7 +423,7 @@ _el_patch_sync(u3_el_patch* pat_u)
 
 //! Make sure image is synced to disk.
 static void
-_el_image_sync(u3_el_image* img_u)
+_snap_image_sync(u3_snap_image* img_u)
 {
   if ( -1 == c3_sync(img_u->fid_i) ) {
     fprintf(stderr, "loom: image (%s) sync failed: %s\r\n",
@@ -435,7 +435,7 @@ _el_image_sync(u3_el_image* img_u)
 
 //! Resize image, truncating if it shrunk.
 static void
-_el_image_resize(u3_el_image* img_u, c3_w pgs_w)
+_snap_image_resize(u3_snap_image* img_u, c3_w pgs_w)
 {
   if ( img_u->pgs_w > pgs_w ) {
     if ( ftruncate(img_u->fid_i, pgs_w << (u3a_page + 2)) ) {
@@ -451,14 +451,14 @@ _el_image_resize(u3_el_image* img_u, c3_w pgs_w)
 
 //! Apply patch to images.
 static void
-_el_patch_apply(u3_el_patch* pat_u)
+_snap_patch_apply(u3_snap_patch* pat_u)
 {
   c3_w i_w;
 
   //  resize images
   //
-  _el_image_resize(&pol_u.nor_u, pat_u->con_u->nor_w);
-  _el_image_resize(&pol_u.sou_u, pat_u->con_u->sou_w);
+  _snap_image_resize(&pol_u.nor_u, pat_u->con_u->nor_w);
+  _snap_image_resize(&pol_u.sou_u, pat_u->con_u->sou_w);
 
   //  seek to begining of patch and images
   //
@@ -509,7 +509,7 @@ _el_patch_apply(u3_el_patch* pat_u)
 
 //! Apply image to memory.
 static void
-_el_image_blit(u3_el_image* img_u,
+_snap_image_blit(u3_snap_image* img_u,
                c3_w*        ptr_w,
                c3_ws        stp_ws)
 {
@@ -544,7 +544,7 @@ _el_image_blit(u3_el_image* img_u,
 #ifdef U3_SNAPSHOT_VALIDATION
 //! Compare image to memory.
 static void
-_el_image_fine(u3_el_image* img_u,
+_snap_image_fine(u3_snap_image* img_u,
                c3_w*        ptr_w,
                c3_ws        stp_ws)
 {
@@ -579,13 +579,13 @@ _el_image_fine(u3_el_image* img_u,
 
 //! TODO(peter)
 static c3_o
-_el_image_copy(u3_el_image* fom_u, u3_el_image* tou_u)
+_snap_image_copy(u3_snap_image* fom_u, u3_snap_image* tou_u)
 {
   c3_w i_w;
 
   //  resize images
   //
-  _el_image_resize(tou_u, fom_u->pgs_w);
+  _snap_image_resize(tou_u, fom_u->pgs_w);
 
   //  seek to begining of patch and images
   //
@@ -623,10 +623,10 @@ _el_image_copy(u3_el_image* fom_u, u3_el_image* tou_u)
 
 //! TODO(peter)
 static void
-_el_backup(void)
+_snap_backup(void)
 {
-  u3_el_image nop_u = { .nam_c = "north", .pgs_w = 0 };
-  u3_el_image sop_u = { .nam_c = "south", .pgs_w = 0 };
+  u3_snap_image nop_u = { .nam_c = "north", .pgs_w = 0 };
+  u3_snap_image sop_u = { .nam_c = "south", .pgs_w = 0 };
   c3_i mod_i = O_RDWR | O_CREAT;
   c3_c ful_c[8193];
 
@@ -653,8 +653,8 @@ _el_backup(void)
     return;
   }
 
-  if (  (c3n == _el_image_copy(&pol_u.nor_u, &nop_u))
-     || (c3n == _el_image_copy(&pol_u.sou_u, &sop_u)) )
+  if (  (c3n == _snap_image_copy(&pol_u.nor_u, &nop_u))
+     || (c3n == _snap_image_copy(&pol_u.sou_u, &sop_u)) )
   {
 
     unlink(ful_c);
@@ -675,7 +675,7 @@ _el_backup(void)
 
 //! Handle a memory event with libsigsegv protocol.
 c3_i
-u3_el_fault(void* adr_v, c3_i ser_i)
+u3_snap_fault(void* adr_v, c3_i ser_i)
 {
   //  Let the stack overflow handler run.
   if ( 0 == ser_i ) {
@@ -734,7 +734,7 @@ u3_el_fault(void* adr_v, c3_i ser_i)
 //!
 //! If we are in dry-run mode, do nothing.
 //!
-//! First, call `_el_patch_compose` to write all dirty pages to disk and
+//! First, call `_snap_patch_compose` to write all dirty pages to disk and
 //! clear protection and dirty bits. If there were no dirty pages to write,
 //! then we're done.
 //!
@@ -749,35 +749,35 @@ u3_el_fault(void* adr_v, c3_i ser_i)
 //! process in a separate thread, but we'd need to wait until that finishes
 //! before we try to make another snapshot.
 void
-u3_el_save(void)
+u3_snap_save(void)
 {
-  u3_el_patch* pat_u;
+  u3_snap_patch* pat_u;
 
   if ( u3C.wag_w & u3o_dryrun ) {
     return;
   }
 
-  if ( !(pat_u = _el_patch_compose()) ) {
+  if ( !(pat_u = _snap_patch_compose()) ) {
     return;
   }
 
   // u3a_print_memory(stderr, "sync: save", 4096 * pat_u->con_u->pgs_w);
 
-  _el_patch_sync(pat_u);
+  _snap_patch_sync(pat_u);
 
-  if ( c3n == _el_patch_verify(pat_u) ) {
+  if ( c3n == _snap_patch_verify(pat_u) ) {
     c3_assert(!"loom: save failed");
   }
 
-  _el_patch_apply(pat_u);
+  _snap_patch_apply(pat_u);
 
 #ifdef U3_SNAPSHOT_VALIDATION
   {
-    _el_image_fine(&pol_u.nor_u,
+    _snap_image_fine(&pol_u.nor_u,
                    u3_Loom,
                    (1 << u3a_page));
 
-    _el_image_fine(&pol_u.sou_u,
+    _snap_image_fine(&pol_u.sou_u,
                    (u3_Loom + (1 << u3a_bits) - (1 << u3a_page)),
                    -(1 << u3a_page));
 
@@ -786,16 +786,16 @@ u3_el_save(void)
   }
 #endif
 
-  _el_image_sync(&pol_u.nor_u);
-  _el_image_sync(&pol_u.sou_u);
-  _el_patch_delete(pat_u);
+  _snap_image_sync(&pol_u.nor_u);
+  _snap_image_sync(&pol_u.sou_u);
+  _snap_patch_delete(pat_u);
 
-  _el_backup();
+  _snap_backup();
 }
 
 //! Start the checkpointing system.
 c3_o
-u3_el_live(c3_o nuu_o, c3_c* dir_c)
+u3_snap_live(c3_o nuu_o, c3_c* dir_c)
 {
   pol_u.dir_c = dir_c;
   pol_u.nor_u.nam_c = "north";
@@ -811,32 +811,32 @@ u3_el_live(c3_o nuu_o, c3_c* dir_c)
   {
     //  Open image files.
     //
-    if ( (c3n == _el_image_open(&pol_u.nor_u)) ||
-         (c3n == _el_image_open(&pol_u.sou_u)) )
+    if ( (c3n == _snap_image_open(&pol_u.nor_u)) ||
+         (c3n == _snap_image_open(&pol_u.sou_u)) )
     {
       fprintf(stderr, "boot: image failed\r\n");
       exit(1);
     }
     else {
-      u3_el_patch* pat_u;
+      u3_snap_patch* pat_u;
 
       /* Load any patch files; apply them to images.
       */
-      if ( 0 != (pat_u = _el_patch_open()) ) {
-        _el_patch_apply(pat_u);
-        _el_image_sync(&pol_u.nor_u);
-        _el_image_sync(&pol_u.sou_u);
-        _el_patch_delete(pat_u);
+      if ( 0 != (pat_u = _snap_patch_open()) ) {
+        _snap_patch_apply(pat_u);
+        _snap_image_sync(&pol_u.nor_u);
+        _snap_image_sync(&pol_u.sou_u);
+        _snap_patch_delete(pat_u);
       }
 
       /* Write image files to memory; reinstate protection.
       */
       {
-        _el_image_blit(&pol_u.nor_u,
+        _snap_image_blit(&pol_u.nor_u,
                        u3_Loom,
                        (1 << u3a_page));
 
-        _el_image_blit(&pol_u.sou_u,
+        _snap_image_blit(&pol_u.sou_u,
                        (u3_Loom + (1 << u3a_bits) - (1 << u3a_page)),
                        -(1 << u3a_page));
 
@@ -849,7 +849,7 @@ u3_el_live(c3_o nuu_o, c3_c* dir_c)
       ** snapshot on a future boot for which the images are not empty.
       */
       if ( (0 == pol_u.nor_u.pgs_w) && (0 == pol_u.sou_u.pgs_w) ) {
-        u3_el_foul();
+        u3_snap_foul();
         u3l_log("live: logical boot\r\n");
         nuu_o = c3y;
       }
@@ -864,9 +864,9 @@ u3_el_live(c3_o nuu_o, c3_c* dir_c)
 
 //! Disable page tracking, which makes the entire loom writable.
 c3_o
-u3_el_yolo(void)
+u3_snap_yolo(void)
 {
-  //    NB: u3_el_save() will reinstate protection flags
+  //    NB: u3_snap_save() will reinstate protection flags
   //
   if ( 0 != mprotect((void *)u3_Loom, u3a_bytes, (PROT_READ | PROT_WRITE)) ) {
     return c3n;
@@ -877,7 +877,7 @@ u3_el_yolo(void)
 
 //! Dirty all pages of the loom.
 void
-u3_el_foul(void)
+u3_snap_foul(void)
 {
   memset((void*)pol_u.dit_w, 0xff, sizeof(pol_u.dit_w));
 }
