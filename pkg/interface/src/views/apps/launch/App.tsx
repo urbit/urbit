b14dc769929abd11927e100af7cbde1c6d16467a
Liam Fitzgerald <liam@tlon.io>
2022-01-31 16:21:27 -0600

    interface: refine joining error cases

--- pkg/interface/src/views/apps/launch/App.tsx
+++ pkg/interface/src/views/apps/launch/App.tsx
@@ -94,7 +94,7 @@ export const LaunchApp = (props: LaunchAppProps): ReactElement | null => {
                 border={0}
                 p={0}
                 borderRadius={2}
-                onClick={() => history.push({ search: "?join-kind=group" })}
+                onClick={() => history.push({ search: "?join-kind=groups" })}
               >
                 <Row backgroundColor="white" gapX="2" p={2} height="100%" width="100%" alignItems="center">
                   <Icon icon="BootNode" />
