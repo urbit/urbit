b85b76aa23e819cb683f753dcdd0893e89a6612e
Peter McEvoy <peter@tlon.io>
2022-03-03 09:44:58 -0800

    u3: create noun/all.h

---
+++ pkg/urbit/include/noun/all.h
@@ -0,0 +1,32 @@
+//! @file all.h
+//! Noun environment.
+//!
+//! @note This header file should NOT be included in files that lie within
+//! `pkg/urbit/include/noun` or `pkg/urbit/noun`. Files within these two
+//! directories should explicitly include the noun header files that they need.
+
+#ifndef U3_ALL_H
+#define U3_ALL_H
+
+#include "c/all.h"
+
+#include "noun/types.h"
+#include "noun/aliases.h"
+#include "noun/allocate.h"
+#include "noun/events.h"
+#include "noun/hashtable.h"
+#include "noun/imprison.h"
+#include "noun/jets.h"
+#include "noun/log.h"
+#include "noun/manage.h"
+#include "noun/nock.h"
+#include "noun/options.h"
+#include "noun/retrieve.h"
+#include "noun/serial.h"
+#include "noun/trace.h"
+#include "noun/urth.h"
+#include "noun/vortex.h"
+#include "noun/xtract.h"
+#include "noun/zave.h"
+
+#endif /* ifndef U3_ALL_H */
