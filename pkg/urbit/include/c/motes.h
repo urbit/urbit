039bc7a975dc45bca2bd6da3af1947e69c524a4b
Jōshin <git@wholezero.org>
2022-03-02 21:09:34 -0600

    vere: remove unused and wrong mote

--- pkg/urbit/include/c/motes.h
+++ pkg/urbit/include/c/motes.h
@@ -1276,7 +1276,6 @@
 #   define c3__wyrd   c3_s4('w','y','r','d')
 #   define c3__yell   c3_s4('y','e','l','l')
 #   define c3__yelp   c3_s4('y','e','l','p')
-#   define c3__yew    c3_s3('y','a','w')
 #   define c3__z      c3_s1('z')
 #   define c3__zact   c3_s4('z','a','c','t')
 #   define c3__zalt   c3_s4('z','a','l','t')
