cc1048dd21031e6cf81c81169d3dd570d0b55dee
Patrick O'Sullivan <patrick@osullivan.io>
2022-03-31 10:52:31 -0500

    groups: add padding to error box in collections

--- pkg/interface/src/views/apps/links/components/LinkBlockInput.tsx
+++ pkg/interface/src/views/apps/links/components/LinkBlockInput.tsx
@@ -103,6 +103,7 @@ export function LinkBlockInput(props: LinkBlockInputProps) {
             flexDirection="column"
             width="100%"
             height="100%"
+            padding={3}
             position="absolute"
             left={0}
             right={0}
