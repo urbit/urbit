c54c47924534281162dc818599db2b18c6e9bb28
Patrick O'Sullivan <patrick@osullivan.io>
2022-04-18 12:16:31 -0500

    grid: Use mock-http-api lib to mock urbit responses

--- pkg/grid/src/state/api.ts
+++ pkg/grid/src/state/api.ts
@@ -1,5 +1,7 @@
 import Urbit from '@urbit/http-api';
+import UrbitMock from '@urbit/mock-http-api';
 import { useMockData } from './util';
+import { handlers } from './mock-handlers';
 
 declare global {
   interface Window {
@@ -7,15 +9,7 @@ declare global {
   }
 }
 
-const api = useMockData
-  ? ({
-      poke: async () => {},
-      subscribe: async () => {},
-      subscribeOnce: async () => {},
-      ship: '',
-      scry: async () => {}
-    } as unknown as Urbit)
-  : new Urbit('', '');
+const api = useMockData ? new UrbitMock('', '', handlers) : new Urbit('', '');
 if (import.meta.env.DEV || useMockData) {
   api.verbose = true;
 }
---
+++ pkg/grid/src/state/mock-handlers.ts
@@ -0,0 +1,34 @@
+import { Handler } from '@urbit/mock-http-api';
+import { mockAllies, mockCharges } from './mock-data';
+
+export const handlers: Handler[] = [
+  {
+    app: 'treaty',
+    path: '/default-ally',
+    func: () => '~zod'
+  },
+  {
+    app: 'docket',
+    path: '/charges',
+    func: () => ({ initial: mockCharges })
+  },
+  {
+    app: 'treaty',
+    path: '/allies',
+    func: () => ({ ini: mockAllies })
+  },
+  {
+    app: 'hood',
+    path: '/kiln/lag',
+    func: () => false
+  },
+  {
+    app: 'settings-store',
+    path: '/desk/garden',
+    func: () => {
+      return {
+        thing: 'A thing'
+      };
+    }
+  }
+];
