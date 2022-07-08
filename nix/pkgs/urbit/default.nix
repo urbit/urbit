517ca69823716e226a6edf7336b84ccb442931cd
Joe Bryan <joemfb@gmail.com>
2022-07-08 14:37:39 -0400

    build: default to using guard page

--- nix/pkgs/urbit/default.nix
+++ nix/pkgs/urbit/default.nix
@@ -68,7 +68,6 @@ in stdenv.mkDerivation {
 
   CFLAGS = oFlags ++ lib.optionals (!enableDebug) [ "-Werror" ];
 
-  GUARD_PAGE = true;
   MEMORY_DEBUG = enableDebug;
   CPU_DEBUG = enableDebug;
   EVENT_TIME_DEBUG = false;
