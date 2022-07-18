e8e7e84d32b7e5d8ae72f86fc6c0104b25a00495
Patrick O'Sullivan <patrick@osullivan.io>
2022-07-18 10:54:26 -0500

    landscape: WIP, search system prefs

---
+++ pkg/grid/src/components/icons/MagnifyingGlassIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MagnifyingGlassIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 11a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-5a5 5 0 0 0-4.172 7.757l-.535.536-2 2a1 1 0 1 0 1.414 1.414l2-2 .536-.535A5 5 0 1 0 13 6Z"
+        className="fill-current"
+      />
+    </svg>
+  );
+}
