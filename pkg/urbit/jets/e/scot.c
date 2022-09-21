014391fc658f5da762db928ace2869ca48114780
Joe Bryan <joemfb@gmail.com>
2022-09-21 18:57:43 -0400

    jets: switches +scot to the new implementations

--- pkg/urbit/jets/e/scot.c
+++ pkg/urbit/jets/e/scot.c
@@ -128,13 +128,13 @@ u3qe_scot(u3_atom a, u3_atom b)
 {
   switch (a) {
     case c3__tas: return u3k(b);
-    case c3__ud: return _print_ud(b);
-    // case c3__ud:  return u3s_etch_ud(b);
+    // case c3__ud: return _print_ud(b);
+    case c3__ud:  return u3s_etch_ud(b);
     case c3__ux:  return u3s_etch_ux(b);
-    case c3__uv: return _print_uv(b);
-    // case c3__uv:  return u3s_etch_uv(b);
-    case c3__uw: return _print_uw(b);
-    // case c3__uw:  return u3s_etch_uw(b);
+    // case c3__uv: return _print_uv(b);
+    case c3__uv:  return u3s_etch_uv(b);
+    // case c3__uw: return _print_uw(b);
+    case c3__uw:  return u3s_etch_uw(b);
     default:      return u3_none;
   }
 }
