45444f63a72d3e13b284e4257027f9896168419a
Joe Bryan <joemfb@gmail.com>
2022-11-10 12:38:35 -0500

    test: renames nock-tests to meme-tests

--- pkg/urbit/tests/meme_tests.c
+++ pkg/urbit/tests/meme_tests.c
@@ -5,9 +5,6 @@
 static void
 _setup(void)
 {
-  //  XX at 1<<24, this succeeds on mac, but bail:exit's on linux.
-  //  investigate possible u3n_prog corruption
-  //
   u3m_init(1 << 24);
   u3m_pave(c3y);
   u3e_init();
@@ -53,7 +50,7 @@ _test_nock_meme(void)
 }
 
 static c3_i
-_test_nock(void)
+_test_meme(void)
 {
   c3_i ret_i = 1;
 
@@ -72,8 +69,8 @@ main(int argc, char* argv[])
 {
   _setup();
 
-  if ( !_test_nock() ) {
-    fprintf(stderr, "test nock: failed\r\n");
+  if ( !_test_meme() ) {
+    fprintf(stderr, "test meme: failed\r\n");
     exit(1);
   }
 
@@ -81,6 +78,6 @@ main(int argc, char* argv[])
   //
   u3m_grab(u3_none);
 
-  fprintf(stderr, "test nock: ok\r\n");
+  fprintf(stderr, "test meme: ok\r\n");
   return 0;
 }
