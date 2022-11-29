1e03ddf23316392d93701a974610f6547891164a
barter-simsum <barter-simsum@protonmail.com>
2022-11-29 00:21:41 -0500

    new canon

--- pkg/urbit/include/c/types.h
+++ pkg/urbit/include/c/types.h
@@ -5,6 +5,8 @@
   **/
     /* Canonical integers.
     */
+      typedef size_t c3_z;
+      typedef ssize_t c3_zs;
       typedef uint64_t c3_d;
       typedef int64_t c3_ds;
       typedef uint32_t c3_w;
@@ -27,5 +29,43 @@
       typedef int       c3_i;      // int - really bad
       typedef uintptr_t c3_p;      // pointer-length uint - really really bad
       typedef intptr_t c3_ps;      // pointer-length int - really really bad
+
+      /* Print specifiers
+      */
+
+      /* c3_z */
+      #define PRIc3_z  "zu"      /* unsigned dec */
+      #define PRIc3_zs "zd"      /*   signed dec */
+      #define PRIxc3_z "zx"      /* unsigned hex */
+      #define PRIXc3_z "zX"      /* unsigned HEX */
+
+      /* c3_d */
+      #define PRIc3_d  PRIu64
+      #define PRIc3_ds PRIi64
+      #define PRIxc3_d PRIx64
+      #define PRIXc3_d PRIX64
+
+      /* c3_w */
+      #define PRIc3_w  PRIu32
+      #define PRIc3_ws PRIi32
+      #define PRIxc3_w PRIx32
+      #define PRIXc3_w PRIX32
+
+      /* c3_s */
+      #define PRIc3_s  PRIu16
+      #define PRIc3_ss PRIi16
+      #define PRIxc3_s PRIx16
+      #define PRIXc3_s PRIX16
+
+      /* c3_y */
+      #define PRIc3_y  PRIu8
+      #define PRIc3_ys PRIi8
+      #define PRIxc3_y PRIx8
+      #define PRIXc3_y PRIX8
+
+      /* c3_b */
+      #define PRIc3_b  PRIu8
+      #define PRIxc3_b PRIx8
+      #define PRIXc3_b PRIX8
 
 #endif /* ifndef C3_TYPES_H */
