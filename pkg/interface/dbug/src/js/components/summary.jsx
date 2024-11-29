aa4b91e876274bcc6821b96a9d4a49af8e28afdd
yosoyubik <yosoyubik@gmail.com>
2024-11-29 11:38:22 +0100

    interface: fix ~debug multiple re-renders of %ames flows

--- pkg/interface/dbug/src/js/components/summary.jsx
+++ pkg/interface/dbug/src/js/components/summary.jsx
@@ -1,36 +1,44 @@
-import React, { Component } from 'react';
+import React, {
+    useEffect,
+    useRef, useCallback,
+    useState} from 'react';
 
-export class Summary extends Component {
-  // expected props:
-  // id: 'id'
-  // summary: <jsx>
-  // details: <jsx>
-  // onOpen: function(id)
-  // onClose: function(id)
 
-  constructor(props) {
-    super(props);
-    this.onToggle = this.onToggle.bind(this);
-  }
+const Details = ({ ...props }) => {
+    const detailsRef = useRef();
+    const onToggleCallback = useCallback(() => {
+      props.onToggle(detailsRef.current?.open)
+    }, [props.onToggle]);
 
-  onToggle(event) {
-    if (event.target.open) {
-      if (this.props.onOpen) this.props.onOpen(this.props.id);
-    } else {
-      if (this.props.onClose) this.props.onClose(this.props.id);
-    }
-  }
+    useEffect(() => {
+      detailsRef.current?.addEventListener('toggle', onToggleCallback)
 
-  render() {
-    const { props } = this;
+      return () => {
+        detailsRef.current?.removeEventListener('toggle', onToggleCallback)
+      }
+    }, [onToggleCallback])
+
 
     return (
-      <details onToggle={this.onToggle} {...props} style={{border: '1px solid black', padding: '4px', position: 'relative', ...props.style}}>
-        <summary>
-          {props.summary}
-        </summary>
+      <details ref={detailsRef} open={props.open}
+        style={{border: '1px solid black', padding: '4px', position: 'relative', ...props.style}}
+      >
+        <summary>{props.summary}</summary>
         <div style={{borderTop: '1px solid black'}}>{props.details}</div>
       </details>
-    )
-  }
+    );
+}
+
+export const Summary = ({ ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Details open={isOpen} onToggle={(value) => {
+        setIsOpen(value);
+        if (props.onOpen) {
+          props.onOpen(props.id);
+        }
+      }
+    } {...props} />
+  )
 }
